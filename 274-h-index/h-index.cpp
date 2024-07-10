0873ec31d3819eb37ed6a421a1de90948476feae
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-10 13:42:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (54.81%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -3,23 +3,50 @@ public:
     int hIndex(vector<int>& cit) 
     {
         sort(cit.begin(),cit.end());
-        int cnt=0;
-        int ans=0;
+        int i=0;
         int n=cit.size();
-        for(int i=n-1;i>=0;i--)
+        while(i<n && cit[i]<n-i)
         {
-            if(cit[i]<cnt+1)
-            {
-                ans=cnt;
-                break;
-            }
-            cnt++;
+            i++;
         }
-        //to handle case when cit=[1] only
-        if(ans==0)
-        {
-            ans=cnt;
-        }
-        return ans;
+        return n-i;
     }
 };
+
+//METHOD-2
+//OPTIMIZED USING BINARY SEARCH
+//class Solution {
+// public:
+//     bool possible(vector<int>& cit, int k)
+//     {
+//         int cnt=0;
+//         for(auto i:cit)
+//         {
+//             if(i>=k)
+//             {
+//                 cnt++;
+//             }
+//         }
+//         return cnt>=k;
+//     }
+//     int hIndex(vector<int>& cit) 
+//     {
+//        int low= 0;
+//        int high= cit.size();
+//        int ans=0;
+//        while(low<=high)
+//         {
+//             int mid= (low+high)/2;
+//             if(possible(cit,mid))
+//             {
+//                 ans= mid;
+//                 low= mid+1;
+//             }
+//             else
+//             {
+//                 high= mid-1;
+//             }
+//         } 
+//         return ans;
+//     }
+// };
