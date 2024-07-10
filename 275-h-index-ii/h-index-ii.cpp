0e471897c3ffbf62867d8681c69a9319c6b6c2e6
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-10 13:38:13 +0530

    Time: 15 ms (39.30%) | Memory: 21 MB (27.68%) - LeetSync

--- 275-h-index-ii/h-index-ii.cpp
+++ 275-h-index-ii/h-index-ii.cpp
@@ -2,29 +2,12 @@ class Solution {
 public:
     int hIndex(vector<int>& cit) 
     {
-        int n= cit.size();
-        int low= 0;
-        int high= n-1;//starting from n-1 
-        //otherwise will go out of bound
-        while(low<=high)
+        int i=0;
+        int n=cit.size();
+        while(i<n && cit[i]<n-i)
         {
-            int mid= (low+high)/2;
-            //since array is sorted so it means that
-            //we have n-mid papers with atleast n-mid citations so this will be our ans 
-            if(cit[mid]== n-mid)
-            {
-                return n-mid;
-            }
-            else if(cit[mid] > n-mid)
-            {
-                high= mid-1;
-            }
-            else
-            {
-                low= mid+1;
-            }
+            i++;
         }
-        //if not find ans then return this
-        return n-low;
+        return n-i;
     }
 };
