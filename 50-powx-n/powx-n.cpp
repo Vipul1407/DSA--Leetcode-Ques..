81c75092b6cb1a6001d130bf186b94b6a8dbf579
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-16 22:22:30 +0530

    Time: 4 ms (20.80%) | Memory: 8.6 MB (6.96%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,27 +2,6 @@ class Solution {
 public:
     double myPow(double x, int n) 
     {
-        if(n<0)
-        {
-            n= abs(n);
-            x= 1/x;
-        }
-        double ans=1;
-        while(n>0)
-        {
-            //if n is odd
-            if(n%2==1)
-            {
-                ans*=x;
-                n-=1;
-            }
-            //if n is even
-            else
-            {
-                x*=x;
-                n/=2;
-            }
-        }
-        return ans;
+        return pow(x,n);
     }
 };
