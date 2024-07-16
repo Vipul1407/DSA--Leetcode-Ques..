0eac3e21fcf22233594d1288a0fe9623694222d1
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-16 22:31:58 +0530

    Time: 4 ms (20.80%) | Memory: 8.3 MB (6.96%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     double myPow(double x, int n) 
     {
-        return pow(x,n);
+        long long nn= abs(n);
+        double ans=1;
+        while(nn>0)
+        {
+            if(nn%2==1)
+            {
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else
+            {
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        return (n<0)? 1/ans:ans;
     }
 };
