7f43b6a28e6ab78da79164f3afcfdce8774f255f
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-19 01:01:39 +0530

    Time: 78 ms (5.42%) | Memory: 24.6 MB (6.17%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -6,21 +6,31 @@ public:
         int n= matrix.size();
         // int m= matrix[0].size();
 
-        //MAX HEAP
-        priority_queue<int>pq;
-        int cnt=0;
+        //MIN HEAP taking advantage that elements are sorted
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>pq;
+        //value, row, col
+
+        //pushing first value of each row
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            pq.push({matrix[i][0],i,0});
+        }
+        int cnt=0;
+        int ans=0;
+        while(cnt<k)
+        {
+            auto curr= pq.top();
+            pq.pop();
+            ans= curr[0];
+            int row= curr[1];
+            int col= curr[2];
+            cnt++;
+
+            if(col+1<n)
             {
-                pq.push(matrix[i][j]);
-                cnt++;
-                if(cnt>k)
-                {
-                    pq.pop();
-                }
+                pq.push({matrix[row][col+1],row,col+1});
             }
         }
-        return pq.top();
+        return ans;
     }
 };
