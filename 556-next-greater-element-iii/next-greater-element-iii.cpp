3d2d0fbcc78eef3ca8489bad478a36c604060478
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-22 18:43:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (13.23%) - LeetSync

--- 556-next-greater-element-iii/next-greater-element-iii.cpp
+++ 556-next-greater-element-iii/next-greater-element-iii.cpp
@@ -4,44 +4,30 @@ public:
     {
         string str= to_string(num);
         string ans="";
-        int n= str.length();
+        int n= str.size();
         int idx=-1;
-        //find index of greater element from right staring from n-2 index as n-1 has no element no right
         for(int i=n-2;i>=0;i--)
         {
             if(str[i]<str[i+1])
             {
                 idx=i;
+                for(int k=n-1;k>idx;k--)
+                {
+                    if(str[k]> str[idx])
+                    {
+                        swap(str[idx],str[k]);
+                        break;
+                    }
+                }
                 break;
             }
         }
-        //if no element found means it means larger no. cannot be formed, it is already larger
         if(idx==-1)
         {
             return -1;
         }
-        //finding just greater element than the element at idx
-        int nextgreat= idx+1;
-        int k= n-1;
-        while(k>idx)
-        {
-            if(str[k]>str[idx])
-            {
-                nextgreat= k;
-                break;
-            }
-            k--;
-        }
-        //swap both elements
-        swap(str[idx],str[nextgreat]);
-
-        //after swapping sort the remainging right side string
-        reverse(str.begin()+idx+1, str.end());
-
-        //converting back string to long long
+        reverse(str.begin()+idx+1,str.end());
         long long res= stoll(str);
-
-        //if ans fits in 32-bit integer then return it else return -1
-        return res<=INT_MAX? res: -1;
+        return res<=INT_MAX? res:-1;
     }
 };
