53491249da4d9f5ab50858adae869b731a2b9cb1
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-24 21:28:23 +0530

    Time: 135 ms (93.07%) | Memory: 87.7 MB (64.94%) - LeetSync

--- 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.cpp
@@ -6,23 +6,33 @@ public:
     }
     vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) 
     {
+        vector<pair<int,int>>vec;
         vector<int>ans;
-        vector<pair<int,int>>vec;//original mapping given, converted string
         for(auto i:nums)
         {
-            string str= to_string(i);
-            string form="";
-            for(char c:str)
+            int original=i;
+            int form=0;
+            int place=1;
+            if(i==0)
             {
-                form+= to_string(mapping[c-'0']);
+                form= mapping[0];//place original no.
             }
-            vec.push_back({i,stoi(form)});
+            else
+            {
+                while(i>0)
+                {
+                    form+= mapping[i%10]*place;
+                    i/=10;
+                    place*=10;
+                }
+            }
+            vec.push_back({original,form});//we are pushing original mapping, formed integer as 
+            //  in sorting to keep relative order if same formed integer
         }
         sort(vec.begin(),vec.end(),cmp);
-
         for(auto i:vec)
         {
-            ans.push_back(i.first);
+            ans.push_back({i.first});
         }
         return ans;
     }
