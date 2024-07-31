d48d1aa97556158b17226689758940406020a953
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-07-31 14:40:22 +0530

    Time: 131 ms (90.72%) | Memory: 96.5 MB (71.53%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,60 +1,42 @@
-// Using Floyd Warshall 
-
-// TC= O(26^3)+ O(2* N)
-// SC= O(N^2)
-
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost)
+    long long minimumCost(string s, string t, vector<char>& ori, vector<char>& chan, vector<int>& cost) 
     {
-        int n= original.size();
-
-        // create adj matrix...
-        vector<vector<long long>> adjMatrix(26, vector<long long> (26, INT_MAX));
-
-        for(int i=0; i<26; i++)
+        int n= s.size();
+        vector<vector<int>>dis(26,vector<int>(26,INT_MAX));
+        for(int i=0;i<ori.size();i++)
         {
-            adjMatrix[i][i]= 0;
+            dis[ori[i]-'a'][chan[i]-'a']= min(cost[i],dis[ori[i]-'a'][chan[i]-'a']);
         }
-
-        for(int i=0; i<n; i++)
-        {   
-            char ch1= original[i];
-            char ch2= changed[i];
-    
-            adjMatrix[ch1-'a'][ch2-'a']= min((long long) cost[i], adjMatrix[ch1-'a'][ch2-'a']);
-        }
-        
-        // Initializing min cost... 
-        for(int k=0; k<26; k++)
+        for(int k=0;k<26;k++)
         {
-            for(int i=0; i<26; i++)
+            for(int i=0;i<26;i++)
             {
-                for(int j=0; j<26; j++)
+                for(int j=0;j<26;j++)
                 {
-                    if(adjMatrix[i][k]== INT_MAX || adjMatrix[k][j]== INT_MAX)
+                    if(dis[i][k]!=INT_MAX && dis[k][j]!=INT_MAX)
                     {
-                        continue;
+                        dis[i][j]= min(dis[i][j], dis[i][k]+dis[k][j]);
                     }
-                adjMatrix[i][j]= min(adjMatrix[i][j], adjMatrix[i][k]+adjMatrix[k][j]);
                 }
             }
         }
-        
-        long long totalCost= 0;
-        for(int i=0; i<source.size(); i++)
+        long long ans=0;
+        for(int i=0;i<n;i++)
         {
-            if(source[i] != target[i])
+            if(s[i]!=t[i])
             {
-               long long currCost= adjMatrix[source[i]-'a'][target[i]- 'a'];
-               if(currCost== INT_MAX)
-               {
-                return -1;
-                } 
-            totalCost+= currCost;
+                int d= dis[s[i]-'a'][t[i]-'a'];
+                if(d!=INT_MAX)
+                {
+                    ans+=d;
+                }
+                else
+                {
+                    return -1;
+                }
             }
         }
-
-    return totalCost;
+        return ans;
     }
 };
