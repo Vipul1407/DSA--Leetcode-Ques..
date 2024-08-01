63900b1cdb489892e825803f5980bf61c5066f66
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-08-01 13:31:13 +0530

    Time: 773 ms (5.80%) | Memory: 287.6 MB (5.35%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,138 +1,49 @@
-//  ***** Merge Sort *****
-// TC= O(N)
-
-
-class Solution{
-public:
-    void merge(vector<int> &arr, int start, int mid, int end)
-    {
-         // [start ....  mid]     [mid+1    end]
-
-         vector<int> temp;
-         int left= start;
-         int right= mid+1;
-
-         while(left <= mid && right <= end)
-         {
-              if(arr[left] <= arr[right])
-              {
-                  temp.push_back(arr[left]);
-                  left++;
-              }
-              else
-              {
-                  temp.push_back(arr[right]);
-                  right++;
-              }
-         }
-
-         // if some elements left in one subarray and other is finished..
-         while(left<= mid)
-         {
-            temp.push_back(arr[left]);
-            left++;
-         }
-
-         while(right <= end)
-         {
-            temp.push_back(arr[right]);
-            right++;
-         }
-
-        // Now copy content from temp to original array...
-        for(int i=start; i<=end; i++)
-        {
-            arr[i]= temp[i-start];
-        }  
-    }
-
-    void mergeSort(vector<int>&arr, int start, int end)
-    {
-        // base case
-        if(start >= end){
-            return;
-        }
-        
-        int mid= (start+end)/2;
-        mergeSort(arr,start,mid);
-        mergeSort(arr,mid+1,end);
-
-        merge(arr,start,mid,end);
-    }
-
-    vector<int> sortArray(vector<int>& arr) {
-        int n= arr.size();
-         mergeSort(arr,0,n-1);
-         return arr;
-    }
-};
-
-
-
-
-
-
-
-
-
-//  -----  TLE ------
-
-//  ***** Quick Sort  *****  Giving TLE bcoz pivot= arr[lb]
-//   ---- use pivot element as pivot..
-
-// In avg case TC- O(N log N)
-// Worst case TC= O(N^2)
-
-/*
-
 class Solution {
 public:
-   int findPivot(vector<int> &arr, int lb, int ub)
-   {
-        int pivot= arr[lb];
-        int start= lb;
-        int end= ub;
-
-        while(start< end)
-        {
-           while(start <= ub && arr[start] <= pivot)
-           {
-              start++;
-           }
-
-           while(end >= lb  && arr[end] > pivot)
-           {
-              end--;
-           }
-
-           if(start < end)
-           {
-              swap(arr[start], arr[end]);
-           }
-        }
-
-        swap(arr[lb], arr[end]);
-        return end;
-   }
-
-
-    vector<int> quickSort(vector<int> &arr, int lb, int ub)
+    void merge(int lb, int mid, int ub, vector<int>&nums)
     {
-        int partition;
-        if(lb < ub)
+        //merging both starting from lb and from mid+1
+        vector<int>temp;
+        int i=lb;
+        int j=mid+1;
+        while(i<=mid && j<=ub)
         {
-          partition= findPivot(arr,lb,ub);
-          quickSort(arr, lb, partition-1);
-          quickSort(arr, partition+1, ub);
+            if(nums[i]<=nums[j])
+            {
+                temp.push_back(nums[i++]);
+            }
+            else if(nums[j]<nums[i])
+            {
+                temp.push_back(nums[j++]);
+            }
         }
-        return arr;
+        while(i<=mid)
+        {
+            temp.push_back(nums[i++]);
+        }
+        while(j<=ub)
+        {
+            temp.push_back(nums[j++]);
+        }
+        for(int i=lb;i<=ub;i++)
+        {
+            nums[i]= temp[i-lb];
+        }
     }
- 
-
-    vector<int> sortArray(vector<int>& arr) {
-        int n= arr.size();
-        return quickSort(arr,0,n-1);
+    void mergesort(int lb, int ub, vector<int>& nums)
+    {
+        int mid= (lb+ub)/2;
+        if(lb<ub)
+        {
+            mergesort(lb,mid,nums);
+            mergesort(mid+1,ub,nums);
+            merge(lb,mid,ub,nums);
+        }
+    }
+    vector<int> sortArray(vector<int>& nums) 
+    {
+        int n= nums.size();
+        mergesort(0, n-1, nums);
+        return nums;
     }
 };
-
-*/
