b59d52b9711baea1aef0ed1d67f70e9b2ee52ab0
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-08-06 18:36:50 +0530

    Time: 2 ms (45.91%) | Memory: 8.1 MB (30.19%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -3,40 +3,10 @@ public:
 
     int minimumPushes(string word) 
     {
-        vector<int>vec(26,0);
-        //8-->single letter
-        //8-->double digit
-        //8-->triple digit
-        //2-->4 digits
-        for(auto i:word)
-        {
-            vec[i-'a']++;
-        } 
-        int cnt=0;
         int ans=0;
-        for(int i=0;i<26;i++)
+        for(int i=0;i<word.size();i++)
         {
-            if(vec[i]==0)
-            {
-                continue;
-            }
-            cnt++;
-            if(cnt<=8)
-            {
-                ans+=vec[i];
-            }
-            else if(cnt<=16)
-            {
-                ans+= vec[i]*2;
-            }
-            else if(cnt<=24)
-            {
-                ans+= vec[i]*3;
-            }
-            else
-            {
-                ans+= vec[i]*4;
-            }
+            ans+= (1+i/8);
         }
         return ans;
     }
