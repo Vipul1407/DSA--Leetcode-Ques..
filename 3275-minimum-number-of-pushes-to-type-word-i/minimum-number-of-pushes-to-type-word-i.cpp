8c4ca77af11efe0810d236653595b6d60b6566f2
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-08-06 13:29:41 +0530

    Time: 4 ms (33.96%) | Memory: 8.3 MB (21.91%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
+
     int minimumPushes(string word) 
     {
-        unordered_map<char,int>mp;
+        vector<int>vec(26,0);
         //8-->single letter
         //8-->double digit
         //8-->triple digit
         //2-->4 digits
         for(auto i:word)
         {
-            mp[i]++;
+            vec[i-'a']++;
         } 
-        vector<int>vec;
-        for(auto i:mp)
-        {
-            vec.push_back(i.second);
-        }
-        sort(vec.begin(),vec.end());
         int cnt=0;
         int ans=0;
-        for(auto i:vec)
+        for(int i=0;i<26;i++)
         {
+            if(vec[i]==0)
+            {
+                continue;
+            }
             cnt++;
             if(cnt<=8)
             {
-                ans+= i;
+                ans+=vec[i];
             }
             else if(cnt<=16)
             {
-                ans+= i*2;
+                ans+= vec[i]*2;
             }
             else if(cnt<=24)
             {
-                ans+= i*3;
+                ans+= vec[i]*3;
             }
             else
             {
-                ans+= i*4;
+                ans+= vec[i]*4;
             }
         }
         return ans;
