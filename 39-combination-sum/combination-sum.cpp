545cce474ebe114a146c4edf39dd60cb45950ae1
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-08-13 20:26:08 +0530

    Time: 17 ms (31.89%) | Memory: 18.7 MB (28.52%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -5,24 +5,22 @@ public:
     vector<vector<int>>ans;
     void solve(int i, vector<int>& cand, int k, vector<int>vec)
     {
+        int n= cand.size();
         if(k==0)
         {
             ans.push_back(vec);
             return;
         }
-        if(i==cand.size() || k<0)
+        if(i==n || k<0)
         {
             return;
         }
-        //take
-        if(cand[i]<=k)
+        for(int j=i;j<n;j++)
         {
-            vec.push_back(cand[i]);
-            solve(i,cand,k-cand[i],vec);
+            vec.push_back(cand[j]);
+            solve(j,cand,k-cand[j],vec);
             vec.pop_back();
         }
-        //not take
-        solve(i+1,cand,k,vec);
     }
 
     vector<vector<int>> combinationSum(vector<int>& cand, int k) 
