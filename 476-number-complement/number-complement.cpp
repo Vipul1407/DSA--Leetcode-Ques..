5267802e5fac01a83a37b32b32c7ab422acd0b5d
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-08-22 17:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (57.13%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -4,10 +4,7 @@ public:
     int findComplement(int num) 
     {
         int totalbits= log2(num)+1;
-        for(int i=0;i<totalbits;i++)
-        {
-            num= (num^(1<<i));
-        }
-        return num;
+        unsigned mask= (1U<<totalbits)-1;
+        return num^mask;
     }
 };
