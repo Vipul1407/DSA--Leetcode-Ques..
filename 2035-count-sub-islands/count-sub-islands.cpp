4d179cbec1afc2ecb4a61d732b79fc7f4ec25d5c
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-10 13:13:31 +0530

    Time: 305 ms (25.62%) | Memory: 222.1 MB (13.93%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-
-    bool dfs(int i,int j,vector<vector<int>>& grid1, vector<vector<int>>& grid2) 
+    //right--> 0,1 
+    //down--> 1,0
+    //left--> 0,-1
+    //up--> -1,0
+    vector<pair<int,int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+    bool dfs(int i, int j, vector<vector<int>>& grid1, vector<vector<int>>& grid2)
     {
         int m= grid2.size();
         int n= grid2[0].size();
-        //base cases..
-        //if we get any out of bound indexes means we have done our computations
-        if(i<0 || i>=m || j<0 || j>=n)
-        {
-            return true;
-        }
-        //if we get any water in grid2 means we are done with our computations
-        if(grid2[i][j]!=1)
-        {
-            return true;
-        }
-        grid2[i][j]=-1;//to mark it as visited block..
+        //USING BFS
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        grid2[i][j]=-1;//to mark it as visited
 
-        bool result= (grid1[i][j]==1);// grid1 must have water there..
-        result = dfs(i-1,j,grid1,grid2) && result ;//up
-        result = dfs(i,j+1,grid1,grid2) && result;//right
-        result = dfs(i+1,j,grid1,grid2) && result;//down
-        result = dfs(i,j-1,grid1,grid2) && result;//left
+        bool result= true;
+        while(!q.empty())
+        {
+            auto [x,y]= q.front();
+            q.pop();
+            //base case within queue
+            if(grid1[x][y]!=1)
+            {
+                result= false;
+            }
+            for(auto i:dir)
+            {
+                int newx= x+ i.first;
+                int newy= y+ i.second;
+                if(newx>=0 && newx<m && newy>=0 && newy<n && grid2[newx][newy]==1)
+                {
+                    grid2[newx][newy]=-1; //to mark it as visited
+                    q.push({newx,newy});
+                }
+            }
+        }
         return result;
     }
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) 
     {
-        //Note that we have to check sub-islands in grid2
-        //Conditions-  
-        //1)grid2 check for horizontal and vertical blocks of 1 only
-        //2)grid2 wle hrr block p grid1 p bhi 1 hona chaiye agar ek bhi 0 hua to usko ni lenge
-        int m= grid2.size();
         int n= grid2[0].size();
+        int m= grid2.size();
         int cnt=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(grid2[i][j]==1)
+                if(grid2[i][j]==1 && dfs(i,j,grid1,grid2))
                 {
-                    if(dfs(i,j,grid1,grid2))
-                    {
-                        cnt++;
-                    }
-                    
+                    cnt++;
                 }
             }
         }
