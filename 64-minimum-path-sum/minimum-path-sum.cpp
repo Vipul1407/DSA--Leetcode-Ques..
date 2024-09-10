cf35a6a91a5f70c777a7ed5f5a0106f07b5f2648
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-10 19:23:04 +0530

    Time: 12 ms (20.71%) | Memory: 14.3 MB (22.69%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,30 +2,36 @@ class Solution
 {
 public:
 
+    //It is only dependent on-:
+    //dp[i+1][j]--> next row
+    //dp[i][j+1]--> curr row
     int minPathSum(vector<vector<int>>& grid) 
     {
         int n= grid[0].size();
         int m= grid.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
+
         for(int i=m-1;i>=0;i--)
         {
             for(int j=n-1;j>=0;j--)
             {
                 if(i==m-1)
                 {
-                    dp[i][j]= grid[i][j]+ dp[i][j+1];
+                    curr[j]= grid[i][j]+ curr[j+1];
                 }
                 else if(j==n-1)
                 {
-                    dp[i][j]= grid[i][j]+ dp[i+1][j];
+                    curr[j]= grid[i][j]+ next[j];
                 }
                 else
                 {
-                    dp[i][j]= grid[i][j]+ min(dp[i+1][j], dp[i][j+1]);
+                    curr[j]= grid[i][j]+ min(curr[j+1], next[j]);
                 }
             }
+            swap(curr,next);
         }
-        return dp[0][0];
+        return next[0];
     }
     
 };
