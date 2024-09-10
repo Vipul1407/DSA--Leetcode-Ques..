2759cc01c3cd3c62370e0f4c65578b02b6297c9e
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-10 18:59:15 +0530

    Time: 10 ms (39.12%) | Memory: 14.9 MB (8.37%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,39 +1,32 @@
-class Solution {
+class Solution 
+{
 public:
 
-    int tda(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp)
-    {
-        int n= grid[0].size();
-        int m= grid.size();
-        if(i==m-1 && j==n-1)
-        {
-            return grid[i][j];
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        //can either go right or down..
-        //if in last row then can go only right
-        if(i==m-1)
-        {
-            return dp[i][j]= grid[i][j] + tda(i,j+1,grid,dp);
-        }
-        //if in last col then can go only down
-        if(j==n-1)
-        {
-            return dp[i][j]= grid[i][j] + tda(i+1,j,grid,dp);
-        }
-        else
-        {
-            return dp[i][j]= grid[i][j] + min(tda(i+1,j,grid,dp),tda(i,j+1,grid,dp));
-        }
-    }
     int minPathSum(vector<vector<int>>& grid) 
     {
         int n= grid[0].size();
         int m= grid.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return tda(0,0,grid,dp);
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                if(i==m-1)
+                {
+                    dp[i][j]= grid[i][j]+ dp[i][j+1];
+                }
+                else if(j==n-1)
+                {
+                    dp[i][j]= grid[i][j]+ dp[i+1][j];
+                }
+                else
+                {
+                    dp[i][j]= grid[i][j]+ min(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
     }
+    
 };
+
