d80f8f04d50aeba413fd52bad668fff782eca068
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-14 10:36:46 +0530

    Time: 110 ms (36.84%) | Memory: 84.9 MB (77.85%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int longestSubarray(vector<int>& nums) 
     {
-        int streak=0;//to find max contigous occurrence of same number..
-        int maxi=0;
-        int ans=0;//to find max length of longest subarray
+        int ans=1;
+        int cnt=0;
+        int maxi= *max_element(nums.begin(),nums.end());
         for(auto i:nums)
         {
-            if(i>maxi)
+            if(i==maxi)
             {
-                maxi=i;
-                streak=1;
-                ans=1;//reset ans to 1 
+                cnt++;
             }
-            else if(maxi==i)
-            {
-                streak++;
-            }
-            //element small h mtlb no contribution to ans so its streak=0
             else
             {
-                streak=0;
+                cnt=0;
             }
-            ans= max(ans,streak);
+            ans= max(ans,cnt);
         }
         return ans;
     }
