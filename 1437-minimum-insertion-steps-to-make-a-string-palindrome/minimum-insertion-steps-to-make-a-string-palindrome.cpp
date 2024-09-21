140055ed64793777afca5991ba4283e4eca37cad
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-21 12:15:58 +0530

    Time: 25 ms (74.79%) | Memory: 30.1 MB (31.98%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int lcs(int i,int j,string &s, string &rev, vector<vector<int>>&dp)
-    {
-        int n= s.length();
-        if(i==n || j==n)
-        {
-            return 0;
-        }
-        if(dp[i][j]!=-1)
-        {
-            return dp[i][j];
-        }
-        else if(s[i]==rev[j])
-        {
-            return dp[i][j]= 1+lcs(i+1,j+1,s,rev,dp);
-        }
-        else
-        {
-            return dp[i][j]= max(lcs(i+1,j,s,rev,dp),lcs(i,j+1,s,rev,dp));
-        }
-    }
+    
     //DIRECTLY SAME AS LONGEST COMMOM SUBSEQUENCE....
     int minInsertions(string s)
     {
         int n= s.length();
         string rev=s;
         reverse(rev.begin(),rev.end());
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                if(s[i]==rev[j])
+                {
+                    dp[i][j]= 1+ dp[i+1][j+1];
+                }
+                else 
+                {
+                    dp[i][j]= max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
         //to get min operations..
-        return n-lcs(0,0,s,rev,dp);
+        //becoz dp[0][0] is storing the longest common palindromic subsequence length so n- this will give the min insertion..
+        return n-dp[0][0];
     }
 };
