8e583d31d4c75e2e840f29923d1ae85b2a9990fe
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-09-28 12:24:31 +0530

    Time: 142 ms (44.76%) | Memory: 86.7 MB (60.83%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int rec(int i,string s, unordered_set<string>&st, vector<int>&dp)
-    {
-        if(i>=s.length())
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int result= 1+ rec(i+1,s,st,dp);
-        for(int j=i;j<s.length();j++)
-        {
-            //forming a substring starting from i itself
-            string curr= s.substr(i,j-i+1);
-            if(st.find(curr)!=st.end())
-            {
-                result= min(result, rec(j+1,s,st,dp));
-            }
-        }
-        return dp[i]=result;
-    }
     int minExtraChar(string s, vector<string>& dict) 
     {
         unordered_set<string>st(dict.begin(),dict.end());
-        vector<int>dp(s.length(),-1);
-        return rec(0,s,st,dp);
+        int n= s.length();
+        vector<int>dp(n+1,0);
+        for(int i= n-1;i>=0;i--)
+        {
+            int result= 1+dp[i+1];
+            for(int j=i;j<n;j++)
+            {
+                string curr= s.substr(i,j-i+1);
+                if(st.find(curr)!=st.end())
+                {
+                    result= min(result, dp[j+1]);
+                }
+            }
+            dp[i]=result;
+        }
+        return dp[0];
     }
 };
