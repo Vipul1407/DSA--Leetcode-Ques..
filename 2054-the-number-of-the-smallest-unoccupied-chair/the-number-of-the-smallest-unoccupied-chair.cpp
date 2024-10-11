74fc3bcc255cf1597e14a13d65d5bfba1e91fd22
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-11 15:53:42 +0530

    Time: 133 ms (84.54%) | Memory: 58.9 MB (81.79%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,5 +1,53 @@
 class Solution {
 public:
+    int smallestChair(vector<vector<int>>& time, int fri)
+    {
+        int n= time.size();
+        int curr=0;//storing curr chair
+        //min heap storing departure time, curr chair
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        //min heap storing free chair
+        priority_queue<int,vector<int>,greater<int>>free;
+
+        int arrif= time[fri][0];//storing friend's arrivall time..
+        sort(time.begin(),time.end());
+        for(auto e:time)
+        {
+            int arrival= e[0];
+            int dep= e[1];
+            //if departure<=arrival
+            while(pq.size() && pq.top().first<=arrival)
+            {
+                free.push(pq.top().second);//to store curr free chair
+                pq.pop();
+            }
+            if(free.size())
+            {
+                if(arrif== arrival)
+                {
+                    return free.top();
+                }
+                pq.push({dep,free.top()});
+                free.pop();
+            }
+            else
+            {
+                if(arrif== arrival)
+                {
+                    return curr;
+                }
+                pq.push({dep,curr});
+                curr++;
+            }
+        }
+        return -1;
+    }
+};
+
+//BRUTE FORCE------------------>O(N^2)
+
+/*class Solution {
+public:
     // static bool cmp(const vector<int>&a, const vector<int>&b)
     // {
     //     if(a[0]!=b[0])
@@ -35,4 +83,4 @@ public:
         }
         return -1;
     }
-};
+};*/
