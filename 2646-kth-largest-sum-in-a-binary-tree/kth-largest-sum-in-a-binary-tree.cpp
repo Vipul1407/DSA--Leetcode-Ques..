46ca7b0a1cc97874016e1632ad3ef1947d7ee0e3
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-23 23:01:51 +0530

    Time: 21 ms (96.68%) | Memory: 160 MB (5.08%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -11,34 +11,40 @@
  */
 class Solution {
 public:
-    long long kthLargestLevelSum(TreeNode* root, int k) {
-        vector<long long> res;      // To store sum of each level
-        queue<TreeNode*> q;         // Queue for level-order traversal (BFS)
-        
-        q.push(root);               // Start BFS from the root node
-        while (!q.empty()) {
-            int n = q.size();       // Number of nodes at the current level
-            long long sum = 0;      // Sum of node values at the current level
-            
-            // Process all nodes at the current level
-            while (n--) {
-                TreeNode* node = q.front(); q.pop();   // Get the front node from the queue
-                sum += (long long)node->val;           // Add node's value to the level sum
-                
-                // Push left and right children of the node to the queue (if they exist)
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+    long long kthLargestLevelSum(TreeNode* root, int k) 
+    {
+        // if(!root)
+        // {
+        //     return -1;
+        // }
+        queue<TreeNode*>q;
+        q.push(root);
+        vector<long long>vec;
+        while(q.size())
+        {
+            int size= q.size();
+            long long sum=0;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* temp= q.front();
+                q.pop();
+                sum+=temp->val;
+                if(temp->left)
+                {
+                    q.push(temp->left);
+                }
+                if(temp->right)
+                {
+                    q.push(temp->right);
+                }
             }
-            res.push_back(sum);     // Store the sum of the current level
+            vec.push_back(sum);
         }
-
-        // If k is greater than the number of levels, return -1
-        if (k > res.size()) return -1;
-        
-        // Sort the level sums in descending order
-        sort(res.begin(), res.end(), greater<long long>());
-        
-        // Return the k-th largest level sum (k-1 due to 0-based indexing)
-        return res[k-1];
+        if(k>vec.size())
+        {
+            return -1;
+        }
+        sort(vec.rbegin(),vec.rend());
+        return vec[k-1];
     }
 };
