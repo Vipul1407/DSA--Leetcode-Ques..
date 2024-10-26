3864d4cf791177169a8138677b7f57fc03bd2d7b
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-26 22:14:46 +0530

    Time: 11 ms (98.41%) | Memory: 15.5 MB (90.82%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.cpp
@@ -2,20 +2,43 @@ class Solution {
 public:
     long long minimumSteps(string s) 
     {
-        //left-->right
+        //right-->left
+        //trying to make 0's to left
         long long ans=0;
-        int cnt1=0;
-        for(auto i:s)
+        int cnt0=0;
+        int n=s.size();
+        for(int i=n-1;i>=0;i--)
         {
-            if(i=='1')
+            if(s[i]=='0')
             {
-                cnt1++;
+                cnt0++;
             }
             else
             {
-                ans+=cnt1;
+                ans+=cnt0;
             }
         }
         return ans;
     }
 };
+
+//METHOD-1
+// long long minimumSteps(string s) 
+//     {
+//         //left-->right
+//          //trying to make 1's to right
+//         long long ans=0;
+//         int cnt1=0;
+//         for(auto i:s)
+//         {
+//             if(i=='1')
+//             {
+//                 cnt1++;
+//             }
+//             else
+//             {
+//                 ans+=cnt1;
+//             }
+//         }
+//         return ans;
+//     }
