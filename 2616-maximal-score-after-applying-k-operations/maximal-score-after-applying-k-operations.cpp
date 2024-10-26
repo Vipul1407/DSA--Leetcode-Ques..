3f7db10a191733e6058c0cf7237b4aa8869a2671
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-26 19:15:59 +0530

    Time: 115 ms (90.75%) | Memory: 79.9 MB (18.74%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    long long maxKelements(vector<int>& nums, int k) {
-        ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-        priority_queue<long long> maxheap;
-        for(auto i : nums) maxheap.push(i);
-
-        long long score = 0;
-
-        for(int i = 0; i < k; i++) {
-            int n = maxheap.top();
-            score += n;
-            maxheap.pop();
-            n = ceil((double) n/3);
-            maxheap.push(n);
+    long long maxKelements(vector<int>& nums, int k) 
+    {
+        priority_queue<int>pq;//max heap..
+        long long ans=0;
+        for(auto i:nums)
+        {
+            pq.push(i);
         }
-        return score;
-    }
+        while(pq.size() && k>0)
+        {
+            ans+= pq.top();
+            pq.push(ceil((double)pq.top()/3));
+            pq.pop();
+            k--;
+        }
+        return ans;
+    }
 };
