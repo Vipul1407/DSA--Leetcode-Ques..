5f3f6dcb06d3d7106835b25107b704b6142ed729
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-27 13:45:01 +0530

    Time: 3 ms (22.86%) | Memory: 8.5 MB (59.18%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string reorganizeString(string s) 
+    {
+        vector<int>vec(26,0);
+        for(auto i:s)
+        {
+            vec[i-'a']++;
+        }
+        priority_queue<pair<int,char>>pq;
+        for(int i=0;i<26;i++)
+        {
+            if(vec[i]>0)
+                pq.push({vec[i],i+'a'});
+        }
+        string ans="";
+        //same logic as LONGEST HAPPY STRING QUES..
+        while(pq.size())
+        {
+            auto top= pq.top();
+            pq.pop();
+            int n= ans.size();
+            //cannot again reuse same char..
+            if(n>0 && ans[n-1]==top.second)
+            {
+                if(pq.empty())
+                {
+                    return "";
+                }
+                auto newtop= pq.top();
+                pq.pop();
+                ans+= newtop.second;
+                if(newtop.first-1>0)
+                {
+                    pq.push({newtop.first-1, newtop.second});
+                }
+                pq.push(top);
+            }
+            //can again reuse same char...
+            else
+            {
+                ans+= top.second;
+                if(top.first-1>0)
+                {
+                    pq.push({top.first-1,top.second});
+                }
+            }
+        }
+        return pq.empty()?ans:"";
+    }
+};
