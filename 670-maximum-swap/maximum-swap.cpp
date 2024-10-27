53c9314e0d3e03105dab2defe2873025c8b0ddc6
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-27 22:04:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (35.26%) - LeetSync

--- 670-maximum-swap/maximum-swap.cpp
+++ 670-maximum-swap/maximum-swap.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
+    //METHOD-2
+    //SC=O(1)
     int maximumSwap(int num) 
+    {
+        string str= to_string(num);
+        int n= str.size();
+        vector<int>vec(10,-1);//stroing index
+        //left ---> right traversal
+        for(int i=0;i<n;i++)
+        {
+            vec[str[i]-'0']=i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            int digit= str[i]-'0';
+            for(int j=9;j>digit;j--)
+            {
+                if(vec[j]>i)
+                {
+                    swap(str[i],str[vec[j]]);
+                    return stoi(str);
+                }
+            }
+        }
+        return num;
+    }
+};
+
+//METHOD-1
+//USING SC=O(N)
+/*
+int maximumSwap(int num) 
     {
         string str= to_string(num);
         int n= str.size();
@@ -22,4 +53,4 @@ public:
         }
         return stoi(str);
     }
-};
+*/
