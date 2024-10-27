dcad3a4aceb72377fa86f071bfbaf34dfe657ad8
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-27 12:59:42 +0530

    Time: 2 ms (15.97%) | Memory: 7.7 MB (71.44%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -15,16 +15,8 @@ public:
             auto top=pq.top();
             pq.pop();
             int n= ans.size();
-            //can use curr char..
-            if(n<2 || (ans[n-1]!=top.second || ans[n-2]!=top.second))
-            {
-                ans+= top.second;
-                if(top.first-1>0)
-                {
-                    pq.push({top.first-1, top.second});
-                }
-            }
-            else
+            //cannot use curr char..
+            if(n>=2 && (ans[n-1]==top.second && ans[n-2]==top.second))
             {
                 if(pq.empty())
                 {
@@ -40,7 +32,65 @@ public:
                 //pushing prev top...
                 pq.push(top);
             }
+            else
+            {
+                ans+= top.second;
+                if(top.first-1>0)
+                {
+                    pq.push({top.first-1, top.second});
+                }
+            }
         }
         return ans;
     }
 };
+
+//METHOD-1
+/*
+string longestDiverseString(int a, int b, int c)
+{
+    // max heap..
+    priority_queue<pair<int, char>> pq;
+
+    if (a > 0)
+        pq.push({a, 'a'});
+    if (b > 0)
+        pq.push({b, 'b'});
+    if (c > 0)
+        pq.push({c, 'c'});
+
+    string ans = "";
+    while (pq.size())
+    {
+        auto top = pq.top();
+        pq.pop();
+        int n = ans.size();
+        // can use curr char..
+        if (n < 2 || (ans[n - 1] != top.second || ans[n - 2] != top.second))
+        {
+            ans += top.second;
+            if (top.first - 1 > 0)
+            {
+                pq.push({top.first - 1, top.second});
+            }
+        }
+        else
+        {
+            if (pq.empty())
+            {
+                break;
+            }
+            auto newtop = pq.top();
+            pq.pop();
+            ans += newtop.second;
+            if (newtop.first - 1 > 0)
+            {
+                pq.push({newtop.first - 1, newtop.second});
+            }
+            // pushing prev top...
+            pq.push(top);
+        }
+    }
+    return ans;
+}
+*/
