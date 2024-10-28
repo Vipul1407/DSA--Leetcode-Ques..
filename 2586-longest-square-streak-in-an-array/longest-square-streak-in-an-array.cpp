590398a0b7f94c2c0f2658d3ed15857cee389e9d
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-28 13:24:03 +0530

    Time: 43 ms (88.89%) | Memory: 86.5 MB (96.78%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) 
     {
-        unordered_set<int>st;
-        for(auto i:nums)
-        {
-            st.insert(i);
-        }
+        sort(nums.begin(),nums.end());
+        //applying binary search...
         int ans=1;
         for(auto i:nums)
         {
             int cnt=1;
-            long long curr=i;
-            while(curr*curr<= INT_MAX && st.find(curr*curr)!=st.end())
+            long long curr= i;
+            while(curr*curr<=INT_MAX && binary_search(nums.begin(),nums.end(),curr*curr))
             {
                 curr*=curr;
                 cnt++;
             }
-            ans= max(ans,cnt);
+            ans= max(cnt,ans);
         }
         return ans==1? -1:ans;
     }
