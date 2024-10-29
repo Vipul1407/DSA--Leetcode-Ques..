5bc1cdd84281207931fc4901778f01ab12e54f52
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-29 19:44:10 +0530

    Time: 4 ms (55.11%) | Memory: 18 MB (19.69%) - LeetSync

--- 539-minimum-time-difference/minimum-time-difference.cpp
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-
-    int findMinDifference(std::vector<std::string>& timePoints) {
-        int n = timePoints.size();
-        std::vector<int> times(n);
-        for (int i = 0; i < n; ++i) {
-            int hours = std::stoi(timePoints[i].substr(0, 2));
-            int minutes = std::stoi(timePoints[i].substr(3, 2));
-            times[i] = hours * 60 + minutes;
+    int findMinDifference(vector<string>& time) 
+    {
+        vector<int>arr;
+        for(auto ele:time)
+        {
+            int hr= stoi(ele.substr(0,2));
+            int min= stoi(ele.substr(3));
+            int total= hr*60+min;
+            arr.push_back(total);
         }
-        
-        std::sort(times.begin(), times.end());
-        
-        int minDiff = INT_MAX;
-        
-        for (int i = 0; i < n - 1; ++i) {
-            minDiff = std::min(minDiff, times[i + 1] - times[i]);
+        sort(arr.begin(),arr.end());
+        int diff=1440;
+        int n=arr.size();
+        for(int i=1;i<n;i++)
+        {
+            diff= min(diff,arr[i]-arr[i-1]);
         }
-        
-        minDiff = std::min(minDiff, 1440 + times[0] - times[n - 1]);
-        
-        return minDiff;
+        //24:00=  1440 min--> will see the diff of last element in arr..
+        //==> 1440-arr[n-1] becoz it is clockwise
+        //00:00=  0 min-----> will see the diff from first element in arr..
+        //==> arr[0]-0 becoz it is anticlockwise
+        //adding both of them might be the possible ans..
+        return min(diff, 1440-arr[n-1]+arr[0]);
     }
 };
