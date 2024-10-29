2da0545510ff72027ca12a9595e0e73606bef02b
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-29 10:28:19 +0530

    Time: 8 ms (32.76%) | Memory: 11.8 MB (23.89%) - LeetSync

--- 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.cpp
@@ -1,47 +1,62 @@
 class Solution {
 public:
-    bool parseBoolExpr(string expression) {
-        stack<char> stk;  // Stack to hold characters and operators
-        
-        // Iterate over each character in the expression
-        for (char c: expression) {
-            // Push valid characters (non ')' and non ',') to the stack
-            if (c != ')' && c != ',') stk.push(c);
-            else if (c == ')') {  // When ')' is encountered, evaluate subexpression
-                vector<bool> exp;  // Vector to hold boolean values of the current subexpression
-                
-                // Pop characters until '(' is found, collect 't' or 'f' values
-                while (!stk.empty() && stk.top() != '(') {
-                    char t = stk.top(); 
-                    stk.pop();
-                    if (t == 't') exp.push_back(true);
-                    else exp.push_back(false);
-                }
-                
-                stk.pop();  // Pop the '(' from the stack
-                
-                if (!stk.empty()) {
-                    char t = stk.top();  // Get the operator before '('
-                    stk.pop();
-                    bool v = exp[0];  // Initialize result with the first value
-                    
-                    // Perform the corresponding logical operation
-                    if (t == '&') {  // AND operation: all values must be true
-                        for (bool b: exp) v &= b;
-                    } else if (t == '|') {  // OR operation: at least one value is true
-                        for (bool b: exp) v |= b;
-                    } else {  // NOT operation: negate the first value
-                        v = !v;
-                    }
-                    
-                    // Push the result back to the stack as 't' or 'f'
-                    if (v) stk.push('t');
-                    else stk.push('f');
+    char solve(vector<char>&ans, char oper)
+    {
+        if(oper=='!')
+        {
+            return ans[0]=='t'? 'f':'t';
+        }
+        else if(oper=='&')
+        {
+            for(auto i:ans)
+            {
+                if(i=='f')
+                {
+                    return 'f';
                 }
             }
+            return 't';
         }
-        
-        // Return the final result from the stack
-        return stk.top() == 't' ? true : false;
+        else if(oper=='|')
+        {
+            for(auto i:ans)
+            {
+                if(i=='t')
+                {
+                    return 't';
+                }
+            }
+            return 'f';
+        }
+        return 't';
+    }
+    bool parseBoolExpr(string exp) 
+    {
+        stack<char>st;
+        for(auto i:exp)
+        {
+            if(i==',')
+            {
+                continue;
+            }
+            else if(i==')')
+            {
+                vector<char>ans;
+                while(st.size() && st.top()!='(')
+                {
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+                st.pop();//to pop out '(' bracket
+                char oper= st.top();
+                st.pop();
+                st.push(solve(ans,oper));
+            }
+            else
+            {
+                st.push(i);
+            }
+        }
+        return st.top()=='t'? true:false;
     }
 };
