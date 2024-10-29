163d7c999ac2d60ca0fc77d1fd5808212dceba9b
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-29 14:00:41 +0530

    Time: 34 ms (60.94%) | Memory: 30.4 MB (62.57%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isunique(string curr)
+    {
+        vector<int>unique(26,0);
+        for(auto j:curr)
+        {
+            if(unique[j-'a']>0)
+            {
+                return false;
+            }
+            unique[j-'a']++;
+        }
+        return true;
+    }
+    void solve(int i,string curr,vector<string>& arr,int &maxcnt)
+    {
+        if(isunique(curr))
+        {
+            maxcnt= max((int)curr.size(),maxcnt);
+        }
+        else
+        {
+            return;//marking early return...
+        }
+        for(int j=i;j<arr.size();j++)
+        {
+            solve(j+1,curr+arr[j],arr,maxcnt);
+        }
+    }
+    int maxLength(vector<string>& arr) 
+    {
+        int maxcnt=0;
+        solve(0,"",arr,maxcnt);
+        return maxcnt;
+    }
+};
