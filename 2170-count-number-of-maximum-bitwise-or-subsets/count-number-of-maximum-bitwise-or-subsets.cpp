1d080025576fb5463ba8b5e279c9b2e23ccc80c3
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-30 17:28:11 +0530

    Time: 125 ms (15.56%) | Memory: 312.6 MB (6.51%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    void subset(vector<int>& nums,int i,int temp,int &cnt,int tar)
+    int solve(int i,vector<int>& nums,int temp, int &maxor, vector<vector<int>>&dp)
     {
         if(i==nums.size())
         {
-            if(temp==tar)
-                cnt++;
-            return;
+            if(temp==maxor)
+            {
+                return 1;
+            }
+            else return 0;
         }
-        subset(nums,i+1,temp,cnt,tar);
-        subset(nums,i+1,temp|nums[i],cnt,tar);
+        if(dp[i][temp]!=-1)
+        {
+            return dp[i][temp];
+        }
+        int take= solve(i+1,nums,temp|nums[i],maxor,dp);
+        int notake= solve(i+1,nums,temp,maxor,dp);
+        return dp[i][temp]= take+notake;
     }
     int countMaxOrSubsets(vector<int>& nums) 
     {
-        int tar=0;
+        int n= nums.size();
+        int cnt=0;
+        int maxor= 0;
         for(auto i:nums)
         {
-            tar|=i;
+            maxor|=i;
         }
-        int cnt=0;
-        subset(nums,0,0,cnt,tar);
-        return cnt;
+        int temp=0;
+        vector<vector<int>>dp(n+1,vector<int>(maxor+1,-1));
+        return solve(0,nums,temp,maxor,dp);    
     }
 };
