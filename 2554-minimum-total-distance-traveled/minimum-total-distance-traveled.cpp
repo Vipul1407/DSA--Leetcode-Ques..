bb6947358b0ae7787df6d0e995384e81989e9d3e
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-31 12:56:09 +0530

    Time: 59 ms (65.52%) | Memory: 67.6 MB (25.63%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,5 +1,54 @@
 class Solution {
 public:
+    //BUA
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) 
+    {
+        sort(robot.begin(),robot.end());
+        sort(fact.begin(),fact.end());
+        vector<int>position;
+        for(auto i:fact)
+        {
+            int cnt= i[1];
+            while(cnt--)
+            {
+                position.push_back(i[0]);
+            }
+        }
+        int rs= robot.size();
+        int fs= position.size();
+        vector<vector<long long>>dp(rs+1,vector<long long>(fs+1,1e15));
+        //BASE CASES...
+        //all robots assigned..
+        // if(r>=robot.size())
+        // {
+        //     return 0;
+        // }
+        for(int f=fs;f>=0;f--)
+        {
+            dp[rs][f]=0;
+        }
+        //more requests cannot be assigned.. as robots are left but factories are not left==> dp initialized by 1e15
+        // if(f>= position.size())
+        // {
+        //     return (long long)1e15;
+        // }
+        for(int r=rs-1;r>=0;r--)
+        {
+            for(int f=fs-1;f>=0;f--)
+            {
+                long long inc= abs(robot[r]-position[f])+dp[r+1][f+1];
+                long long notinc= dp[r][f+1];
+                dp[r][f]= min(inc,notinc);
+            }
+        }
+        return dp[0][0];
+    }
+};
+
+
+//TDA...
+//BEATS 20%
+    /*
     long long rec(int r,int f,vector<int>& robot, vector<int>&position, vector<vector<long long>>&dp)
     {
         //all robots assigned..
@@ -38,4 +87,4 @@ public:
         //will apllying 0/1 knapsack logic of include/non-include..
         return rec(0,0,robot,position,dp); 
     }
-};
+    */
