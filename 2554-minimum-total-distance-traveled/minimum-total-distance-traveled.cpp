8d6e80fbda60161f1be0a6273e967977472cc2d5
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-10-31 13:13:32 +0530

    Time: 39 ms (82.76%) | Memory: 13.1 MB (56.88%) - LeetSync

--- 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
+++ 2554-minimum-total-distance-traveled/minimum-total-distance-traveled.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    //BUA
+    //SPACE OPTIMIZED..
     long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) 
     {
         sort(robot.begin(),robot.end());
@@ -16,75 +16,110 @@ public:
         }
         int rs= robot.size();
         int fs= position.size();
-        vector<vector<long long>>dp(rs+1,vector<long long>(fs+1,1e15));
-        //BASE CASES...
-        //all robots assigned..
-        // if(r>=robot.size())
-        // {
-        //     return 0;
-        // }
+
+        vector<long long>curr(fs+1,1e15);
+        vector<long long>next(fs+1,1e15);
         for(int f=fs;f>=0;f--)
         {
-            dp[rs][f]=0;
+            next[f]=0;
         }
-        //more requests cannot be assigned.. as robots are left but factories are not left==> dp initialized by 1e15
-        // if(f>= position.size())
-        // {
-        //     return (long long)1e15;
-        // }
         for(int r=rs-1;r>=0;r--)
         {
             for(int f=fs-1;f>=0;f--)
             {
-                long long inc= abs(robot[r]-position[f])+dp[r+1][f+1];
-                long long notinc= dp[r][f+1];
-                dp[r][f]= min(inc,notinc);
+                long long inc= abs(robot[r]-position[f])+ next[f+1];
+                long long notinc= curr[f+1];
+                curr[f]= min(inc,notinc);
             }
+            next= curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
+
+//BUA...
+//BEATS 65%
+// long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) 
+// {
+//     sort(robot.begin(),robot.end());
+//     sort(fact.begin(),fact.end());
+//     vector<int>position;
+//     for(auto i:fact)
+//     {
+//         int cnt= i[1];
+//         while(cnt--)
+//         {
+//             position.push_back(i[0]);
+//         }
+//     }
+//     int rs= robot.size();
+//     int fs= position.size();
+//     vector<vector<long long>>dp(rs+1,vector<long long>(fs+1,1e15));
+//     //BASE CASES...
+//     //all robots assigned..
+//     // if(r>=robot.size())
+//     // {
+//     //     return 0;
+//     // }
+//     for(int f=fs;f>=0;f--)
+//     {
+//         dp[rs][f]=0;
+//     }
+//     //more requests cannot be assigned.. as robots are left but factories are not left==> dp initialized by 1e15
+//     // if(f>= position.size())
+//     // {
+//     //     return (long long)1e15;
+//     // }
+//     for(int r=rs-1;r>=0;r--)
+//     {
+//         for(int f=fs-1;f>=0;f--)
+//         {
+//             long long inc= abs(robot[r]-position[f])+dp[r+1][f+1];
+//             long long notinc= dp[r][f+1];
+//             dp[r][f]= min(inc,notinc);
+//         }
+//     }
+//     return dp[0][0];
+// }
 
 
 //TDA...
 //BEATS 20%
-    /*
-    long long rec(int r,int f,vector<int>& robot, vector<int>&position, vector<vector<long long>>&dp)
-    {
-        //all robots assigned..
-        if(r>=robot.size())
-        {
-            return 0;
-        }
-        //more requests cannot be assigned.. as robots are left but factories are not left
-        if(f>= position.size())
-        {
-            return (long long)1e15;
-        }
-        if(dp[r][f]!=-1)
-        {
-            return dp[r][f];
-        }
-        long long inc= abs(robot[r]-position[f])+rec(r+1,f+1,robot,position,dp);
-        long long notinc= rec(r,f+1,robot,position,dp);
-        return dp[r][f]= min(inc,notinc);
-    }
-    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) 
-    {
-        sort(robot.begin(),robot.end());
-        sort(fact.begin(),fact.end());
-        vector<int>position;
-        for(auto i:fact) 
-        {
-            int cnt= i[1];
-            int pos= i[0];
-            while(cnt--)
-            {
-                position.push_back(pos);
-            }
-        }
-        vector<vector<long long>>dp(robot.size(),vector<long long>(position.size(),-1));
-        //will apllying 0/1 knapsack logic of include/non-include..
-        return rec(0,0,robot,position,dp); 
-    }
-    */
+// long long rec(int r,int f,vector<int>& robot, vector<int>&position, vector<vector<long long>>&dp)
+// {
+//     //all robots assigned..
+//     if(r>=robot.size())
+//     {
+//         return 0;
+//     }
+//     //more requests cannot be assigned.. as robots are left but factories are not left
+//     if(f>= position.size())
+//     {
+//         return (long long)1e15;
+//     }
+//     if(dp[r][f]!=-1)
+//     {
+//         return dp[r][f];
+//     }
+//     long long inc= abs(robot[r]-position[f])+rec(r+1,f+1,robot,position,dp);
+//     long long notinc= rec(r,f+1,robot,position,dp);
+//     return dp[r][f]= min(inc,notinc);
+// }
+// long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& fact) 
+// {
+//     sort(robot.begin(),robot.end());
+//     sort(fact.begin(),fact.end());
+//     vector<int>position;
+//     for(auto i:fact) 
+//     {
+//         int cnt= i[1];
+//         int pos= i[0];
+//         while(cnt--)
+//         {
+//             position.push_back(pos);
+//         }
+//     }
+//     vector<vector<long long>>dp(robot.size(),vector<long long>(position.size(),-1));
+//     //will apllying 0/1 knapsack logic of include/non-include..
+//     return rec(0,0,robot,position,dp); 
+// }
