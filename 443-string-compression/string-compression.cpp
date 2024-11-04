8cb5a3f29059f2c45504e7b2c1577094645e5c03
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-04 20:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (33.81%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -6,9 +6,9 @@ public:
         int i=0;//original index..
         int k=0;//updated index..
         int cnt=1;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<=n;i++)
         {
-            if(arr[i-1]==arr[i])
+            if(i<n && arr[i-1]==arr[i])
             {
                 cnt++;
             }
@@ -26,16 +26,7 @@ public:
                 cnt=1;//reinitialized to 1
             }
         }
-        //handling last char..
-        arr[k++]= arr[n-1];
-        if(cnt>1)
-        {
-            string num= to_string(cnt);
-            for(auto d:num)
-            {
-                arr[k++]= d;
-            }
-        }
+        //handling last char..will not req now as we have handled it by <=n in for loop
         return k;//new arr size
     }
 };
