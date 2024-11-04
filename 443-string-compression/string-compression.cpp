9efe62984c8d8b0ce76cf95642132dbe1731b9ea
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-04 19:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (68.00%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int compress(vector<char>& arr) 
+    {
+        int n= arr.size();
+        int i=0;//original index..
+        int k=0;//updated index..
+        int cnt=1;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i-1]==arr[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                arr[k++]= arr[i-1];//if cnt==1 then we have to only add the char not count
+                if(cnt>1)
+                {
+                    string num= to_string(cnt);
+                    for(auto d:num)
+                    {
+                        arr[k++]= d;
+                    }
+                }
+                cnt=1;//reinitialized to 1
+            }
+        }
+        //handling last char..
+        arr[k++]= arr[n-1];
+        if(cnt>1)
+        {
+            string num= to_string(cnt);
+            for(auto d:num)
+            {
+                arr[k++]= d;
+            }
+        }
+        return k;//new arr size
+    }
+};
