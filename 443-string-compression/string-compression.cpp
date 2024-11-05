1c209ae7a521a2aaa394a102eb21261e3814923f
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-05 13:02:42 +0530

    Time: 3 ms (17.48%) | Memory: 13.8 MB (87.08%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -6,25 +6,23 @@ public:
         int i=0;//original index..
         int k=0;//updated index..
         int cnt=1;
-        for(int i=1;i<=n;i++)
+        for(i=1;i<=n;i++)
         {
-            if(i<n && arr[i-1]==arr[i])
+            while(i<n && arr[i-1]==arr[i])
             {
                 cnt++;
+                i++;
             }
-            else
+            arr[k++]= arr[i-1];//if cnt==1 then we have to only add the char not count
+            if(cnt>1)
             {
-                arr[k++]= arr[i-1];//if cnt==1 then we have to only add the char not count
-                if(cnt>1)
+                string num= to_string(cnt);
+                for(auto d:num)
                 {
-                    string num= to_string(cnt);
-                    for(auto d:num)
-                    {
-                        arr[k++]= d;
-                    }
+                    arr[k++]= d;
                 }
-                cnt=1;//reinitialized to 1
             }
+            cnt=1;//reinitialized to 1
         }
         //handling last char..will not req now as we have handled it by <=n in for loop
         return k;//new arr size
