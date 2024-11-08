f7f7638e6cffe8d0670eab80cbd68c1ccf29160d
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-08 23:06:09 +0530

    Time: 7 ms (34.29%) | Memory: 95.5 MB (63.99%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.cpp
@@ -5,20 +5,15 @@ public:
         int n= nums.size();
         //max xor that we can get is by flipping the bits
         int xorvalue=0;
-        for(auto i:nums)
-        {
-            xorvalue^=i;
-        }
-        vector<int>ans;
+    
+        vector<int>ans(n);
         int mask= (1<<mb)-1;
         //to get max result of xor when we xor the bits with its flip bits
         //now to get flip we will xor the bits with mask
-        int idx=n-1;
-        while(idx>=0)
+        for(int i=0;i<n;i++)
         {
-            ans.push_back(xorvalue^mask);
-            xorvalue^=nums[idx];//to remove that element from its xor
-            idx--;
+            xorvalue^=nums[i];
+            ans[n-i-1]= xorvalue^mask;
         }
         return ans;
     }
