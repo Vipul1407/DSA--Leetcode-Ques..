fe77304ca9fe7e11e15cd02a347762170d0b8b4e
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-09 12:41:36 +0530

    Time: 3 ms (31.25%) | Memory: 9.5 MB (12.59%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.cpp
+++ 3394-minimum-array-end/minimum-array-end.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    long long minEnd(int n, int x) {
+        vector<int> shifts;  // Positions of 0-bits in x
+        long long cur = x;   // Start with the value of x
+        long long calc = n - 1;
+        
+        // Store all 0-bit positions of x up to the maximum necessary position.
+        for(int i = 0; i < 64; i++) {
+            if (!((1LL << i) & x)) {  // Use long long for shifting to prevent overflow
+                shifts.push_back(i);
+            }
+        }
+        
+        // Increment cur using positions in shifts based on bits in calc
+        for(int i = 0; calc > 0; i++, calc >>= 1) {
+            if (calc & 1) {
+                cur += (1LL << shifts[i]);  // Use long long for shifting to avoid overflow
+            }
+        }
+        return cur;
+    }
+};
+
+/*
+class Solution {
+public:
     long long minEnd(int n, int x) 
     {
         //to get the next value whose and value is same as x
@@ -18,3 +43,4 @@ public:
         return ans;
     }
 };
+*/
