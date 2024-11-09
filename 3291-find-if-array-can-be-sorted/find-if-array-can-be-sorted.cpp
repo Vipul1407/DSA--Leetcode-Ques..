cd1395e4fc377cea81e0a9e6425167c011c612b0
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-09 15:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (55.48%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,46 +1,55 @@
-class Solution 
-{
+class Solution {
 public:
-
-    bool setbit(int num1, int num2) {
-        int cnt1 = 0, cnt2 = 0;
-        while (num1 > 0) {
-            cnt1 += (num1 & 1);
-            num1 >>= 1;
+    bool equalsetbit(int a,int b)
+    {
+        int cnt1=0;
+        int cnt2=0;
+        while(a>0)
+        {
+            if(1 & a)
+            {
+                cnt1++;
+            }
+            a>>=1;//a= a>>1
         }
-        while (num2 > 0) {
-            cnt2 += (num2 & 1);
-            num2 >>= 1;
+        while(b>0)
+        {
+            if(1 & b)
+            {
+                cnt2++;
+            }
+            b>>=1;//b= b>>1
         }
-        return cnt1 == cnt2;
+        return cnt1==cnt2;//return true if they have same no. of set bits
     }
-
     bool canSortArray(vector<int>& nums) 
     {
-        int n = nums.size();
-
-        int maxi = nums[0];
-        int mini = nums[0];
-        int prevmaxi= INT_MIN;
-
-        for (int i = 1; i < n; ++i) 
+        int prevmaxi= -1;//INT_MIN
+        int maxi= nums[0];
+        int mini= nums[0];
+        int n= nums.size();
+        for(int i=1;i<n;i++)
         {
-            if (setbit(nums[i - 1], nums[i])) 
+            //same group..update maxi,mini
+            if(equalsetbit(nums[i-1],nums[i]))
             {
                 maxi= max(maxi,nums[i]);
                 mini= min(mini,nums[i]);
-            } 
-            else 
+            }
+            //different group..start new maxi,mini and update prevmaxi= maxi
+            else
             {
-                if (mini<prevmaxi) 
+                // array will we sorted when prevmaxi<mini
+                if(prevmaxi>mini)
                 {
                     return false;
-                } 
+                }
                 prevmaxi= maxi;
-                maxi=nums[i];
+                maxi= nums[i];
                 mini= nums[i];
             }
         }
+        //to handle for last element
         return mini>prevmaxi;
     }
 };
