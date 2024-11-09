535b34370fb2c4e39a461f208c5a5ab7cf0097d3
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-09 16:53:47 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (94.42%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -6,19 +6,13 @@ public:
         int cnt2=0;
         while(a>0)
         {
-            if(1 & a)
-            {
-                cnt1++;
-            }
-            a>>=1;//a= a>>1
+            a&=(a-1);//a= a&(a-1);
+            cnt1++;
         }
         while(b>0)
         {
-            if(1 & b)
-            {
-                cnt2++;
-            }
-            b>>=1;//b= b>>1
+            b&=(b-1);//b= b&(b-1);
+            cnt2++;
         }
         return cnt1==cnt2;//return true if they have same no. of set bits
     }
@@ -53,3 +47,52 @@ public:
         return mini>prevmaxi;
     }
 };
+
+/*
+class Solution 
+{
+public:
+
+    bool setbit(int num1, int num2) {
+        int cnt1 = 0, cnt2 = 0;
+        while (num1 > 0) {
+            cnt1 += (num1 & 1);
+            num1 >>= 1;
+        }
+        while (num2 > 0) {
+            cnt2 += (num2 & 1);
+            num2 >>= 1;
+        }
+        return cnt1 == cnt2;
+    }
+
+    bool canSortArray(vector<int>& nums) 
+    {
+        int n = nums.size();
+
+        int maxi = nums[0];
+        int mini = nums[0];
+        int prevmaxi= INT_MIN;
+
+        for (int i = 1; i < n; ++i) 
+        {
+            if (setbit(nums[i - 1], nums[i])) 
+            {
+                maxi= max(maxi,nums[i]);
+                mini= min(mini,nums[i]);
+            } 
+            else 
+            {
+                if (mini<prevmaxi) 
+                {
+                    return false;
+                } 
+                prevmaxi= maxi;
+                maxi=nums[i];
+                mini= nums[i];
+            }
+        }
+        return mini>prevmaxi;
+    }
+};
+*/
