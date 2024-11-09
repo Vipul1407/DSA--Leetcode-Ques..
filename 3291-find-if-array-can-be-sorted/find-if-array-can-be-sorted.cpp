f8734ceb3355746d6b8bcc3e5de92b5e3b30b4f4
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-09 17:40:46 +0530

    Time: 7 ms (28.00%) | Memory: 31.1 MB (94.42%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -1,98 +1,61 @@
 class Solution {
 public:
-    bool equalsetbit(int a,int b)
+    bool equalsetbit(int num1, int num2)
     {
-        int cnt1=0;
-        int cnt2=0;
-        while(a>0)
+        int cnt1 = 0;
+        int cnt2 = 0;
+        while (num1 > 0)
         {
-            a&=(a-1);//a= a&(a-1);
-            cnt1++;
-        }
-        while(b>0)
-        {
-            b&=(b-1);//b= b&(b-1);
-            cnt2++;
-        }
-        return cnt1==cnt2;//return true if they have same no. of set bits
-    }
-    bool canSortArray(vector<int>& nums) 
-    {
-        int prevmaxi= -1;//INT_MIN
-        int maxi= nums[0];
-        int mini= nums[0];
-        int n= nums.size();
-        for(int i=1;i<n;i++)
-        {
-            //same group..update maxi,mini
-            if(equalsetbit(nums[i-1],nums[i]))
+            if (num1 & 1)
             {
-                maxi= max(maxi,nums[i]);
-                mini= min(mini,nums[i]);
+                cnt1++;
             }
-            //different group..start new maxi,mini and update prevmaxi= maxi
-            else
+            num1 >>= 1; // right shift by 1
+        }
+        while (num2 > 0)
+        {
+            if (num2 & 1)
             {
-                // array will we sorted when prevmaxi<mini
-                if(prevmaxi>mini)
-                {
-                    return false;
-                }
-                prevmaxi= maxi;
-                maxi= nums[i];
-                mini= nums[i];
+                cnt2++;
             }
-        }
-        //to handle for last element
-        return mini>prevmaxi;
-    }
-};
-
-/*
-class Solution 
-{
-public:
-
-    bool setbit(int num1, int num2) {
-        int cnt1 = 0, cnt2 = 0;
-        while (num1 > 0) {
-            cnt1 += (num1 & 1);
-            num1 >>= 1;
-        }
-        while (num2 > 0) {
-            cnt2 += (num2 & 1);
-            num2 >>= 1;
+            num2 >>= 1; // right shift by 1
         }
         return cnt1 == cnt2;
     }
-
+    //BUBBLE SORT..
     bool canSortArray(vector<int>& nums) 
     {
-        int n = nums.size();
-
-        int maxi = nums[0];
-        int mini = nums[0];
-        int prevmaxi= INT_MIN;
-
-        for (int i = 1; i < n; ++i) 
+        int n=nums.size();
+        for(int i=0;i<n;i++)
         {
-            if (setbit(nums[i - 1], nums[i])) 
+            int flag=1;
+            for(int j=0;j<n-i-1;j++)
             {
-                maxi= max(maxi,nums[i]);
-                mini= min(mini,nums[i]);
-            } 
-            else 
-            {
-                if (mini<prevmaxi) 
+                if(nums[j]<=nums[j+1])
                 {
-                    return false;
-                } 
-                prevmaxi= maxi;
-                maxi=nums[i];
-                mini= nums[i];
+                    continue;
+                }
+                //nums[j]>nums[j+1]
+                else
+                {
+                    if(equalsetbit(nums[j],nums[j+1]))
+                    {
+                        swap(nums[j],nums[j+1]);
+                        flag=0;
+                    }
+                    else
+                    {
+                        //we cannot swap hence array cannot be sorted..
+                        return false;
+                    }
+                }
+            }
+            //array is already sorted..
+            if(flag==1)
+            {
+                break;
             }
         }
-        return mini>prevmaxi;
+        return true;
     }
 };
-*/
