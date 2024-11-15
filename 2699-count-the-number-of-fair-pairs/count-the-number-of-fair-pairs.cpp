4ae39cb88d57d83dae1ce62cafdcdbc6af9acd7a
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-11-16 01:02:49 +0530

    Time: 53 ms (75.43%) | Memory: 60.4 MB (18.69%) - LeetSync

--- 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
     //lower bound gives smallest idx of >=x
+    int lb(int i,int ele,vector<int>& nums)
+    {
+        int s=i+1;
+        int e= nums.size()-1;
+        while(s<=e)
+        {
+            int mid= (s+e)/2;
+            if(nums[mid]>=ele)
+            {
+                e= mid-1;
+            }
+            else
+            {
+                s= mid+1;
+            }
+        }
+        return s;
+    }
     //upper bound gives smallest idx of >x
-
+    int ub(int i,int ele,vector<int>& nums)
+    {
+        int s=i+1;
+        int e= nums.size()-1;
+        while(s<=e)
+        {
+            int mid= (s+e)/2;
+            if(nums[mid]>ele)
+            {
+                e= mid-1;
+            }
+            else
+            {
+                s= mid+1;
+            }
+        }
+        return s;
+    }
     long long countFairPairs(vector<int>& nums, int lower, int upper) 
     {
         sort(nums.begin(),nums.end());
@@ -17,9 +52,9 @@ public:
         for(int i=0;i<n-1;i++)
         {
             //ub and lb will start from their next index.. 
-            int ub= upper_bound(nums.begin()+i+1,nums.end(),upper-nums[i])-nums.begin();
-            int lb= lower_bound(nums.begin()+i+1,nums.end(),lower-nums[i])-nums.begin();
-            ans+= (ub-lb);
+            int upperb= ub(i,upper-nums[i],nums);
+            int lowerb= lb(i,lower-nums[i],nums);
+            ans+= (upperb-lowerb);
         }
         return ans;
     }
