2b1e31e0754438e6fb8fda1e460235c1caf09b40
Vipul1407 <129517280+Vipul1407@users.noreply.github.com>
2024-12-01 15:09:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (16.30%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) 
     {
+        unordered_set<int>st;
+        for(auto i:arr)
+        {
+            if(st.find(2*i)!=st.end() || (i%2==0 && st.find(i/2)!=st.end()))
+            {
+                return true;
+            }
+            st.insert(i);
+        }
+        return false;
+    }
+};
+/*
+bool checkIfExist(vector<int>& arr) 
+    {
         unordered_set<int>st(arr.begin(),arr.end());
         int cnt0=0;
         for(auto i:arr)
@@ -20,4 +35,4 @@ public:
         }
         return cnt0>1 ? true:false;
     }
-};
+*/
