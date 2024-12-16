306c5b61668325f1265954e5edcb451eb674397c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-16 10:36:37 +0530

    Time: 35 ms (44.56%) | Memory: 59.7 MB (57.98%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    int minimumSize(vector<int>& nums, int k) {
-        const int n=nums.size();
-    //    sort(nums.begin(), nums.end());
-        int l=1, r=*max_element(nums.begin(), nums.end()), m;
-        while(l<r){
-            m=l+(r-l)/2;
-            long long cnt=0;
-            for(int j=0; j<n && cnt<=k; j++)
-                cnt+=(nums[j]-1)/m;
-            if(cnt<=k) r=m;
-            else l=m+1;
+    bool possi(vector<int>& nums, int k, int mid)
+    {
+        int op=0;
+        for(auto i:nums)
+        {
+            op+= i/mid;
+            if(i%mid==0)
+            {
+                op--;
+            }
         }
-        return r;
+        return op<=k;
+    }
+    int minimumSize(vector<int>& nums, int k) 
+    {
+        int l=1;
+        int r= *max_element(nums.begin(),nums.end());
+        int ans=r;
+        while(l<=r)    
+        {
+            int mid= l+(r-l)/2;
+            if(possi(nums,k,mid))
+            {
+                ans= mid;
+                r= mid-1;//to get min ans
+            }
+            else
+            {
+                l= mid+1;
+            }
+        }
+        return ans;
     }
 };
