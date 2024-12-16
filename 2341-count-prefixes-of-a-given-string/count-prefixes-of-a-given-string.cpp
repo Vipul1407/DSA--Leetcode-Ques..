beefefbd70f134f2777f73254f6356af61435e0a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-16 14:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (92.59%) - LeetSync

---
+++ 2341-count-prefixes-of-a-given-string/count-prefixes-of-a-given-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) 
+    {
+        int cnt=0;
+        for(auto i:words)
+        {
+            if(s.find(i)==0)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
