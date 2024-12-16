3ff44f9a4865fb96846b15da1a86cc5d211a5f52
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-16 14:23:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (23.94%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    //USING STRING STREAM
+    //USING Stringstream + Getine function..
     int isPrefixOfWord(string str, string search) 
     {
         stringstream ss(str);
         int start=1;
         string word;
-        while(ss>>word)
+        while(getline(ss,word,' '))
         {
             if(word.find(search)==0)
             {
@@ -18,43 +18,60 @@ public:
     }
 };
 
-//BEATS 100%
 /*
-class Solution {
-public:
-    int isPrefixOfWord(string str, string search) 
+//METHOD-2
+//BEATS 100%
+// Using stringstream
+int isPrefixOfWord(string str, string search)
+{
+    stringstream ss(str);
+    int start = 1;
+    string word;
+    while (ss >> word)
     {
-        int n= str.size();
-        int m= search.size();
-        int k=0;
-        int start=1;//to track word index in sentence..
-        bool neww= true;
-        for(int i=0;i<n;i++)
+        if (word.find(search) == 0)
         {
-            if(str[i]==' ')
+            return start;
+        }
+        start++;
+    }
+    return -1;
+}
+
+//METHOD-1
+// BEATS 100%
+int isPrefixOfWord(string str, string search) 
+{
+    int n= str.size();
+    int m= search.size();
+    int k=0;
+    int start=1;//to track word index in sentence..
+    bool neww= true;
+    for(int i=0;i<n;i++)
+    {
+        if(str[i]==' ')
+        {
+            k=0;
+            neww= true;
+            start++;
+        }
+        else if(neww)
+        {
+            if(str[i]==search[k])
+            {
+                k++;
+                if(k>=m)
+                {
+                    return start;
+                }
+            }
+            else
             {
                 k=0;
-                neww= true;
-                start++;
-            }
-            else if(neww)
-            {
-                if(str[i]==search[k])
-                {
-                    k++;
-                    if(k>=m)
-                    {
-                        return start;
-                    }
-                }
-                else
-                {
-                    k=0;
-                    neww= false;
-                }
+                neww= false;
             }
         }
-        return -1;
     }
-};
+    return -1;
+}
 */
