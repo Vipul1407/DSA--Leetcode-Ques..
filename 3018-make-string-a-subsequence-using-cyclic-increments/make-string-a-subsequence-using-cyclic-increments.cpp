e8277f76601eaf59519178eae84d0f1162a0dc3b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-17 23:33:54 +0530

    Time: 7 ms (37.01%) | Memory: 17.1 MB (65.66%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -7,7 +7,7 @@ public:
         int n= str1.size();
         while(i<n)
         {
-            if(str1[i]==str2[j] || ((str1[i]-'a'+1)%26+'a' == str2[j]) || ((str1[i]-'a'-25)%26+'a' == str2[j]))
+            if(str1[i]==str2[j] || (str1[i]+1 == str2[j]) || (str1[i]-25 == str2[j]))
             {
                 j++;
             }
@@ -16,3 +16,37 @@ public:
         return j== str2.size();
     }
 };
+/*
+
+// METHOD-1
+bool canMakeSubsequence(string str1, string str2)
+{
+    int i = 0;
+    int j = 0;
+    int n = str1.size();
+    while (i < n)
+    {
+        if (str1[i] == str2[j] || ((str1[i] - 'a' + 1) % 26 + 'a' == str2[j]) || ((str1[i] - 'a' - 25) % 26 + 'a' == str2[j]))
+        {
+            j++;
+        }
+        i++;
+    }
+    return j == str2.size();
+}
+
+// METHOD-2
+bool canMakeSubsequence(string source, string target)
+{
+    int targetIdx = 0;
+    int targetLen = target.length();
+    for (char currChar : source)
+    {
+        if (targetIdx < targetLen && (target[targetIdx] - currChar + 26) % 26 <= 1)
+        {
+            targetIdx++;
+        }
+    }
+    return targetIdx == targetLen;
+}
+*/
