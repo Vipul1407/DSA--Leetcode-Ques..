6eef3bf8a13cecb76c0624294d019b8e3e4ddcf0
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-17 22:23:00 +0530

    Time: 178 ms (53.20%) | Memory: 177.8 MB (65.37%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int maxCount(vector<int>& banned, int n, int maxSum) {
-        // Create a set to mark banned numbers
-        unordered_set<int> bannedSet(banned.begin(), banned.end());
-        
-        long long sum = 0; // Track cumulative sum
-        int count = 0;     // Track count of valid numbers
-
-        // Iterate through 1 to n
-        for (int i = 1; i <= n; i++) {
-            if (bannedSet.count(i)) continue; // Skip banned numbers
-            sum += i; // Add current number to the sum
-            if (sum > maxSum) break; // Stop if sum exceeds maxSum
-            count++; // Increment count of valid numbers
+    int maxCount(vector<int>& banned, int n, int k) 
+    {
+        unordered_set<int>st(banned.begin(),banned.end());
+        long sum=0;
+        int cnt=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(st.find(i)==st.end())
+            {
+                sum+=i;
+                if(sum<=k)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    return cnt;
+                }
+            }
         }
-
-        return count; // Return the count of valid numbers
+        return cnt;
     }
 };
