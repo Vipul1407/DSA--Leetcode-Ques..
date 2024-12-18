756b3b163f2b351c6ee984c52345e57812ff0df0
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-18 21:19:07 +0530

    Time: 39 ms (35.22%) | Memory: 136.4 MB (20.06%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
+    //METHOD-2
+    //Making pref arr of end valid range
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& quer) 
+    {
+        int n= nums.size();
+        vector<int>pref(n,n-1);//to get pref[n-1]=n-1
+        int j=0;
+        for(int i=0;i<n-1;i++)
+        {
+            //getting a valid ending index range for index i
+            if(j<i)
+            {
+                j=i;
+            }
+            while(j+1<n && nums[j]%2 != nums[j+1]%2)
+            {
+                j++;
+            }
+            pref[i]= j;
+        }
+        //pref is storing ending valid rnage for starting index i
+        //pref[2]=5 means start=2 valid range is upto 5
+        vector<bool>res;
+        for(auto i:quer)
+        {
+            //i[1]= ending range of query
+            //i[0]= start of quer
+            //if this comes under our stored range i.e <= (ending valid range..)pref[start]
+            if(i[1]<=pref[i[0]])
+            {
+                res.push_back(true);
+            }
+            else
+            {
+                res.push_back(false);
+            }
+        }
+        return res;
+    }
+};
+/*
+class Solution {
+public:
     //METHOD-1
+    //Making pref arr of 0 and 1
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& quer) 
     {
         int n= nums.size();
@@ -32,3 +76,4 @@ public:
         return res;    
     }
 };
+*/
