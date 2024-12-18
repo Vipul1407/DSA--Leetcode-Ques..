d15051a0c60435bf0d42e17eb212d318618fa929
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-18 19:15:28 +0530

    Time: 37 ms (36.01%) | Memory: 136.3 MB (20.06%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> prefix(n, 0); // Initialize prefix array with size n
-        
-        // Calculate the prefix array
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1];
-            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
-                prefix[i]++;
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& quer) 
+    {
+        int n= nums.size();
+        vector<int>pref(n,0);
+    
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]%2==0 && nums[i-1]%2==0)
+            {
+                pref[i]=pref[i-1]+1;
+            }
+            else if(nums[i]%2==1 && nums[i-1]%2==1)
+            {
+                pref[i]=pref[i-1]+1;
+            }
+            else
+            {
+                pref[i]=pref[i-1];
             }
         }
-
-        vector<bool> ans; // Result vector
-        
-        // Process the queries
-        for (auto& q : queries) {
-            int left = q[0], right = q[1];
-            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
-            ans.push_back(specialCount == 0);
+        vector<bool>res;
+        for(auto i:quer)
+        {
+            if(pref[i[1]]-pref[i[0]]==0)
+            {
+                res.push_back(true);
+            }
+            else
+            {
+                res.push_back(false);
+            }
         }
-
-        return ans; // Return the result
+        return res;    
     }
 };
