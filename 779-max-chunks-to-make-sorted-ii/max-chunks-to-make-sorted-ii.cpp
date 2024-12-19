de51a5bcbcdfb9faedc1c7d03562597b455f164a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-19 14:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (39.41%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
+    //METHOD-2
+    //MONOTONIC STACK
     int maxChunksToSorted(vector<int>& arr) 
     {
-        int n= arr.size();
-        vector<int>leftmax(n,INT_MIN);
-        leftmax[0]= arr[0];
-        vector<int>rightmin(n,INT_MAX);
-        rightmin[n-1]= arr[n-1];
-        for(int i=1;i<n;i++)
+        stack<int>st;
+        int largest= INT_MIN;
+        
+        for(int i:arr)
         {
-            leftmax[i]= max(leftmax[i-1],arr[i]);
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            rightmin[i]= min(rightmin[i+1],arr[i]);
-        }
-        int maxchunks=0;
-        for(int i=0;i<n-1;i++)
-        {
-            if(leftmax[i] <= rightmin[i+1])
+            largest= max(largest, i);
+            while(st.size() && st.top()>i)
             {
-                maxchunks++;
+                st.pop();
             }
+            st.push(largest);
         }
-        return maxchunks+1;
+        return st.size();
     }
 };
+
+/*
+
+// METHOD-1
+// PREFIX SUM
+int maxChunksToSorted(vector<int> &arr)
+{
+    int n = arr.size();
+    vector<int> leftmax(n, INT_MIN);
+    leftmax[0] = arr[0];
+    vector<int> rightmin(n, INT_MAX);
+    rightmin[n - 1] = arr[n - 1];
+    for (int i = 1; i < n; i++)
+    {
+        leftmax[i] = max(leftmax[i - 1], arr[i]);
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightmin[i] = min(rightmin[i + 1], arr[i]);
+    }
+    int maxchunks = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        // until leftmax is greater equal to rightmin, we know that there exist a element on right that must comes in left chunk
+        //  so when when we got less than equal means that now we can form a chunk
+        if (leftmax[i] <= rightmin[i + 1])
+        {
+            maxchunks++;
+        }
+    }
+    return maxchunks + 1;
+}
+*/
