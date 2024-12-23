c4d4c5b5daea8ef17327727592d26c203dd6faba
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-23 18:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (82.04%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -11,22 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* reverseOddLevels(TreeNode* root) {
-        DFS(root->left, root->right, 0);
-        return root;
+    TreeNode* dfs(TreeNode* ll, TreeNode* rr, int level)
+    {
+        if(!ll || !rr)
+        {
+            return NULL;
+        }
+        if(level%2!=0)
+        {
+           swap(ll->val,rr->val);
+        }
+        dfs(ll->left,rr->right,level+1);
+        dfs(ll->right,rr->left,level+1);
+        return NULL;
     }
-
-private:
-    void DFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
-        if (leftChild == nullptr || rightChild == nullptr) {
-            return;
-        }
-        if (level % 2 == 0) {
-            int temp = leftChild->val;
-            leftChild->val = rightChild->val;
-            rightChild->val = temp;
-        }
-        DFS(leftChild->left, rightChild->right, level + 1);
-        DFS(leftChild->right, rightChild->left, level + 1);
+    TreeNode* reverseOddLevels(TreeNode* root) 
+    {
+        dfs(root->left,root->right,1);  
+        return root;
     }
 };
