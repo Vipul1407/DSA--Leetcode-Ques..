9d49cfa8466a3292111f2a7982e4f0031e459963
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-23 22:19:14 +0530

    Time: 12 ms (40.22%) | Memory: 10.7 MB (63.83%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -3,6 +3,41 @@ public:
     int maximumLength(string s) 
     {
         int n= s.length();
+        map<pair<char,int>,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            char ch=s[i];
+            int cnt=0;
+            for(int j=i;j<n;j++)
+            {
+               if(s[j]==ch) 
+               {
+                    cnt++;
+               }
+               else
+               {
+                    break;
+               }
+               mp[{ch,cnt}]++;
+            }
+        }
+        int maxlen= -1;
+        for(auto i:mp)
+        {
+            if(i.second>=3)
+            {
+                maxlen= max(maxlen,i.first.second);
+            }
+        }
+        return maxlen;
+    }
+};
+/*
+class Solution {
+public:
+    int maximumLength(string s) 
+    {
+        int n= s.length();
         unordered_map<string,int>mp;
         for(int i=0;i<n;i++)
         {
@@ -32,3 +67,4 @@ public:
         return maxcnt;
     }
 };
+*/
