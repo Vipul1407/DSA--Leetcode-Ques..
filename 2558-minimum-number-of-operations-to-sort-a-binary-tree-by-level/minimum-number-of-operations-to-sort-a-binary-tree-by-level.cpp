2a38200434922be6b79de40bc791dc36ae672473
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-23 17:15:40 +0530

    Time: 180 ms (31.08%) | Memory: 225.3 MB (6.45%) - LeetSync

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -11,60 +11,111 @@
  */
 class Solution {
 public:
-    int cnt=0;
-    void solve(TreeNode* root)
+    //DFS..
+    void dfs(TreeNode* root, int level, vector<vector<int>>&ans)
     {
         if(!root)
         {
             return;
         }
-        queue<TreeNode*>q;
-        q.push(root);
-        while(!q.empty())
+        if(level==ans.size())
         {
-            vector<int>ans;
-            int n= q.size();
-            for(int i=0;i<n;i++)
-            {
-                auto temp= q.front();
-                ans.push_back(temp->val);
-                if(temp->left)
-                {
-                    q.push(temp->left);
-                }
-                if(temp->right)
-                {
-                    q.push(temp->right);
-                }
-                q.pop();    
-            }
-            //here ans contains all nodes at a level
-            vector<int>sorted= ans;
-            sort(sorted.begin(),sorted.end());
+            ans.push_back({});
+        }
+        ans[level].push_back(root->val);
+        dfs(root->left,level+1,ans);
+        dfs(root->right,level+1,ans);
+    }
+    int minimumOperations(TreeNode* root) 
+    {
+        int cnt=0;
+        vector<vector<int>>ans;
+        int level=0;
+        dfs(root,level,ans);
+        for(vector<int>vec: ans)
+        {
             unordered_map<int,int>mp;
-            int m= ans.size();
+            vector<int>sorted= vec;
+            sort(sorted.begin(),sorted.end());
+            int m= sorted.size();
             for(int i=0;i<m;i++)
             {
-                mp[ans[i]]= i; 
+                mp[vec[i]]=i;
             }
             for(int i=0;i<m;i++)
             {
-                if(ans[i]!=sorted[i])
+                if(vec[i]!=sorted[i])
                 {
                     int j= mp[sorted[i]];
-                    //updating the map indexes...
-                    mp[ans[i]]=j;
-                    mp[ans[j]]=i;
-                    //swapping after that..
-                    swap(ans[i],ans[j]);
+                    mp[vec[i]]=j;
+                    mp[vec[j]]=i;
+                    swap(vec[i],vec[j]);
                     cnt++;
                 }
             }
         }
-    }
-    int minimumOperations(TreeNode* root) 
-    {
-        solve(root);
         return cnt;
     }
 };
+
+/*
+// METHOD-1
+// BFS..
+// BEATS 40%
+void solve(TreeNode *root, int &cnt)
+{
+    if (!root)
+    {
+        return;
+    }
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        vector<int> ans;
+        int n = q.size();
+        for (int i = 0; i < n; i++)
+        {
+            auto temp = q.front();
+            ans.push_back(temp->val);
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+            q.pop();
+        }
+        // here ans contains all nodes at a level
+        vector<int> sorted = ans;
+        sort(sorted.begin(), sorted.end());
+        unordered_map<int, int> mp;
+        int m = ans.size();
+        for (int i = 0; i < m; i++)
+        {
+            mp[ans[i]] = i;
+        }
+        for (int i = 0; i < m; i++)
+        {
+            if (ans[i] != sorted[i])
+            {
+                int j = mp[sorted[i]];
+                // updating the map indexes...
+                mp[ans[i]] = j;
+                mp[ans[j]] = i;
+                // swapping after that..
+                swap(ans[i], ans[j]);
+                cnt++;
+            }
+        }
+    }
+}
+int minimumOperations(TreeNode *root)
+{
+    int cnt = 0;
+    solve(root, cnt);
+    return cnt;
+}
+*/
