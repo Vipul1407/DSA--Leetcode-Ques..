1688c1dfcee26ecee4452142855afc3b1138089b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-25 16:17:33 +0530

    Time: 219 ms (13.10%) | Memory: 208.9 MB (7.01%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,6 +1,51 @@
+//MERRY CHRISTMAS!! 25-DEC-2024 ~Vipul
 class Solution {
 public:
+    //METHOD-2
+    //DFS (RECURSION...)
+    int ans=0;
+    long long dfs(int node,unordered_map<int,vector<int>>&mp,vector<int>&values,vector<int>&vis,int k)
+    {
+        vis[node]= 1;
+        long long sum= values[node];
+        for(auto i:mp[node])
+        {
+            if(vis[i]==1)
+            {
+                continue;
+            }
+            sum+= dfs(i,mp,values,vis,k);
+            sum%=k;//to avoid out of range for integer..
+        }
+        if(sum%k==0)
+        {
+            ans++;
+            return 0;
+        }
+        return sum;
+    }
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
+    {
+        if(n==1)
+        {
+            return (values[0]%k==0)? 1:0;
+        }
+        unordered_map<int,vector<int>>mp;
+        vector<int>vis(n,0);
+        for(auto i:edges)
+        {
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+        }
+        //assuming start node is 0
+        dfs(0,mp,values,vis,k);
+        return ans;
+    }
+};
+/*
+//METHOD-1
+//BFS (QUEUE...)
+int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
     {
         //base case when edges=[]
         if(n==1)
@@ -31,7 +76,7 @@ public:
             int temp= q.front();
             q.pop();
             indegree[temp]--;
-            long long toadd= (values[temp]%k==0) ? 0:values[temp]%k; 
+            long long toadd= (values[temp]%k==0) ? 0:values[temp]%k; //to avoid integer overflow we will do values[temp]%k
             if(values[temp]%k==0)
             {
                 cnt++;
@@ -51,4 +96,4 @@ public:
         }
         return cnt;
     }
-};
+*/
