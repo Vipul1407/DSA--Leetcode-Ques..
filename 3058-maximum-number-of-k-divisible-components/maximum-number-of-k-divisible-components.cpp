99de0d238a30f1146f00339c03a2175592ff72a2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-25 15:52:17 +0530

    Time: 218 ms (13.18%) | Memory: 207.4 MB (7.30%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
-    int ans=0;
-    vector<vector<int>> adj;
-    bitset<30001> visited=0;
-    inline void build_adj(int n, vector<vector<int>>& edges, vector<int>& values)
-    {
-        for(auto& e: edges){
-            int i=e[0], j=e[1];
-            adj[i].push_back(j);
-            adj[j].push_back(i);
-        }
-    }
-    inline long long dfs(int i, vector<int>& values, int k){
-        visited[i]=1;
-        long long sum=values[i];
-        for (int j: adj[i]){
-            if (visited[j]) continue;
-            sum+=dfs(j, values, k);
-            sum%=k;
-        }
-        if (sum%k==0){
-            ans++;
-            return 0;
-        }
-        return sum;
-    }
-    
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) 
     {
-        adj.resize(n);
-    
-        build_adj(n, edges, values);
-
-        dfs(0, values, k);
-
-        return ans;
+        //base case when edges=[]
+        if(n==1)
+        {
+            return (values[0]%k==0)? 1:0;
+        }
+        unordered_map<int,vector<int>>mp;
+        vector<int>indegree(n,0);
+        for(auto i:edges)
+        {
+            mp[i[0]].push_back(i[1]);
+            mp[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
+            indegree[i[1]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            //pushing only leaf nodes..
+            if(indegree[i]==1)
+            {
+                q.push(i);
+            }
+        }
+        int cnt=0;
+        while(q.size())
+        {
+            int temp= q.front();
+            q.pop();
+            indegree[temp]--;
+            long long toadd= (values[temp]%k==0) ? 0:values[temp]%k; 
+            if(values[temp]%k==0)
+            {
+                cnt++;
+            }
+            for(auto i:mp[temp])
+            {
+                if(indegree[i]>0)
+                {
+                    indegree[i]--;
+                    values[i]+= toadd;
+                    if(indegree[i]==1)
+                    {
+                        q.push(i);
+                    }
+                }
+            }
+        }
+        return cnt;
     }
 };
