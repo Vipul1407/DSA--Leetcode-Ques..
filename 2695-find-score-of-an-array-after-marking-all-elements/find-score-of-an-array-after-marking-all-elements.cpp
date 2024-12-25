c300a0a69669412a3868620e1e4a6b821ada44c7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-25 23:32:08 +0530

    Time: 125 ms (40.56%) | Memory: 108.9 MB (32.41%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,33 +1,74 @@
-class Solution {
+class Solution 
+{
 public:
+    //METHOD-2
+    //MIN HEAP..
+    //TC= O(NLOGN)
     long long findScore(vector<int>& nums) 
     {
-        int n= nums.size();
-        long long ans=0;
-        vector<int>vis(n,0);//to mark visited index..
-        vector<pair<int,int>>vec;
-        for(int i=0;i<n;i++)
+        int n = nums.size();
+        long long ans = 0;
+        vector<int> vis(n, 0); // to mark visited index..
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for (int i = 0; i < n; i++)
         {
-            vec.push_back({nums[i],i});
+            pq.push({nums[i], i});
         }
-        sort(vec.begin(),vec.end());
-        for(auto i:vec)
+        while(pq.size())
         {
-            if(vis[i.second]==1)
+            auto top= pq.top();
+            pq.pop();
+            if(vis[top.second]==1)
             {
                 continue;
             }
-            ans+= i.first;
-            vis[i.second]=1;
-            if(i.second-1>=0)
+            ans+= top.first;
+            vis[top.second]=1;
+            if(top.second-1>=0)
             {
-                vis[i.second-1]=1;
+                vis[top.second-1]=1;
             }
-            if(i.second+1<n)
+            if(top.second+1<n)
             {
-                vis[i.second+1]=1;
+                vis[top.second+1]=1;
             }
         }
         return ans;
     }
-};   
+};
+/*
+
+// METHOD-1
+// TC= O(NLOGN)
+// BEATS 73%
+long long findScore(vector<int> &nums)
+{
+    int n = nums.size();
+    long long ans = 0;
+    vector<int> vis(n, 0); // to mark visited index..
+    vector<pair<int, int>> vec;
+    for (int i = 0; i < n; i++)
+    {
+        vec.push_back({nums[i], i});
+    }
+    sort(vec.begin(), vec.end());
+    for (auto i : vec)
+    {
+        if (vis[i.second] == 1)
+        {
+            continue;
+        }
+        ans += i.first;
+        vis[i.second] = 1;
+        if (i.second - 1 >= 0)
+        {
+            vis[i.second - 1] = 1;
+        }
+        if (i.second + 1 < n)
+        {
+            vis[i.second + 1] = 1;
+        }
+    }
+    return ans;
+}
+*/
