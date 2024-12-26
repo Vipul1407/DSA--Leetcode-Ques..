11e8958304a12cd62d0e7bbf162c37ed154a8734
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-26 19:25:20 +0530

    Time: 73 ms (94.15%) | Memory: 113.1 MB (54.62%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    long long continuousSubarrays(vector<int>& nums) {
-        int l = 0;
-        long long res = 0;  
-        deque<int> minD, maxD;
-
-        for (int r = 0; r < nums.size(); r++) {
-            while (!minD.empty() && nums[minD.back()] >= nums[r]) minD.pop_back();
-            while (!maxD.empty() && nums[maxD.back()] <= nums[r]) maxD.pop_back();
-            minD.push_back(r);
-            maxD.push_back(r);
-
-            while (nums[maxD.front()] - nums[minD.front()] > 2) {
-                l++;
-                if (minD.front() < l) minD.pop_front();
-                if (maxD.front() < l) maxD.pop_front();
+    long long continuousSubarrays(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int i=0,j=0;
+        //we are taking sorted map to take care the min element can be find by mp.begin() and max element can be find by mp.rbegin()
+        map<int,int>mp;
+        long long ans=0;
+        while(j<n)
+        {
+            mp[nums[j]]++;
+            while(i<n && (mp.rbegin()->first-mp.begin()->first)>2)
+            {
+                mp[nums[i]]--;
+                if(mp[nums[i]]==0)
+                {
+                    mp.erase(nums[i]);
+                }
+                i++;
             }
-
-            res += r - l + 1;
+            ans+= j-i+1;
+            j++;
         }
-
-        return res;
+        return ans;
     }
 };
-
-
