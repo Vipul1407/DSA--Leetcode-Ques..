ca6543465707505049e94d080f6cb36745bb56fc
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-28 00:17:13 +0530

    Time: 9 ms (9.44%) | Memory: 49.8 MB (6.87%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,17 +1,54 @@
 class Solution {
 public:
-    //METHOD-1
-    //Using prefix max vector for arr[i]+i 
+    //METHOD-3
+    //Using Priority_queue
     int maxScoreSightseeingPair(vector<int>& values) 
     {
         int n= values.size();
         int ans= 0;
-        int pref= values[0];
+        //max heap for values[i]+i
+        priority_queue<int>pq;
+        pq.push(values[0]);
         for(int j=1;j<n;j++)
         {
-            ans= max(ans, pref+values[j]-j);
-            pref= max(pref,values[j]+j);
+            ans= max(ans, pq.top()+values[j]-j);
+            pq.push(values[j]+j);
         }
         return ans;
     }
 };
+/*
+// METHOD-1
+// Using prefix max vector for arr[i]+i
+int maxScoreSightseeingPair(vector<int> &values)
+{
+    int n = values.size();
+    vector<int> pref(n, 0);
+    pref[0] = values[0];
+    for (int i = 1; i < n; i++)
+    {
+        pref[i] = max(pref[i - 1], values[i] + i);
+    }
+    int ans = 0;
+    for (int j = 1; j < n; j++)
+    {
+        ans = max(ans, pref[j - 1] + values[j] - j);
+    }
+    return ans;
+}
+
+// METHOD-2
+// Without using method, taking variable for max value of i+arr[i] at runtime...
+int maxScoreSightseeingPair(vector<int> &values)
+{
+    int n = values.size();
+    int ans = 0;
+    int pref = values[0];
+    for (int j = 1; j < n; j++)
+    {
+        ans = max(ans, pref + values[j] - j);
+        pref = max(pref, values[j] + j);
+    }
+    return ans;
+}
+*/
