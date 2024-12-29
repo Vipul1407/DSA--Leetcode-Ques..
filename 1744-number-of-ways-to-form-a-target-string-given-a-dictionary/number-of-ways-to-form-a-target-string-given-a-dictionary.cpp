3c14b5f888f4aa5ccabc0580250c4d9b040cc991
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-30 02:10:29 +0530

    Time: 99 ms (27.01%) | Memory: 84.3 MB (18.01%) - LeetSync

---
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    #define MOD 1000000007
+    int rec(int i,int j,vector<string>& words, string &tar, vector<vector<long long>>&freq, vector<vector<long long>>&dp)
+    {
+        if(j>=tar.size())
+        {
+            return 1; 
+        }
+        if(i>=words[0].size())
+        {
+            return 0;
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        long long notake= (rec(i+1,j,words,tar,freq,dp))%MOD;
+        long long take= (freq[tar[j]-'a'][i]*rec(i+1,j+1,words,tar,freq,dp))%MOD;
+        return dp[i][j]= (take+notake)%MOD;
+    }
+    int numWays(vector<string>& words, string tar) 
+    {
+        int n= words.size();
+        int m= words[0].size();
+        int k= tar.size();
+        //to cnt freq of each char at each index for word[i] i.e internal size ..
+        vector<vector<long long>>freq(26,vector<long long>(m,0));
+        for(int i=0;i<m;i++)
+        {
+            for(string &word: words)
+            {
+                freq[word[i]-'a'][i]++;
+            }
+        }
+        //i--> denoting internal dict word index..
+        //j--> denoting tar index..
+        vector<vector<long long>>dp(m+1,vector<long long>(k+1,-1));
+        return rec(0,0,words,tar,freq,dp);
+    }
+};
