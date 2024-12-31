d2544832ce1ad878bc11fc0b90ac5fce6b13fabb
Vipul <129517280+Vipul1407@users.noreply.github.com>
2024-12-31 16:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (23.42%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,35 +1,95 @@
 class Solution {
 public:
-    int rec(int i,vector<int>& days, vector<int>& costs, vector<int>&dp)
-    {
-        if(i>=days.size())
-        {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        //pass-1day
-        int day1= costs[0]+ rec(i+1,days,costs,dp);
-        //pass-7day
-        int k=i;
-        while(k<days.size() && days[k]<days[i]+7)
-        {
-            k++;
-        }
-        int day7= costs[1]+ rec(k,days,costs,dp);
-        //pass-30day
-        while(k<days.size() && days[k]<days[i]+30)
-        {
-            k++;
-        }
-        int day30= costs[2]+ rec(k,days,costs,dp);
-        return dp[i]= min(day1,min(day7,day30));
-    }
+    // USING WHILE LOOP--------------------------------------------------->
+    //METHOD-3
+    //BUA
     int mincostTickets(vector<int>& days, vector<int>& costs) 
     {
-        vector<int>dp(days.size(),-1);
-        return rec(0,days,costs,dp);
+        int n= days.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            int day1= costs[0]+ dp[i+1];
+            int k=i;
+            while(k<n && days[k]<days[i]+7)
+            {
+                k++;
+            }
+            int day7= costs[1]+ dp[k];
+            while(k<n && days[k]<days[i]+30)
+            {
+                k++;
+            }
+            int day30= costs[2]+ dp[k];
+            dp[i]= min(day1,min(day7,day30));
+        }
+        return dp[0];
     }
 };
+/*
+// METHOD-1
+// RECURSIVE
+int rec(int i, vector<int> &days, vector<int> &costs)
+{
+    if (i >= days.size())
+    {
+        return 0;
+    }
+    // pass-1day
+    int day1 = costs[0] + rec(i + 1, days, costs);
+    // pass-7day
+    int k = i;
+    while (k < days.size() && days[k] < days[i] + 7)
+    {
+        k++;
+    }
+    int day7 = costs[1] + rec(k, days, costs);
+    // pass-30day
+    while (k < days.size() && days[k] < days[i] + 30)
+    {
+        k++;
+    }
+    int day30 = costs[2] + rec(k, days, costs);
+    return min(day1, min(day7, day30));
+}
+int mincostTickets(vector<int> &days, vector<int> &costs)
+{
+    return rec(0, days, costs);
+}
+
+// METHOD-2
+// TDA
+// BEATS 100%
+int tda(int i, vector<int> &days, vector<int> &costs, vector<int> &dp)
+{
+    if (i >= days.size())
+    {
+        return 0;
+    }
+    if (dp[i] != -1)
+    {
+        return dp[i];
+    }
+    // pass-1day
+    int day1 = costs[0] + tda(i + 1, days, costs, dp);
+    // pass-7day
+    int k = i;
+    while (k < days.size() && days[k] < days[i] + 7)
+    {
+        k++;
+    }
+    int day7 = costs[1] + tda(k, days, costs, dp);
+    // pass-30day
+    while (k < days.size() && days[k] < days[i] + 30)
+    {
+        k++;
+    }
+    int day30 = costs[2] + tda(k, days, costs, dp);
+    return dp[i] = min(day1, min(day7, day30));
+}
+int mincostTickets(vector<int> &days, vector<int> &costs)
+{
+    vector<int> dp(days.size(), -1);
+    return tda(0, days, costs, dp);
+}
+*/
