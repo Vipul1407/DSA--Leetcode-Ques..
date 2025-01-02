cd5829647a9b7343bebf497e99754759bcd5fcbc
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-02 17:15:14 +0530

    Time: 2 ms (91.64%) | Memory: 69.8 MB (65.84%) - LeetSync

---
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& quer) 
+    {
+        int n= words.size();
+        vector<int>pref(n,0);
+        for(int i=0;i<n;i++)
+        {
+            char front= words[i].front();
+            char back= words[i].back();
+            if((front=='a'||front=='e'||front=='i'||front=='o'||front=='u')&&(back=='a'||back=='e'||back=='i'||back=='o'||back=='u'))
+            {
+                if(i==0)
+                {
+                    pref[i]=1;
+                }
+                else
+                {
+                    pref[i]= pref[i-1]+1;
+                }
+            }
+            else
+            {
+                if(i==0)
+                {
+                    continue;
+                }
+                pref[i]=pref[i-1];
+            }
+        }
+        vector<int>res;
+        for(int i=0;i<quer.size();i++)
+        {
+            int r= quer[i][1];
+            int l= quer[i][0];
+            if(l==0)
+            {
+                res.push_back(pref[r]);
+            }
+            else
+            {
+                res.push_back(pref[r]-pref[l-1]);
+            }
+        }
+        return res;
+    }
+};
