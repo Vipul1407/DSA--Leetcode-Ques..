ab49aff65fbad9296d7d98894f9807fbe4bca2fc
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-03 14:50:43 +0530

    Time: 4 ms (47.38%) | Memory: 97 MB (30.84%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    //METHOD-1
-    //TAKING 2 VECTORS..
     int waysToSplitArray(vector<int>& nums) 
     {
         int n= nums.size();
-        vector<long long>leftsum(n,0);
-        vector<long long>rightsum(n,0);
-        leftsum[0]= nums[0];
-        rightsum[n-1]= nums[n-1];
+        vector<long long>rminusl(n,0);
+        rminusl[0]= nums[0];
         for(int i=1;i<n;i++)
         {
-            leftsum[i]=leftsum[i-1]+nums[i];
+            rminusl[i]= rminusl[i-1]+nums[i];
         }
+        long long sum= nums[n-1];
         for(int i=n-2;i>=0;i--)
         {
-            rightsum[i]= rightsum[i+1]+nums[i];
+            rminusl[i]-=sum;
+            sum+=nums[i];
         }
         int cnt=0;
         for(int i=0;i<n-1;i++)
         {
-            if(leftsum[i]>=rightsum[i+1])
+            if(rminusl[i]>=0)
             {
                 cnt++;
             }
@@ -28,3 +26,33 @@ public:
         return cnt;
     }
 };
+/*
+// METHOD-1
+// TAKING 2 VECTORS..
+// BEATS 8%
+int waysToSplitArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<long long> leftsum(n, 0);
+    vector<long long> rightsum(n, 0);
+    leftsum[0] = nums[0];
+    rightsum[n - 1] = nums[n - 1];
+    for (int i = 1; i < n; i++)
+    {
+        leftsum[i] = leftsum[i - 1] + nums[i];
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightsum[i] = rightsum[i + 1] + nums[i];
+    }
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (leftsum[i] >= rightsum[i + 1])
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+} 
+*/
