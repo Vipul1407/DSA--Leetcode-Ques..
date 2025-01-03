df4a91da4536b12652bd61ad3b2e4cfe08ea9dbe
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-03 14:55:15 +0530

    Time: 6 ms (30.21%) | Memory: 89.2 MB (79.17%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
+    // METHOD-3
+    // WITHOUT USING ANY VECTOR..
     int waysToSplitArray(vector<int>& nums) 
     {
         int n= nums.size();
-        vector<long long>rminusl(n,0);
-        rminusl[0]= nums[0];
-        for(int i=1;i<n;i++)
-        {
-            rminusl[i]= rminusl[i-1]+nums[i];
-        }
-        long long sum= nums[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            rminusl[i]-=sum;
-            sum+=nums[i];
-        }
+        long long total= accumulate(nums.begin(),nums.end(),0LL);
+        long long sum=0;
         int cnt=0;
         for(int i=0;i<n-1;i++)
         {
-            if(rminusl[i]>=0)
+            sum+= nums[i];
+            if(sum>= total-sum)
             {
                 cnt++;
             }
-        }
+        }    
         return cnt;
     }
 };
@@ -54,5 +47,33 @@ int waysToSplitArray(vector<int> &nums)
         }
     }
     return cnt;
-} 
+}
+
+// METHOD-2
+// TAKING 1 VECTOR ONLY..
+int waysToSplitArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<long long> rminusl(n, 0);
+    rminusl[0] = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        rminusl[i] = rminusl[i - 1] + nums[i];
+    }
+    long long sum = nums[n - 1];
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rminusl[i] -= sum;
+        sum += nums[i];
+    }
+    int cnt = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (rminusl[i] >= 0)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
 */
