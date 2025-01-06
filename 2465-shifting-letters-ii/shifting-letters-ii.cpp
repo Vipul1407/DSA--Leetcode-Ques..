97dd6fdd658065de39b7c8571512d14a9e706c85
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-06 13:06:48 +0530

    Time: 49 ms (31.98%) | Memory: 108.2 MB (28.29%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,27 +1,76 @@
 class Solution {
 public:
-    string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> shift(n + 1, 0);
-
-        // Process the shifts
-        for (auto& shiftOp : shifts) {
-            int start = shiftOp[0], end = shiftOp[1], direction = shiftOp[2];
-            shift[start] += (direction == 1 ? 1 : -1);
-            shift[end + 1] -= (direction == 1 ? 1 : -1);
+    
+    // METHOD-2
+    // TC=O(M+N)
+    string shiftingLetters(string s, vector<vector<int>>& shifts) 
+    {
+        int n= s.size();
+        vector<int>change(n,0);
+        for(auto i:shifts)
+        {
+            int l= i[0];
+            int r= i[1];
+            int dir= i[2];
+            //forward shifting by 1..
+            if(dir==1)
+            {
+                change[l]++;
+                //will add opposite at index r+1 only if it is valid index..
+                if(r+1<n)
+                {
+                    change[r+1]--;
+                }
+            }
+            else
+            {
+                change[l]--;
+                //will add opposite at index r+1 only if it is valid index..
+                if(r+1<n)
+                {
+                    change[r+1]++;
+                }
+            }
         }
-
-        int currentShift = 0;
-        for (int i = 0; i < n; ++i) {
-            currentShift += shift[i];
-            shift[i] = currentShift;
+        //taking final prefix sum of change array.. and updating string s
+        int netchange= (change[0]%26+26)%26;
+        s[0]= 'a'+ (s[0]-'a'+netchange)%26;
+        for(int i=1;i<n;i++)
+        {
+            change[i]+= change[i-1];
+            netchange= (change[i]%26+26)%26;
+            s[i]= 'a'+ (s[i]-'a'+netchange)%26;
         }
-
-        for (int i = 0; i < n; ++i) {
-            int netShift = (shift[i] % 26 + 26) % 26;
-            s[i] = 'a' + (s[i] - 'a' + netShift) % 26;
-        }
-
         return s;
     }
 };
+/*
+// METHOD-1
+// BRUTE FORCE
+// TC= O(M*N)
+string shiftingLetters(string s, vector<vector<int>> &shifts)
+{
+    int n = s.size();
+    for (auto i : shifts)
+    {
+        int l = i[0];
+        int r = i[1];
+        int dir = i[2];
+        // we have to do shifting for all elements present in that range..
+        for (int k = l; k <= r; k++)
+        {
+            // backward shifting..
+            if (dir == 0)
+            {
+                s[k] = 'a' + (s[k] - 'a' - 1 + 26) % 26;
+            }
+            // forward shifting..
+            else
+            {
+                s[k] = 'a' + (s[k] - 'a' + 1) % 26;
+            }
+        }
+    }
+    return s;
+}
+*/
