8fe6117c4e67fdc54f24ab140ee51f9ff547bde6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-09 18:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (95.47%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    // METHOD-2
+    // USING FIND
     int prefixCount(vector<string>& words, string pref) 
     {
-        int cnt=0;
         int n= words.size();
+        int cnt=0;
         int m= pref.size();
         for(int i=0;i<n;i++)
         {
-            string str= words[i].substr(0,m);
-            if(str==pref)
+            if(words[i].find(pref)==0)
             {
                 cnt++;
             }
@@ -16,3 +17,22 @@ public:
         return cnt;
     }
 };
+/*
+// METHOD-1
+// USING SUBSTRING
+int prefixCount(vector<string> &words, string pref)
+{
+    int cnt = 0;
+    int n = words.size();
+    int m = pref.size();
+    for (int i = 0; i < n; i++)
+    {
+        string str = words[i].substr(0, m);
+        if (str == pref)
+        {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+*/
