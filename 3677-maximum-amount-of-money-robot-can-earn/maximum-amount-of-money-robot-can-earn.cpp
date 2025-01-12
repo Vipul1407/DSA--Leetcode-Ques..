0e9b40bd7a09e90b1789bedbe7ad95a1525f802e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-12 14:07:42 +0530

    Time: 525 ms (0.00%) | Memory: 225.7 MB (0.00%) - LeetSync

--- 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -10,47 +10,34 @@ public:
         }
         if(i==m-1 && j==n-1)
         {
-            if(coins[i][j]>=0)
+            if(coins[i][j]<0 && rob>0)
             {
-                return coins[i][j];
+                return 0;
             }
-            else
-            {
-                return rob>0? 0:coins[i][j];
-            }
+            return coins[i][j];
         }
         if(dp[i][j][rob]!=-1)
         {
             return dp[i][j][rob];
         }
-        int c= coins[i][j];
         int down= solve(i+1,j,rob,coins,dp);
         int right= solve(i,j+1,rob,coins,dp);
-        int best= max(down,right);
-        if(c>=0)
+        int profit= coins[i][j]+ max(down,right);
+
+        if(coins[i][j]<0 && rob>0)
         {
-            return dp[i][j][rob]= c+best;
+            int d2= solve(i+1,j,rob-1,coins,dp);
+            int r2= solve(i,j+1,rob-1,coins,dp);
+            int b2= max(d2,r2);
+            profit= max(profit,b2);
         }
-        else
-        {
-            if(rob>0)
-            {
-                int d2= solve(i+1,j,rob-1,coins,dp);
-                int r2= solve(i,j+1,rob-1,coins,dp);
-                int b2= max(d2,r2);
-                return dp[i][j][rob]= max(c+best,b2);
-            }
-            else
-            {
-                return dp[i][j][rob]= c+best;
-            }
-        }
+        return dp[i][j][rob]= profit;
     }
     int maximumAmount(vector<vector<int>>& coins) 
     {
         int m= coins.size();
         int n= coins[0].size();
-        vector<vector<vector<int>>>dp(m,vector<vector<int>>(n,vector<int>(3,-1)));
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(3,-1)));
         return solve(0,0,2,coins,dp);
     }
 };
