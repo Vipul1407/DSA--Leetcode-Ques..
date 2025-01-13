b1e6c65449d596f194e4157b9da294ea5f701353
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-13 10:48:21 +0530

    Time: 27 ms (58.87%) | Memory: 32.1 MB (74.19%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -3,23 +3,16 @@ public:
     int minimumLength(string s) 
     {
         vector<int>vec(26,0);
+        int ans=0;
         for(auto i:s)
         {
             vec[i-'a']++;
             if(vec[i-'a']==3)
             {
                 vec[i-'a']-=2;
+                ans+=2;
             }
         }    
-        int ans=0;
-        for(int i=0;i<26;i++)
-        {
-            if(vec[i]==0)
-            {
-                continue;
-            }
-            ans+= vec[i];
-        }
-        return ans;
+        return s.size()-ans;
     }
 };
