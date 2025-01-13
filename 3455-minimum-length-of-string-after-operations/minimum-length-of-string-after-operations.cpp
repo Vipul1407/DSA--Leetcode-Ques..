ec508199ac91e87d983d868c132576363f0cbaa6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-13 10:59:50 +0530

    Time: 12 ms (73.39%) | Memory: 32 MB (86.69%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,18 +1,73 @@
 class Solution {
 public:
+    //METHOD-3
     int minimumLength(string s) 
     {
-        vector<int>vec(26,0);
         int ans=0;
+        vector<int>vec(26,0);
         for(auto i:s)
         {
             vec[i-'a']++;
-            if(vec[i-'a']==3)
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(vec[i]==0)
             {
-                vec[i-'a']-=2;
+                continue;
+            }
+            //odd times..
+            if(vec[i]%2==1)
+            {
+                ans++;
+            }
+            //even times..
+            else
+            {
                 ans+=2;
             }
-        }    
-        return s.size()-ans;
+        }
+        return ans;
     }
 };
+/*
+// METHOD-1
+int minimumLength(string s)
+{
+    vector<int> vec(26, 0);
+    for (auto i : s)
+    {
+        vec[i - 'a']++;
+        if (vec[i - 'a'] == 3)
+        {
+            vec[i - 'a'] -= 2;
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (vec[i] == 0)
+        {
+            continue;
+        }
+        ans += vec[i];
+    }
+    return ans;
+}
+
+// METHOD-2
+int minimumLength(string s)
+{
+    vector<int> vec(26, 0);
+    int ans = 0;
+    for (auto i : s)
+    {
+        vec[i - 'a']++;
+        if (vec[i - 'a'] == 3)
+        {
+            vec[i - 'a'] -= 2;
+            ans += 2;
+        }
+    }
+    return s.size() - ans;
+}
+*/
