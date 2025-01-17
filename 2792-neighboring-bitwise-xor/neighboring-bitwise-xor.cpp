942d8fce472112916c5dc626882a998e0fb99829
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-17 15:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 263.9 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
+    //METHOD-2
     bool doesValidArrayExist(vector<int>& derived) 
     {
         int n= derived.size();
-        vector<int>original(n,0);
+        int xorv=derived[0];
         for(int i=1;i<n;i++)
         {
-            original[i]= derived[i-1]^original[i-1];
+            xorv^=derived[i];
         }
-        return original[0]^original[n-1]==derived[n-1];
+        return xorv==0;
     }
 };
+/*
+// METHOD-1
+// BEATS 25%
+bool doesValidArrayExist(vector<int> &derived)
+{
+    int n = derived.size();
+    vector<int> original(n, 0);
+    for (int i = 1; i < n; i++)
+    {
+        original[i] = derived[i - 1] ^ original[i - 1];
+    }
+    return original[0] ^ original[n - 1] == derived[n - 1];
+}
+*/
