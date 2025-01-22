f1bf00718f3efd24295d136ca4b64c1539f68def
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-22 18:21:05 +0530

    Time: 1037 ms (5.09%) | Memory: 346.1 MB (5.09%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& mat) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        vector<vector<int>>ans(m,vector<int>(n,-1));
+        vector<vector<int>>dir= {{0,1},{1,0},{0,-1},{-1,0}};
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==1)
+                {
+                    ans[i][j]=0;
+                    q.push({i,j});
+                }
+            }
+        }
+        while(q.size())
+        {
+            int x= q.front().first;
+            int y= q.front().second;
+            q.pop();
+            for(auto d:dir)
+            {
+                int dx= x+d[0];
+                int dy= y+d[1];
+                if(dx>=0 && dy>=0 && dx<m && dy<n)
+                {
+                    if(ans[dx][dy]==-1)
+                    {
+                        ans[dx][dy]= ans[x][y]+1;
+                        q.push({dx,dy});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
