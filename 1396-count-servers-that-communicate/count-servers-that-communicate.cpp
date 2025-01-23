38282210eb2f3e011110efc737aefd13e80d6ce9
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-01-23 15:02:13 +0530

    Time: 3 ms (55.91%) | Memory: 26.7 MB (59.58%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
+    //METHOD-2
+    //BETTER APPROACH
+    //TC= O(M*N)  SC=O(M+N)
     int countServers(vector<vector<int>>& grid) 
     {
         int m= grid.size();
         int n= grid[0].size();
         int ans=0;
-        
+        vector<int>row(m,0);
+        vector<int>col(n,0);
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==1)
                 {
-                    bool flag= false;
-                    //check same row...
-                    for(int col=0;col<n;col++)
-                    {
-                        if(grid[i][col]==1 && col!=j)
-                        {
-                            flag= true;
-                            break;
-                        }
-                    }
-                    if(!flag)
-                    {
-                        //check same col..
-                        for(int row=0;row<m;row++)
-                        {
-                            if(grid[row][j]==1 && row!=i)
-                            {
-                                flag= true;
-                                break;
-                            }
-                        }  
-                    }
-                    if(flag==true)
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    if(row[i]>=2 || col[j]>=2)
                     {
                         ans++;
                     }
