6c37bda8d338058159ca27400100c1b5af89452e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-16 17:46:15 +0530

    Time: 15 ms (76.85%) | Memory: 7.9 MB (95.81%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    
-    // METHOD-2
-    // TDA MEMOIZATION..
-    bool check(int i,int sum,int num,string str,vector<vector<int>>&dp)
+    //METHOD-3
+    //EASY APPROACH
+    bool check(int sq, int sum, int num)
     {
-        if(i==str.size())
+        if(sq==0)
         {
             return sum==num;
         }
-        if(sum>num)
-        {
-            return false;
-        }
-        if(dp[i][sum]!=-1)
-        {
-            return dp[i][sum];
-        }
-        bool possi= false;
-        for(int j=i;j<str.size();j++)
-        {
-            string sub= str.substr(i,j-i+1);
-            possi= possi || check(j+1,sum+stoi(sub),num,str,dp);
-            if(possi)
-            {
-                return dp[i][sum]=possi;
-            }
-        }
-        return dp[i][sum]= possi;
+        return check(sq/10,sum+sq%10,num) || check(sq/100,sum+sq%100,num) || check(sq/1000,sum+sq%1000,num) || check(sq/10000,sum+sq%10000,num);
     }
     int punishmentNumber(int n) 
     {
         int ans=0;
         for(int num=1;num<=n;num++)
         {
-            string str= to_string(num*num);
-            vector<vector<int>>dp(str.size()+1,vector<int>(num+1,-1));
-            if(check(0,0,num,str,dp))
+            if(check(num*num,0,num))
             {
-                ans+= stoi(str);
+                ans+= num*num;
             }
         }
         return ans;
@@ -75,6 +54,51 @@ int punishmentNumber(int n)
         if (check(0, 0, num, to_string(str)))
         {
             ans += str;
+        }
+    }
+    return ans;
+}
+
+// METHOD-2
+// TDA MEMOIZATION..
+// TC= O(N*2^LOG10(N*N))
+// SC= O(N*LOG10(N*N) + N)
+bool check(int i, int sum, int num, string str, vector<vector<int>> &dp)
+{
+    if (i == str.size())
+    {
+        return sum == num;
+    }
+    if (sum > num)
+    {
+        return false;
+    }
+    if (dp[i][sum] != -1)
+    {
+        return dp[i][sum];
+    }
+    bool possi = false;
+    for (int j = i; j < str.size(); j++)
+    {
+        string sub = str.substr(i, j - i + 1);
+        possi = possi || check(j + 1, sum + stoi(sub), num, str, dp);
+        if (possi)
+        {
+            return dp[i][sum] = possi;
+        }
+    }
+    return dp[i][sum] = possi;
+}
+int punishmentNumber(int n)
+{
+    int ans = 0;
+    for (int num = 1; num <= n; num++)
+    {
+        string str = to_string(num * num);
+        vector<vector<int>> dp(str.size() + 1, vector<int>(num + 1, -1));
+        if (check(0, 0, num, str, dp))
+        {
+            ans += stoi(str);
         }
     }
     return ans;
