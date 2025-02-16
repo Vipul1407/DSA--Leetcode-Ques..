ccbf6c2ec7f8a1553e059ffeea698017431e8c1c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-16 12:33:57 +0530

    Time: 831 ms (5.17%) | Memory: 559.4 MB (5.42%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+    
+    // METHOD-2
+    // TDA MEMOIZATION..
     bool check(int i,int sum,int num,string str,vector<vector<int>>&dp)
     {
         if(i==str.size())
@@ -19,9 +22,9 @@ public:
         {
             string sub= str.substr(i,j-i+1);
             possi= possi || check(j+1,sum+stoi(sub),num,str,dp);
-            if(possi==true)
+            if(possi)
             {
-                return true;
+                return dp[i][sum]=possi;
             }
         }
         return dp[i][sum]= possi;
@@ -41,3 +44,39 @@ public:
         return ans;
     }
 };
+/*
+// METHOD-1
+// RECURSION..
+// BEATS 21%
+bool check(int i, int sum, int num, string str)
+{
+    if (i == str.size())
+    {
+        return sum == num;
+    }
+    bool possi = false;
+    for (int j = i; j < str.size(); j++)
+    {
+        string sub = str.substr(i, j - i + 1);
+        possi = possi || check(j + 1, sum + stoi(sub), num, str);
+        if (possi == true)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int punishmentNumber(int n)
+{
+    int ans = 0;
+    for (int num = 1; num <= n; num++)
+    {
+        int str = num * num;
+        if (check(0, 0, num, to_string(str)))
+        {
+            ans += str;
+        }
+    }
+    return ans;
+}
+*/
