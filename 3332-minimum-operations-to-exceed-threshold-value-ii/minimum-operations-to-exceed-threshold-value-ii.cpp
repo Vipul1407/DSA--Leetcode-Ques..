2356efc21394527edd33ae1fc36f46b73e7d316a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-16 21:53:21 +0530

    Time: 87 ms (89.73%) | Memory: 91.3 MB (82.78%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    static int minOperations(vector<int>& nums, int k) {
-        priority_queue<long long, vector<long long>, greater<>> pq(nums.begin(), nums.end());
+    typedef long long ll;
+    int minOperations(vector<int>& nums, int k)
+    {
+        priority_queue<ll,vector<ll>,greater<ll>>pq(nums.begin(),nums.end());
         int ans=0;
-        while(pq.top()<k){
-            long long x=pq.top(); pq.pop();
-            long long y=pq.top(); pq.pop();
-            pq.push(2*x+y);
+        while(pq.size()>1)
+        {
+            auto x= pq.top();
+            if(x>=k)
+            {
+                return ans;
+            }
+            pq.pop();
+            auto y= pq.top();
+            pq.pop();
+            pq.push(min(x,y)*2+max(x,y));
             ans++;
         }
-        return ans;  
+        return ans;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
