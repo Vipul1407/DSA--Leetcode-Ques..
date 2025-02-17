445a5082fc5cc230e7d629067846eff0f866187a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-17 13:10:13 +0530

    Time: 36 ms (37.36%) | Memory: 73.6 MB (27.05%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    static inline int digitSum(int x){
-        int sum=0;
-        for(; x>0; x/=10){
-            sum+=x%10;
-        }
-        return sum;
-    }
-    static int maximumSum(vector<int>& nums) {
-        int p[82][2]={{0, 0}};
-    //    memset(pq, 0, sizeof(pq));
-        int maxD=0, minD=INT_MAX;
-        for(int x: nums){
-            int D=digitSum(x);
-            if (p[D][0]==0 && p[D][1]==0) p[D][1]=x;
-            else if (x>=p[D][1]) p[D][0]=exchange(p[D][1], x);
-            else if (x>=p[D][0]) p[D][0]=x;
-            maxD=max(maxD, D);
-            minD=min(minD, D);
-        }
-        
+    int maximumSum(vector<int>& nums) 
+    {
+        unordered_map<int,int>mp;
         int ans=-1;
-        for(int D=minD; D<=maxD; D++){
-            if (p[D][0]==0) continue;
-            ans=max(ans, p[D][0]+p[D][1]);
-        }
+        for(auto i:nums)
+        {
+            int temp=i;
+            int sum=0;
+            while(temp)
+            {
+                sum+= temp%10;
+                temp/=10;
+            }
+            if(mp.find(sum)!=mp.end())
+            {
+                ans= max(ans,mp[sum]+i);
+                mp[sum]= max(mp[sum],i);
+            }
+            else
+            {
+                mp[sum]= i;
+            }
+        }  
         return ans;
     }
 };
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();  
