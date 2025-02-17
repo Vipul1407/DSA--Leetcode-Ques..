66bc5e7fbb7569d280d9a03c665afc4f6019b40c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-17 13:19:08 +0530

    Time: 4 ms (95.17%) | Memory: 73.6 MB (27.05%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
+    // METHOD-3
+    // USING VECTOR..
     int maximumSum(vector<int>& nums) 
     {
-        unordered_map<int,int>mp;
+        vector<int>arr(82,-1);
         int ans=-1;
         for(auto i:nums)
         {
@@ -13,16 +15,80 @@ public:
                 sum+= temp%10;
                 temp/=10;
             }
-            if(mp.find(sum)!=mp.end())
+            if(arr[sum]==-1)
             {
-                ans= max(ans,mp[sum]+i);
-                mp[sum]= max(mp[sum],i);
+                arr[sum]= i;
             }
             else
             {
-                mp[sum]= i;
+                ans= max(ans,arr[sum]+i);
+                arr[sum]= max(arr[sum],i);
             }
-        }  
+        }
         return ans;
     }
 };
+/*
+// METHOD-1
+// BRUTE FORCE..
+int maximumSum(vector<int> &nums)
+{
+    int n = nums.size();
+    int ans = -1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int x = nums[i];
+            int sumx = 0;
+            int temp = x;
+            while (temp)
+            {
+                sumx += temp % 10;
+                temp /= 10;
+            }
+            int y = nums[j];
+            int sumy = 0;
+            temp = y;
+            while (temp)
+            {
+                sumy += temp % 10;
+                temp /= 10;
+            }
+            if (sumx == sumy)
+            {
+                ans = max(ans, x + y);
+            }
+        }
+    }
+    return ans;
+}
+
+// METHOD-2
+// USING UNORDERED MAP
+int maximumSum(vector<int> &nums)
+{
+    unordered_map<int, int> mp;
+    int ans = -1;
+    for (auto i : nums)
+    {
+        int temp = i;
+        int sum = 0;
+        while (temp)
+        {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if (mp.find(sum) != mp.end())
+        {
+            ans = max(ans, mp[sum] + i);
+            mp[sum] = max(mp[sum], i);
+        }
+        else
+        {
+            mp[sum] = i;
+        }
+    }
+    return ans;
+}
+*/
