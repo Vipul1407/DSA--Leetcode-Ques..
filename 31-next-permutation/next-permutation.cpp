79067089e188fc8219f8afa41386c4f7e07bb6d6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-19 18:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (99.75%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -2,6 +2,43 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) 
     {
-        next_permutation(nums.begin(),nums.end());
+        int n= nums.size();
+        int idx=-1;
+        //when we get a dip in mountain then we have that in our idx..
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                idx=i;
+                break;
+            }
+        }
+        //we have full array in decreasing order then it means it is the last permuatation and hence we will go to its 1st permutation
+        if(idx==-1)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            for(int i=n-1;i>idx;i--)
+            {
+                //swap the nums[idx] with the just greater element..
+                if(nums[i]>nums[idx])
+                {
+                    swap(nums[i],nums[idx]);
+                    break;
+                }
+            }
+            //reverse the nums for idx+1 to last..
+            reverse(nums.begin()+idx+1,nums.end());
+        }
     }
 };
+/*
+// METHOD-1
+// INBUILT STL
+void nextPermutation(vector<int> &nums)
+{
+    next_permutation(nums.begin(), nums.end());
+}
+*/
