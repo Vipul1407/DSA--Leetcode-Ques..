8741470ceff7a9adb7d76bb88be38c71a7c4adf2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-19 21:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (87.75%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    //METHOD-3
-    //USING BACKTRACKING LIKE Ques. Letter Tile Permutation..
-    void solve(vector<int>curr, vector<int>& nums, vector<int>&used, vector<vector<int>>&ans)
+    // METHOD-4
+    // USING SWAP + BACKTRACKING..
+    //CAN BE USED FOR DIFF. NUMBERS
+    void solve(int idx, vector<int>& nums, vector<vector<int>>&ans)
     {
-        if(curr.size()==nums.size())
+        if(idx>=nums.size())
         {
-            ans.push_back(curr);
+            ans.push_back(nums);
+            return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=idx;i<nums.size();i++)
         {
-            if(used[i]==1)
-            {
-                continue;
-            }
-            used[i]=1;
-            curr.push_back(nums[i]);
-            solve(curr,nums,used,ans);
-            curr.pop_back();
-            used[i]=0;
+            swap(nums[idx],nums[i]);
+            solve(idx+1,nums,ans);
+
+            //backtracking...
+            swap(nums[idx],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) 
     {
         int n= nums.size();
         vector<vector<int>>ans;
-        vector<int>used(n,0);
-        vector<int>curr;
-        solve(curr,nums,used,ans);
-        return ans;    
+        solve(0,nums,ans);
+        return ans;
     }
 };
-
 /*
 // METHOD-1
+// TC= O(N!)
 // USING NEXT PERMUTATION STL..
+// BEATS 100%
 vector<vector<int>> permute(vector<int> &nums)
 {
     int n = nums.size();
@@ -85,6 +82,38 @@ vector<vector<int>> permute(vector<int> &nums)
     vector<int> used(n, 0);
     vector<int> curr;
     solve(0, curr, nums, used, ans);
+    return ans;
+}
+
+// METHOD-3
+// USING BACKTRACKING LIKE Ques. Letter Tile Permutation..
+// Not taking len directly finding the size of curr vector..
+void solve(vector<int> curr, vector<int> &nums, vector<int> &used, vector<vector<int>> &ans)
+{
+    if (curr.size() == nums.size())
+    {
+        ans.push_back(curr);
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (used[i] == 1)
+        {
+            continue;
+        }
+        used[i] = 1;
+        curr.push_back(nums[i]);
+        solve(curr, nums, used, ans);
+        curr.pop_back();
+        used[i] = 0;
+    }
+}
+vector<vector<int>> permute(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+    vector<int> used(n, 0);
+    vector<int> curr;
+    solve(curr, nums, used, ans);
     return ans;
 }
 */
