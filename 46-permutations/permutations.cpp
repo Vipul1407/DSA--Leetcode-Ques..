3d5bac07fc4be81f691233def53b74d9d2d99b96
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-19 19:15:48 +0530

    Time: 5 ms (9.55%) | Memory: 11.8 MB (17.57%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,23 +1,58 @@
 class Solution {
 public:
+    //METHOD-2
+    //USING BACKTRACKING LIKE Ques. Letter Tile Permutation..
+    void solve(int len, vector<int>curr, vector<int>& nums, vector<int>&used, vector<vector<int>>&ans)
+    {
+        if(len==nums.size())
+        {
+            ans.push_back(curr);
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(used[i]==1)
+            {
+                continue;
+            }
+            used[i]=1;
+            curr.push_back(nums[i]);
+            solve(len+1,curr,nums,used,ans);
+            curr.pop_back();
+            used[i]=0;
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) 
     {
         int n= nums.size();
         vector<vector<int>>ans;
-        sort(nums.begin(),nums.end());
-
-        //using while loop.....
-        ans.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end()))
-        {
-            ans.push_back(nums);
-        }
-        //using do while loop....
-        // do
-        // {
-        //     ans.push_back(nums);
-        // }
-        // while(next_permutation(nums.begin(),nums.end()));
-        return ans;
+        vector<int>used(n,0);
+        vector<int>curr;
+        solve(0,curr,nums,used,ans);
+        return ans;    
     }
 };
+
+/*
+// METHOD-1
+// USING NEXT PERMUTATION STL..
+vector<vector<int>> permute(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+
+    // using while loop.....
+    ans.push_back(nums);
+    while (next_permutation(nums.begin(), nums.end()))
+    {
+        ans.push_back(nums);
+    }
+    // using do while loop....
+    //  do
+    //  {
+    //      ans.push_back(nums);
+    //  }
+    //  while(next_permutation(nums.begin(),nums.end()));
+    return ans;
+}
+*/
