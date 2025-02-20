9c1617b571900e79f557992a3d4c94a7dc56a43d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-20 22:07:46 +0530

    Time: 5 ms (17.61%) | Memory: 14.1 MB (61.12%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,41 +1,49 @@
-class Solution 
-{
+class Solution {
 public:
-
-    vector<vector<int>>ans;
-    //here we have to pass vec by reference &
-    void solve(int i,vector<int>& cand,int k,vector<int>&vec)
+    void solve(int i, int sum, vector<int>&used,vector<int>&temp, vector<int>&cand, int k, vector<vector<int>>&ans)
     {
-        if(k==0)
-        {
-            ans.push_back(vec);
-            return;
-        }
-        if(k<0 || i==cand.size())
+        if(sum>k || i>=cand.size())
         {
             return;
         }
+        if(sum==k)
+        {
+            ans.push_back(temp);
+        }
+        //include case..
         for(int j=i;j<cand.size();j++)
         {
-            if(j>i && cand[j]==cand[j-1])
+            if(used[cand[j]]==0)
             {
-                continue;//to avoid duplicates repeatition of same vectors like {1,3,2} or {2,1,3}
-                //we will avoid recursive call in that case
+                continue;
             }
-            vec.push_back(cand[j]);
-            //also do j+1 here becoz we have to take 1 number only 1 time
-            solve(j+1,cand,k-cand[j],vec);
-            vec.pop_back();
+            used[cand[j]]--;
+            temp.push_back(cand[j]);
+            solve(j,sum+cand[j],used,temp,cand,k,ans);
+
+            //backtracking..
+            temp.pop_back();
+            used[cand[j]]++;
+
+            //do this extra thing to prevent traversing the same recursive tree again.. before this we must sort cand array
+            while(j+1<cand.size() && cand[j]==cand[j+1])
+            {
+                j++;
+            }
         }
     }
-    
     vector<vector<int>> combinationSum2(vector<int>& cand, int k) 
     {
-        vector<int>vec;
-        //to avoid duplicates or repeation--> we will sort and not start rec call from same element...
+        vector<vector<int>>ans;
+        //must step...
         sort(cand.begin(),cand.end());
-        solve(0,cand,k,vec);
-        
-        return ans;  
+        vector<int>temp;
+        vector<int>used(51,0);
+        for(auto i:cand)
+        {
+            used[i]++;
+        }
+        solve(0,0,used,temp,cand,k,ans);
+        return ans;
     }
 };
