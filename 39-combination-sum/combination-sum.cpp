c29993190fbe57b964654a8e7c9051fd6b960355
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-20 19:36:42 +0530

    Time: 3 ms (56.51%) | Memory: 14.1 MB (74.18%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
+    // METHOD-2
+    // BACKTRACKING..
+    // Using For Loop..
     void solve(int i,int sum, vector<int>&temp, int &k, vector<int>&cand, vector<vector<int>>&ans)
     {
+        if(sum>k || i>=cand.size())
+        {
+            return;
+        }
         if(sum==k)
         {
             ans.push_back(temp);
             return;
         }
-        if(i>=cand.size() || sum>k)
+        for(int j=i;j<cand.size();j++)
         {
-            return;
+            temp.push_back(cand[j]);
+            solve(j,sum+cand[j],temp,k,cand,ans);
+            temp.pop_back();
         }
-        temp.push_back(cand[i]);
-        //include..
-        solve(i,sum+cand[i],temp,k,cand,ans);
-        temp.pop_back();
-        //exclude..
-        solve(i+1,sum,temp,k,cand,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& cand, int k) 
     {
@@ -26,3 +29,33 @@ public:
         return ans;
     }
 };
+/*
+// METHOD-1
+// BACKTRACKING..
+// Without using for loop...
+void solve(int i, int sum, vector<int> &temp, int &k, vector<int> &cand, vector<vector<int>> &ans)
+{
+    if (sum == k)
+    {
+        ans.push_back(temp);
+        return;
+    }
+    if (i >= cand.size() || sum > k)
+    {
+        return;
+    }
+    temp.push_back(cand[i]);
+    // include..
+    solve(i, sum + cand[i], temp, k, cand, ans);
+    temp.pop_back();
+    // exclude..
+    solve(i + 1, sum, temp, k, cand, ans);
+}
+vector<vector<int>> combinationSum(vector<int> &cand, int k)
+{
+    vector<vector<int>> ans;
+    vector<int> temp;
+    solve(0, 0, temp, k, cand, ans);
+    return ans;
+}
+*/
