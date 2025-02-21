946686ca6573f4e9fbc0b86a9b98edd6312243cc
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-21 10:46:40 +0530

    Time: 6 ms (85.42%) | Memory: 27.1 MB (82.80%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -11,26 +11,37 @@
  */
 class FindElements {
 public:
+    //METHOD-3
+    //BFS..
     unordered_set<int>st;
-    void dfs(TreeNode* root, int x)
-    {
-        if(!root)
-        {
-            return;
-        }
-        root->val=x;
-        st.insert(root->val);
-        dfs(root->left,2*x+1);
-        dfs(root->right,2*x+2);
-    }
     FindElements(TreeNode* root) 
     {
-        dfs(root,0);
+        queue<TreeNode*>q;
+        root->val=0;
+        q.push(root);
+
+        while(!q.empty())
+        {
+            auto temp= q.front();
+            st.insert(temp->val);
+            q.pop();
+
+            if(temp->left)
+            {
+                temp->left->val= temp->val*2+1; 
+                q.push(temp->left);
+            }
+            if(temp->right)
+            {
+                temp->right->val= temp->val*2+2; 
+                q.push(temp->right);
+            }
+        }
     }
     
     bool find(int target) 
     {
-        return st.find(target)!=st.end();
+        return st.count(target);
     }
 };
 
@@ -38,4 +49,66 @@ public:
  * Your FindElements object will be instantiated and called as such:
  * FindElements* obj = new FindElements(root);
  * bool param_1 = obj->find(target);
+ */
+ /*
+ // METHOD-1
+// USING DFS..
+// TC= O(N)+O(N)
+TreeNode *root = NULL;
+void dfs(TreeNode *root, int x)
+{
+    if (!root)
+    {
+        return;
+    }
+    root->val = x;
+    dfs(root->left, 2 * x + 1);
+    dfs(root->right, 2 * x + 2);
+}
+FindElements(TreeNode *root)
+{
+    this->root = root;
+    dfs(root, 0);
+}
+bool search(TreeNode *root, int tar)
+{
+    if (!root)
+    {
+        return false;
+    }
+    if (root->val == tar)
+    {
+        return true;
+    }
+    return search(root->left, tar) || search(root->right, tar);
+}
+bool find(int tar)
+{
+    return search(root, tar);
+}
+
+// METHOD-2
+// DFS+ UNORDERED SET
+// TC= O(N)+O(1)
+unordered_set<int> st;
+void dfs(TreeNode *root, int x)
+{
+    if (!root)
+    {
+        return;
+    }
+    root->val = x;
+    st.insert(root->val);
+    dfs(root->left, 2 * x + 1);
+    dfs(root->right, 2 * x + 2);
+}
+FindElements(TreeNode *root)
+{
+    dfs(root, 0);
+}
+
+bool find(int target)
+{
+    return st.find(target) != st.end();
+}
  */
