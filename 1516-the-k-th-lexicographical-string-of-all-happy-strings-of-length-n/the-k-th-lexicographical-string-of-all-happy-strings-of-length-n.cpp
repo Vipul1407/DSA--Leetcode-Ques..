dee979ffb93cab2f7873366d93f59ea171a95393
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-21 23:12:39 +0530

    Time: 11 ms (49.71%) | Memory: 24.5 MB (38.59%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    void solve(int &cnt, string str, string &ans, char prev, int n, int &k)
+    // METHOD-1
+    // BRUTE FORCE Using vector to store all the strings..
+    void solve(string &str, char prev, int n, vector<string>&vec)
     {
         if(n==0)
         {
-            cnt++;
-            if(cnt==k)
-            {
-                ans= str;
-            }
+            vec.push_back(str);
             return;
         }
         for(char ch='a';ch<='c';ch++)
@@ -18,15 +16,54 @@ public:
                 continue;
             }
             str.push_back(ch);
-            solve(cnt,str,ans,ch,n-1,k);
+            solve(str,ch,n-1,vec);
             str.pop_back();
         }
     }
     string getHappyString(int n, int k) 
     {
-        string ans;
-        int cnt=0;
-        solve(cnt,"",ans,'*',n,k);
-        return ans;
+        vector<string>vec;
+        string str;
+        solve(str,'*',n,vec);
+        if(k>vec.size())
+        {
+            return "";
+        }
+        return vec[k-1];
     }
 };
+/*
+//  METHOD-2
+//  OPTIMIZED
+// Taking cnt in recursive calls...
+void solve(int &cnt, string str, string &ans, char prev, int n, int &k)
+{
+    if (n == 0)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            ans = str;
+        }
+        return;
+    }
+    for (char ch = 'a'; ch <= 'c'; ch++)
+    {
+        if (prev == ch)
+        {
+            continue;
+        }
+        str.push_back(ch);
+        solve(cnt, str, ans, ch, n - 1, k);
+        str.pop_back();
+    }
+}
+string getHappyString(int n, int k)
+{
+    string ans;
+    int cnt = 0;
+    solve(cnt, "", ans, '*', n, k);
+    return ans;
+}
+
+*/
