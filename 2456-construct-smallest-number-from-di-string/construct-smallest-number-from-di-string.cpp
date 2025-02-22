aa69bb1b163a296e4fd30b2a375614d083ce44d0
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-22 09:42:43 +0530

    Time: 39 ms (7.77%) | Memory: 7.7 MB (87.47%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
-    string smallestNumber(string pattern) {
-        int n = pattern.length();
-        string result = "";
-        int stack[n + 1];
-        int index = 0;
-
-        for (int i = 0; i <= n; i++) {
-            stack[index++] = i + 1;
-
-            if (i == n || pattern[i] == 'I') {
-                while (index > 0) {
-                    result += to_string(stack[--index]);
-                }
+    //METHOD-1
+    //BRUTE FORCE
+    //Using Next Permutation..
+    bool match_pattern(string num, string &pattern)
+    {
+        for(int i=0;i<pattern.size();i++)
+        {
+            if(pattern[i]=='I' && num[i]>num[i+1])
+            {
+                return false;
+            }
+            if(pattern[i]=='D' && num[i]<num[i+1])
+            {
+                return false;
             }
         }
-
-        return result;
+        return true;
+    }
+    string smallestNumber(string pattern) 
+    {
+        int n= pattern.size();
+        string num="";
+        for(int i=1;i<=n+1;i++)
+        {
+            num+= (i+'0'); 
+        }
+        while(!match_pattern(num,pattern))
+        {
+            next_permutation(num.begin(),num.end());
+        }
+        return num;
     }
 };
