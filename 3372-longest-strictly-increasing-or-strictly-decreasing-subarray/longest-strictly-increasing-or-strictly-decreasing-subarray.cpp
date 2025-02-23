68dfbd2fc4e36e66e5f549f81029f77f35fc8523
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-23 22:53:32 +0530

    Time: 4 ms (4.33%) | Memory: 27.9 MB (61.29%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
-    int longestMonotonicSubarray(vector<int>& nums) {
-        int n=nums.size(), i=0, ans=0;
-        int inc=1, dec=1;
-        if (n==1) return 1;
-        for(i=1 ;i<n; i++){
-            if (nums[i]>nums[i-1]) inc++, dec=1;
-            else if (nums[i]<nums[i-1]) inc=1, dec++;
-            else inc=dec=1;
-            ans=max({ans, dec, inc});
-        } 
-        return ans;   
+    int longestMonotonicSubarray(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int inc=1;
+        int maxinc=1;
+        int dec=1;
+        int maxdec=1;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                inc++;
+                dec=1;
+                maxinc= max(maxinc,inc);
+            }
+            else if(nums[i]>nums[i+1])
+            {
+                dec++;
+                inc=1;
+                maxdec= max(maxdec,dec);
+            }
+            else
+            {
+                inc=1;
+                dec=1;
+            }
+        }
+        return max(maxdec,maxinc);
     }
 };
