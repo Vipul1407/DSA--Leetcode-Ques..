fdf682c6a50ea40f5a0a33720b0ccc77dc536994
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-23 22:59:38 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (99.06%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -4,29 +4,27 @@ public:
     {
         int n= nums.size();
         int inc=1;
-        int maxinc=1;
         int dec=1;
-        int maxdec=1;
+        int ans=1;
         for(int i=0;i<n-1;i++)
         {
             if(nums[i]<nums[i+1])
             {
                 inc++;
                 dec=1;
-                maxinc= max(maxinc,inc);
             }
             else if(nums[i]>nums[i+1])
             {
                 dec++;
                 inc=1;
-                maxdec= max(maxdec,dec);
             }
             else
             {
                 inc=1;
                 dec=1;
             }
+            ans= max(ans,max(inc,dec));
         }
-        return max(maxdec,maxinc);
+        return ans;
     }
 };
