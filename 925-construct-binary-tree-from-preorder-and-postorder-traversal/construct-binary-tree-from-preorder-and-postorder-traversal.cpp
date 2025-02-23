91a812751b26e3929215bb9e623161e452575cc2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-23 21:44:37 +0530

    Time: 2 ms (45.01%) | Memory: 28.1 MB (90.13%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -10,7 +10,38 @@
  * };
  */
 class Solution {
-public: 
+public:
+    // METHOD-1
+    // BRUTE FORCE..
+    TreeNode *solve(int prestart, int poststart, int preend, vector<int> &preorder, vector<int> &postorder)
+    {
+        if (prestart > preend)
+        {
+            return NULL;
+        }
+        TreeNode *root = new TreeNode(preorder[prestart]);
+        if (prestart == preend)
+        {
+            return root;
+        }
+        int nextnode = preorder[prestart + 1];
+        int j = poststart;
+        while (postorder[j] != nextnode)
+        {
+            j++;
+        }
+        int num = j - poststart + 1;
+        root->left = solve(prestart + 1, poststart, prestart + num, preorder, postorder);
+        root->right = solve(prestart + num + 1, j + 1, preend, preorder, postorder);
+        return root;
+    }
+    TreeNode *constructFromPrePost(vector<int> &preorder, vector<int> &postorder)
+    {
+        int n = preorder.size();
+        return solve(0, 0, n - 1, preorder, postorder);
+    }
+};
+/*
     // METHOD-2
     // OPTIMIZED APPROACH
     TreeNode* solve(int prestart,int poststart,int preend,vector<int>& preorder,vector<int>& postorder,unordered_map<int,int>&mp)
@@ -42,4 +73,4 @@ public:
         }
         return solve(0,0,n-1,preorder,postorder,mp);
     }
-};
+*/
