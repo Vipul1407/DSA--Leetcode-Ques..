313a37a012ffa61860f35117ded89cd0a3457d46
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-23 12:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (7.05%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,7 +1,37 @@
 class Solution {
 public:
+    //METHOD-2
+    //BETTER APPROACH
+    //TC= O(N*N)
+    bool check(vector<int>& nums) 
+    {
+        int n= nums.size();
+        bool flag= true;
+        vector<int>vec= nums;
+        sort(vec.begin(),vec.end());
+        for(int r=0;r<n;r++)
+        {
+            bool flag= true;
+            for(int i=0;i<n;i++)
+            {
+                if(nums[i]!=vec[(i+r)%n])
+                {
+                    flag= false;
+                    break;
+                }
+            }
+            if(flag==true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+/*
     //METHOD-1
     //BRUTE FORCE..
+    //TC= O(N*3N)
     bool check(vector<int>& nums) 
     {
         int n= nums.size();
@@ -16,8 +46,8 @@ public:
             {
                 vec.push_back(nums[i]);
             }
+            //check is it sorted ..
             bool sorted= true;
-            //check is it sorted also..
             for(int i=0;i<n-1;i++)
             {
                 if(vec[i]>vec[i+1])
@@ -27,10 +57,10 @@ public:
                 }
             }
             if(sorted)
-            {
+            { 
                 return true;
             }
         }
         return false;
     }
-};
+*/
