d4faafa2793d6aa749a1fdc75092a305d786fd74
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-23 09:49:08 +0530

    Time: 71 ms (51.20%) | Memory: 87.8 MB (45.97%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -1,22 +1,71 @@
 class Solution {
 public:
-    //METHOD-2
-    //BETTER APPROACH..
+    // METHOD-3
+    // OPTIMIZED APPROACH..
     long long countBadPairs(vector<int>& nums) 
     {
         int n= nums.size();
-        for(int i=0;i<n;i++)
-        {
-            nums[i]-=i;
-        }
+        long long ans=0;
         unordered_map<int,int>mp;
         mp[nums[0]]++;
-        long long ans=0;
         for(int i=1;i<n;i++)
         {
-            ans+= (i-mp[nums[i]]);
-            mp[nums[i]]++;
+            ans+= i-mp[nums[i]-i];
+            mp[nums[i]-i]++;
         }
         return ans;
     }
 };
+/*
+
+// METHOD-1
+// OPTIMIZED BRUTE APPROACH..
+// TLE
+long long countBadPairs(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        nums[i] -= i;
+    }
+    unordered_map<int, int> mp;
+    mp[nums[0]]++;
+    int ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int curr = nums[i];
+        for (auto j : mp)
+        {
+            if (curr == j.first)
+            {
+                continue;
+            }
+            ans += j.second;
+        }
+        mp[curr]++;
+    }
+
+    return ans;
+}
+
+// METHOD-2
+// BETTER APPROACH..
+long long countBadPairs(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        nums[i] -= i;
+    }
+    unordered_map<int, int> mp;
+    mp[nums[0]]++;
+    long long ans = 0;
+    for (int i = 1; i < n; i++)
+    {
+        ans += (i - mp[nums[i]]);
+        mp[nums[i]]++;
+    }
+    return ans;
+}
+
+*/
