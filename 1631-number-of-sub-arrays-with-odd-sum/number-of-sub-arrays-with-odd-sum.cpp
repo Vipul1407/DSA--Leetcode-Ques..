8370ff0a94dac5d03c42a0248a15ecbd57fadbc5
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-25 13:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 111.9 MB (80.00%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,6 +1,80 @@
 class Solution {
 public:
-    //METHOD-3
+    //METHOD-4
+    //OPTIMIZED WITHOUT VARIABLE..
+    #define MOD 1000000007
+    int numOfSubarrays(vector<int>& arr) 
+    {
+        int n= arr.size();
+        long long ans=0;
+        int pref=0;
+  
+        //odd+even = odd
+        //even+odd= odd
+        //only odd--> to tackle this case we start even=1 initailly
+        //Then we iterate on pref vector and if , it is odd then we add even and if it is even then we cnt oddcnt
+        int odd=0;
+        int even=1;
+        for(int i=0;i<n;i++)
+        {
+            pref+= arr[i];
+            if(pref%2==0)
+            {
+                ans+= odd;
+                even++;
+            }
+            else
+            {
+                ans+= even;
+                odd++;
+            }
+        }
+        return ans%MOD;
+    }
+};
+/*
+//METHOD-1
+    //BRUTE FORCE
+    #define MOD 1000000007
+    int numOfSubarrays(vector<int>& arr) 
+    {
+        int n= arr.size();
+        long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                int sum=0;
+                for(int k=i;k<=j;k++)
+                {
+                    sum+= arr[k];
+                }
+                ans+= sum%2;
+            }
+        }    
+        return ans%MOD;
+    }
+
+//METHOD-2
+    //BRUTE FORCE OPTIMIZED
+    #define MOD 1000000007
+    int numOfSubarrays(vector<int>& arr) 
+    {
+        int n= arr.size();
+        long long ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum+= arr[j];
+                ans+= sum%2;
+            }
+        }    
+        return ans%MOD;
+    }
+
+//METHOD-3
     //OPTIMIZED TAKING VECTOR..
     #define MOD 1000000007
     int numOfSubarrays(vector<int>& arr) 
@@ -33,45 +107,5 @@ public:
             }
         }
         return ans%MOD;
-    }
-};
-/*
-//METHOD-1
-    //BRUTE FORCE
-    int numOfSubarrays(vector<int>& arr) 
-    {
-        int n= arr.size();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-            {
-                int sum=0;
-                for(int k=i;k<=j;k++)
-                {
-                    sum+= arr[k];
-                }
-                ans+= sum%2;
-            }
-        }    
-        return ans;
-    }
-
-//METHOD-2
-    //BRUTE FORCE OPTIMIZED
-    int numOfSubarrays(vector<int>& arr) 
-    {
-        int n= arr.size();
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            int sum=0;
-            for(int j=i;j<n;j++)
-            {
-                sum+= arr[j];
-                ans+= sum%2;
-            }
-        }    
-        return ans;
     }
 */
