d95b823f2e7e447f8be8f44b3d6c1c67c7e804a2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-02-25 15:58:58 +0530

    Time: 216 ms (40.68%) | Memory: 172.9 MB (33.05%) - LeetSync

--- 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
+++ 2564-most-profitable-path-in-a-tree/most-profitable-path-in-a-tree.cpp
@@ -1,53 +1,71 @@
 class Solution {
 public:
-    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
-        int n = amount.size();
-        vector<vector<int>> graph(n);
-        
-        for (auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+    unordered_map<int,vector<int>>adj;
+    unordered_map<int,int>bobmap;
+    int aliceincome=INT_MIN;
+    bool dfsbob(int curr, int t, vector<bool>&vis)
+    {
+        vis[curr]= true;
+        bobmap[curr]= t;
+        if(curr==0)
+        {
+            return true;
         }
-
-        vector<int> bobPath(n, -1);
-        vector<int> path;
-        
-        function<bool(int, int)> fillBobPath = [&](int node, int parent) {
-            if (node == 0) return true;
-            
-            for (int neighbor : graph[node]) {
-                if (neighbor != parent) {
-                    path.push_back(node);
-                    if (fillBobPath(neighbor, node)) return true;
-                    path.pop_back();
+        for(auto &neigh: adj[curr])
+        {
+            if(!vis[neigh])
+            {
+                if(dfsbob(neigh,t+1,vis))
+                {
+                    return true;
                 }
             }
-            return false;
-        };
-
-        fillBobPath(bob, -1);
-        for (int i = 0; i < path.size(); i++) {
-            bobPath[path[i]] = i;
         }
-
-        function<int(int, int, int, int)> getAliceMaxScore = [&](int node, int parent, int currScore, int timestamp) {
-            if (bobPath[node] == -1 || bobPath[node] > timestamp) {
-                currScore += amount[node];
-            } else if (bobPath[node] == timestamp) {
-                currScore += amount[node] / 2;
+        bobmap.erase(curr);
+        return false;
+    }
+    void dfsalice(int curr, int t,int income,vector<bool>&vis,vector<int>& amount)
+    {
+        vis[curr]= true;
+        if(bobmap.find(curr)==bobmap.end() || t< bobmap[curr])
+        {
+            income+= amount[curr];
+        }
+        else if(t==bobmap[curr])
+        {
+            income+= (amount[curr]/2);
+        }
+        //leaf node..
+        if(adj[curr].size()==1 && curr!=0)
+        {
+            aliceincome= max(aliceincome,income);
+        }
+        for(auto &neigh: adj[curr])
+        {
+            if(!vis[neigh])
+            {
+                dfsalice(neigh,t+1,income,vis,amount);
             }
+        }
+    }
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) 
+    {
+        int n= amount.size();
+        for(vector<int>&edge: edges)
+        {
+            int u= edge[0];
+            int v= edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        //dfs on bob to find time to reach 0
 
-            if (graph[node].size() == 1 && node != 0) return currScore;
+        vector<bool>vis(n,false);
+        dfsbob(bob,0,vis);
 
-            int maxScore = INT_MIN;
-            for (int neighbor : graph[node]) {
-                if (neighbor != parent) {
-                    maxScore = max(maxScore, getAliceMaxScore(neighbor, node, currScore, timestamp + 1));
-                }
-            }
-            return maxScore;
-        };
-
-        return getAliceMaxScore(0, -1, 0, 0);
+        int income=0;
+        vis.assign(n,false);
+        dfsalice(0,0,income,vis,amount);
+        return aliceincome;
     }
 };
