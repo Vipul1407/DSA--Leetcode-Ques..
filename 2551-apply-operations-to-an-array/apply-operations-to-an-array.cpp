c5aba5cd1b3de62a566f3200d9418b4334f1c6a3
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-01 12:24:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
-    //METHOD-2
-    //USING IMPLACE SWAPPING
     vector<int> applyOperations(vector<int>& nums) 
     {
         int n= nums.size();
+        vector<int>ans;
         for(int i=0;i<n-1;i++)
         {
             if(nums[i]==nums[i+1])
@@ -13,18 +12,49 @@ public:
                 nums[i+1]=0;
             }
         }
-        int i=0;//find zero element
-        int j=0;//find non zero so that it can be swapped..
-        while(j<n)
+        int cnt=0;
+        for(auto i:nums)
         {
-            //i will remain at 0 element..
-            if(nums[j]!=0)
+            if(i!=0)
             {
-                swap(nums[i],nums[j]);
-                i++;
+                ans.push_back(i);
+                cnt++;
             }
-            j++;
         }
-        return nums;
+        int len= n-cnt;
+        while(len--)
+        {
+            ans.push_back(0);
+        }
+        return ans;
     }
 };
+/*
+    {// METHOD-2
+// USING IMPLACE SWAPPING
+vector<int> applyOperations(vector<int> &nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (nums[i] == nums[i + 1])
+        {
+            nums[i] *= 2;
+            nums[i + 1] = 0;
+        }
+    }
+    int i = 0; // find zero element
+    int j = 0; // find non zero so that it can be swapped..
+    while (j < n)
+    {
+        // i will remain at 0 element..
+        if (nums[j] != 0)
+        {
+            swap(nums[i], nums[j]);
+            i++;
+        }
+        j++;
+    }
+    return nums;
+}
+*/
