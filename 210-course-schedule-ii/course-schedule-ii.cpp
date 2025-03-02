16b9b658a30521d7d802fbf696bda80f9076040a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-02 22:01:10 +0530

    Time: 12 ms (10.55%) | Memory: 19.6 MB (10.97%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,7 +1,71 @@
 class Solution {
 public:
+    //METHOD-2
+    //NOT A TOPO SORT DFS..  (It's a Cycle Detection in Directed Graph + Stack)
+    bool dfs(int i, unordered_map<int, vector<int>> &adj, vector<int> &vis, vector<int> &pathvis, stack<int>&st)
+    {
+        vis[i] = 1;
+        pathvis[i] = 1;
+        for (auto neigh : adj[i])
+        {
+            // cycle mil gyi..
+            if (vis[neigh] && pathvis[neigh])
+            {
+                return true;
+            }
+            if (!vis[neigh])
+            {
+                if (dfs(neigh, adj, vis, pathvis, st))
+                {
+                    return true;
+                }
+            }
+        }
+        // backtracking..
+        pathvis[i] = 0;
+        st.push(i);
+        return false;
+    }
+    vector<int> findOrder(int course, vector<vector<int>> &pre)
+    {
+        vector<int> pathvis(course, 0);
+        vector<int> vis(course, 0);
+        stack<int>st;
+        unordered_map<int, vector<int>> adj;
+         for (auto i : pre)
+        {
+            // 0->1
+            // then 1 should come before 0..
+            adj[i[1]].push_back(i[0]);
+        }
+        // to handle all disconnected components..
+        for (int i = 0; i < course; i++)
+        {
+            if (!vis[i])
+            {
+                //agar cylce mil gyi..
+                if(dfs(i, adj, vis, pathvis, st))
+                {
+                    return {};
+                }
+            }
+        }
+        if(st.size()!= course)
+        {
+            return {};
+        }
+        vector<int>ans;
+        while(st.size())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
+/*
     // METHOD-1
-    // TOPO SORT USING BFS..(KAHN ALGO..)
+    // TOPO SORT USING BFS..(KAHN ALGO..) as it ensures cycle detection also..
     vector<int> findOrder(int course, vector<vector<int>> &pre)
     {
         queue<int> q;
@@ -43,4 +107,4 @@ public:
         }
         return ans;
     }
-};
+*/
