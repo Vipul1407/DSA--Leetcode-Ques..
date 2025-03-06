59c77f33d3206e4d332da71701ecfc982397ad6e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-06 19:10:50 +0530

    Time: 37 ms (14.17%) | Memory: 110.3 MB (15.98%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,5 +1,61 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    vector<int>dx= {0,0,1,-1};
+    vector<int>dy= {1,-1,0,0};
+    int bfs(vector<vector<int>>& grid, int i,int j,vector<vector<int>>&vis)
+    {
+        int m= grid.size();
+        int n= grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({i,j});
+        vis[i][j]=1;
+        int sum=grid[i][j];
+        while(q.size())
+        {
+            int x= q.front().first;
+            int y= q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++)
+            {
+                int nx= x+dx[k];
+                int ny= y+dy[k];
+                if(nx>=0 && ny>=0 && nx<m && ny<n)
+                {   
+                    if(!vis[nx][ny] && grid[nx][ny]>=1)
+                    {
+                        sum+= grid[nx][ny];
+                        vis[nx][ny]=1;
+                        q.push({nx,ny});
+                    }
+                }
+            }
+        }
+        return sum;
+    }
+    int findMaxFish(vector<vector<int>>& grid) 
+    {
+        int m= grid.size();
+        int n= grid[0].size();
+        int ans=0;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(!vis[i][j] && grid[i][j]>=1)
+                {
+                    ans= max(ans,bfs(grid,i,j,vis));
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //DFS..
     vector<int>dx= {0,0,1,-1};
     vector<int>dy= {1,-1,0,0};
     int dfs(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&vis)
@@ -28,7 +84,6 @@ public:
         int n= grid[0].size();
         int ans=0;
         vector<vector<int>>vis(m,vector<int>(n,0));
-        int maxans=0;
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -41,4 +96,4 @@ public:
         }
         return ans;
     }
-};
+*/
