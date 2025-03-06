735c9a51c55656a79fdeee14c6ae9698e7f27f61
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-06 16:29:43 +0530

    Time: 3 ms (20.29%) | Memory: 7.7 MB (91.18%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    long long coloredCells(int n) {
-        return 1 + 4LL * n * (n - 1) / 2; 
+    
+    // METHOD-2
+    // Aryan Mittal..
+    long long coloredCells(int n) 
+    {
+        long long ans=1;
+        long long val=4;
+        if(n==1)
+        {
+            return 1;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            ans+= val; 
+            val= 4*i;
+        }
+        return ans;
     }
 };
+/*
+// METHOD-1
+long long coloredCells(int n)
+{
+    return 1 + 4LL * n * (n - 1) / 2;
+}
+*/
