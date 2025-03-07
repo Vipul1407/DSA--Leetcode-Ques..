c3acf34dcb3755814d95e165d0e18e57937b417a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-07 19:11:09 +0530

    Time: 22 ms (5.10%) | Memory: 18.5 MB (6.90%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    bool isconnected(int u, int v, unordered_map<int,vector<int>>&adj, vector<int>&vis)
+    //METHOD-2
+    //BFS..
+    bool isconnect(int u,int v,unordered_map<int,vector<int>>&adj)
     {
-        vis[u]=1;
-        if(u==v)
+        unordered_set<int>vis;
+        vis.insert(u);
+        queue<int>q;
+        q.push(u);
+        while(q.size())
         {
-            return true;
-        }
-        for(auto &i:adj[u])
-        {
-            if(!vis[i] && isconnected(i,v,adj,vis))
+            auto top= q.front();
+            q.pop();
+            if(top==v)
             {
                 return true;
+            }
+            for(auto &i:adj[top])
+            {
+                if(vis.find(i)==vis.end())
+                {
+                    q.push(i);
+                    vis.insert(i);
+                }
             }
         }
         return false;
@@ -19,14 +30,13 @@ public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) 
     {
         int n= edges.size();
-        unordered_map<int,vector<int>>adj;
         vector<int>ans;
+        unordered_map<int,vector<int>>adj;
         for(auto i:edges)
         {
             int u= i[0];
             int v= i[1];
-            vector<int>vis(n+1,0);
-            if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end() && isconnected(u,v,adj,vis))
+            if(adj.find(u)!=adj.end() &&  adj.find(v)!=adj.end() && isconnect(u,v,adj))
             {
                 ans.push_back(u);
                 ans.push_back(v);
@@ -37,3 +47,43 @@ public:
         return ans;
     }
 };
+/*
+// METHOD-1
+// DFS..
+bool isconnected(int u, int v, unordered_map<int, vector<int>> &adj, vector<int> &vis)
+{
+    vis[u] = 1;
+    if (u == v)
+    {
+        return true;
+    }
+    for (auto &i : adj[u])
+    {
+        if (!vis[i] && isconnected(i, v, adj, vis))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+vector<int> findRedundantConnection(vector<vector<int>> &edges)
+{
+    int n = edges.size();
+    unordered_map<int, vector<int>> adj;
+    vector<int> ans;
+    for (auto i : edges)
+    {
+        int u = i[0];
+        int v = i[1];
+        vector<int> vis(n, 0);
+        if (adj.find(u) != adj.end() && adj.find(v) != adj.end() && isconnected(u, v, adj, vis))
+        {
+            ans.push_back(u);
+            ans.push_back(v);
+        }
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    return ans;
+}
+*/
