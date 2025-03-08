b67458900a619e4ae01f8234cbe1514ff8c9bc7c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-08 22:14:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (8.46%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,21 +1,54 @@
 class Solution {
 public:
+    // METHOD-2
+    // OPTIMIZED..
+    // SLIDING WINDOW..
     int minimumRecolors(string blocks, int k) 
     {
         int n= blocks.size();
         int ans=n;
-        for(int i=0;i<=n-k;i++)
-        {   
-            int white=0;
-            for(int j=i;j<i+k;j++)
+        int i=0,j=0;
+        int white=0;
+        while(j<n)
+        {
+            if(blocks[j]=='W')
             {
-                if(blocks[j]=='W')
-                {
-                    white++;
-                }
+                white++;
             }
-            ans= min(ans,white);
+            if(j-i+1==k)
+            {
+                ans= min(ans,white);
+                if(blocks[i]=='W')
+                {
+                    white--;
+                }
+                i++;
+            }
+            j++;
         }
         return ans;
     }
 };
+/*
+// METHOD-1
+// BRUTE FORCE
+// BEATS 100%
+int minimumRecolors(string blocks, int k)
+{
+    int n = blocks.size();
+    int ans = n;
+    for (int i = 0; i <= n - k; i++)
+    {
+        int white = 0;
+        for (int j = i; j < i + k; j++)
+        {
+            if (blocks[j] == 'W')
+            {
+                white++;
+            }
+        }
+        ans = min(ans, white);
+    }
+    return ans;
+}
+*/
