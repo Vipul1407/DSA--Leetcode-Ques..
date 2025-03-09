ede978e6963dda0dd09544db291bf1ffa149728d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-09 22:56:58 +0530

    Time: 11 ms (14.42%) | Memory: 27 MB (56.49%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.cpp
+++ 3463-alternating-groups-i/alternating-groups-i.cpp
@@ -1,12 +1,43 @@
 class Solution {
 public:
+    //METHOD-2
+    //SIMPLE APPROACH
+    int numberOfAlternatingGroups(vector<int>& colors) 
+    {
+        int n= colors.size();
+        colors.push_back(colors[0]);
+        colors.push_back(colors[1]); 
+        int ans=0,prev=-1,len=0;
+        for(auto i:colors)
+        {
+            if(prev==-1 || i!=prev)
+            {
+                len++;
+                if(len>=3)
+                {
+                    ans++;
+                }
+            }
+            else
+            {
+                len=1;
+            }
+            prev=i;
+        }
+        return ans;
+    }
+};
+/*
+//METHOD-1
+// BRUTE FORCE..
+// BEATS 88%
     int numberOfAlternatingGroups(vector<int>& colors) 
     {
         int n= colors.size();
         colors.push_back(colors[0]);
         colors.push_back(colors[1]); 
         int ans=0;
-        for(int i=1;i<n+1;i++)
+        for(int i=1;i<n+2;i++)
         {
             if(colors[i]!=colors[i-1] && colors[i]!=colors[i+1])
             {
@@ -15,4 +46,4 @@ public:
         }
         return ans;
     }
-};
+*/
