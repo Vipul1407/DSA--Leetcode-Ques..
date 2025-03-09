f133f83318348ee293688998c51380485714f3e5
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-09 15:51:09 +0530

    Time: 78 ms (29.71%) | Memory: 102.7 MB (76.78%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -1,30 +1,77 @@
 class Solution {
 public:
+    //METHOD-2
+    //SIMPLE APPROACH
     int numberOfAlternatingGroups(vector<int>& colors, int k) 
     {
         int n= colors.size();
+        int len=0;
         int ans=0;
-        int i=0;
-        int j=1;
+        int prev=-1;
+        for(auto i:colors)
+        {
+            if(prev==-1 || prev!=i)
+            {
+                len++;
+                if(len>=k)
+                {
+                    ans++;
+                }
+            }
+            else
+            {
+                len=1;
+            }
+            prev=i;
+        }
         for(int i=0;i<k-1;i++)
         {
-            colors.push_back(colors[i]);
-        }
-        while(j<n+k-1)
-        {
-            if(colors[j-1]==colors[j])
+            if(colors[i]!=prev)
             {
-                i=j;
-                j++;
-                continue;
+                len++;
+                if(len>=k)
+                {
+                    ans++;
+                }
             }
-            if(j-i+1==k)
+            else
             {
-                ans++;
-                i++;
+                len=1;
             }
-            j++;
+            prev= colors[i];
         }
         return ans;
     }
 };
+/*
+// METHOD-1
+// SLIDING WINDOW..
+int numberOfAlternatingGroups(vector<int> &colors, int k)
+{
+    int n = colors.size();
+    int ans = 0;
+    int i = 0;
+    int j = 1;
+    // pushing starting k-1 elements to last to maintain circular order..
+    for (int i = 0; i < k - 1; i++)
+    {
+        colors.push_back(colors[i]);
+    }
+    while (j < n + k - 1)
+    {
+        if (colors[j - 1] == colors[j])
+        {
+            i = j;
+            j++;
+            continue;
+        }
+        if (j - i + 1 == k)
+        {
+            ans++;
+            i++;
+        }
+        j++;
+    }
+    return ans;
+}
+*/
