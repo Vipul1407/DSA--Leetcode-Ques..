dcbddc5cb241216a114f4bbe4ebd616cb37d03d3
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-09 23:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (56.49%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.cpp
+++ 3463-alternating-groups-i/alternating-groups-i.cpp
@@ -1,7 +1,56 @@
 class Solution {
 public:
-    //METHOD-2
-    //SIMPLE APPROACH
+    //METHOD-3
+    //SLIDING WINDOW..
+    int numberOfAlternatingGroups(vector<int>& colors) 
+    {
+        int n= colors.size();
+        colors.push_back(colors[0]);
+        colors.push_back(colors[1]); 
+        int i=0;
+        int j=1;
+        int ans=0;
+        while(j<n+2)
+        {
+            if(colors[j-1]==colors[j])
+            {
+                i=j;
+                j++;
+                continue;
+            }
+            if(j-i+1==3)
+            {
+                ans++;
+                i++;
+            }
+            j++;
+        }
+        return ans;
+    }
+};
+/*
+//METHOD-1
+// BRUTE FORCE..
+// BEATS 88%
+    int numberOfAlternatingGroups(vector<int>& colors) 
+    {
+        int n= colors.size();
+        colors.push_back(colors[0]);
+        colors.push_back(colors[1]); 
+        int ans=0;
+        for(int i=1;i<n+2;i++)
+        {
+            if(colors[i]!=colors[i-1] && colors[i]!=colors[i+1])
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+//METHOD-2
+//SIMPLE APPROACH
+//BEATS 14%
     int numberOfAlternatingGroups(vector<int>& colors) 
     {
         int n= colors.size();
@@ -23,26 +72,6 @@ public:
                 len=1;
             }
             prev=i;
-        }
-        return ans;
-    }
-};
-/*
-//METHOD-1
-// BRUTE FORCE..
-// BEATS 88%
-    int numberOfAlternatingGroups(vector<int>& colors) 
-    {
-        int n= colors.size();
-        colors.push_back(colors[0]);
-        colors.push_back(colors[1]); 
-        int ans=0;
-        for(int i=1;i<n+2;i++)
-        {
-            if(colors[i]!=colors[i-1] && colors[i]!=colors[i+1])
-            {
-                ans++;
-            }
         }
         return ans;
     }
