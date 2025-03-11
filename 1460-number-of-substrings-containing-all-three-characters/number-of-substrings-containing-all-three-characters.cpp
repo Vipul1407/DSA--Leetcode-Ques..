185f3786f97005111331f2ca775d550ae0d75ccb
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-11 11:17:07 +0530

    Time: 3 ms (90.05%) | Memory: 11 MB (81.05%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
+    //METHOD-2
+    //Using Vector..
     int numberOfSubstrings(string s) 
     {
-        int a=0;
-        int b=0;
-        int c=0;
         int n= s.size();
-        int i=0,j=0;
-        int ans=0;
+        vector<int>cnt(3,0);
+        int ans=0,i=0,j=0;
         while(j<n)
         {
-            if(s[j]=='a')
-            {
-                a++;
-            }
-            else if(s[j]=='b')
-            {
-                b++;
-            }
-            else
-            {
-                c++;
-            }
-            while(a>0 && b>0 && c>0)
+            cnt[s[j]-'a']++;
+            while(cnt[0]>0 && cnt[1]>0 && cnt[2]>0)
             {
                 ans+= n-j;
-                if(s[i]=='a')
-                {
-                    a--;
-                }
-                else if(s[i]=='b')
-                {
-                    b--;
-                }
-                else
-                {
-                    c--;
-                }
+                cnt[s[i]-'a']--;
                 i++;
             }
             j++;
@@ -44,3 +21,50 @@ public:
         return ans;
     }
 };
+/*
+//METHOD-1
+//Using Variables..
+int numberOfSubstrings(string s)
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int n = s.size();
+    int i = 0, j = 0;
+    int ans = 0;
+    while (j < n)
+    {
+        if (s[j] == 'a')
+        {
+            a++;
+        }
+        else if (s[j] == 'b')
+        {
+            b++;
+        }
+        else
+        {
+            c++;
+        }
+        while (a > 0 && b > 0 && c > 0)
+        {
+            ans += n - j;
+            if (s[i] == 'a')
+            {
+                a--;
+            }
+            else if (s[i] == 'b')
+            {
+                b--;
+            }
+            else
+            {
+                c--;
+            }
+            i++;
+        }
+        j++;
+    }
+    return ans;
+}
+*/
