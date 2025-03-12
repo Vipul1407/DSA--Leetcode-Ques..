04db12897455c7668ebf507b2d9d716d161b7b5f
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-12 16:51:37 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (11.89%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    //METHOD-3
-    //BFS..
-    void bfs(int i,unordered_map<int,vector<int>>& adj,vector<int>& vis)
+    // METHOD-4
+    // USING DFS FORMING ADJ LIST...
+    void dfs(int i, unordered_map<int,vector<int>>&adj, vector<int>&vis)
     {
-        queue<int>q;
-        q.push(i);
         vis[i]=1;
-        while(q.size())
+        for(auto &j:adj[i])
         {
-            int newi= q.front();
-            q.pop();
-            for(auto j:adj[newi])
+            if(!vis[j])
             {
-                if(!vis[j])
-                {
-                    q.push(j);
-                    vis[j]=1;
-                }
+                dfs(j,adj,vis);
             }
-        }
+        } 
     }
     int findCircleNum(vector<vector<int>>& graph) 
     {
-        int n= graph.size(); 
+        int n= graph.size();
+        unordered_map<int,vector<int>>adj;
         vector<int>vis(n,0);
         int cnt=0;
-        unordered_map<int,vector<int>>adj;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
@@ -42,46 +34,134 @@ public:
         {
             if(!vis[i])
             {
-                bfs(i,adj,vis);
                 cnt++;
+                dfs(i,adj,vis);
             }
         }
         return cnt;
     }
 };
 /*
-//METHOD-1
-    //DFS..
-    //TC= O(N^2)
-    void dfs(int i,vector<vector<int>>& graph,vector<int>& vis)
+// METHOD-1
+// DFS.. WITHOUT USING ADJ LIST...
+// TC= O(N^2)
+void dfs(int i, vector<vector<int>> &graph, vector<int> &vis)
+{
+    if (vis[i])
     {
-        if(vis[i])
+        return;
+    }
+    vis[i] = 1;
+    for (int j = 0; j < graph.size(); j++)
+    {
+        if (graph[i][j] == 1)
         {
-            return;
+            dfs(j, graph, vis);
         }
-        vis[i]=1;
-        for(int j=0;j<graph.size();j++)
+    }
+}
+int findCircleNum(vector<vector<int>> &graph)
+{
+    int n = graph.size();
+
+    vector<int> vis(n, 0);
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
         {
-            if(graph[i][j]==1)
+            cnt++;
+            dfs(i, graph, vis);
+        }
+    }
+    return cnt;
+}
+
+// METHOD-2
+//  BFS..  WITHOUT USING ADJ LIST...
+//  TC= O(N^2)
+void bfs(int i, vector<vector<int>> &graph, vector<int> &vis)
+{
+    queue<int> q;
+    q.push(i);
+    vis[i] = 1;
+    while (q.size())
+    {
+        int newi = q.front();
+        q.pop();
+        for (int j = 0; j < graph.size(); j++)
+        {
+            if (!vis[j] && graph[newi][j] == 1)
             {
-                dfs(j,graph,vis);
+                q.push(j);
+                vis[j] = 1;
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& graph) 
+}
+int findCircleNum(vector<vector<int>> &graph)
+{
+    int n = graph.size();
+
+    vector<int> vis(n, 0);
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
     {
-        int n= graph.size();
-        
-        vector<int>vis(n,0);
-        int cnt=0;
-        for(int i=0;i<n;i++)
+        if (!vis[i])
         {
-            if(!vis[i])
-            {
-                cnt++;
-                dfs(i,graph,vis);
-            }
-        }    
-        return cnt;
+            cnt++;
+            bfs(i, graph, vis);
+        }
     }
+    return cnt;
+}
+
+// METHOD-3
+// USING BFS FORMING ADJ LIST...
+void bfs(int i, unordered_map<int, vector<int>> &adj, vector<int> &vis)
+{
+    queue<int> q;
+    q.push(i);
+    vis[i] = 1;
+    while (q.size())
+    {
+        int newi = q.front();
+        q.pop();
+        for (auto j : adj[newi])
+        {
+            if (!vis[j])
+            {
+                q.push(j);
+                vis[j] = 1;
+            }
+        }
+    }
+}
+int findCircleNum(vector<vector<int>> &graph)
+{
+    int n = graph.size();
+    vector<int> vis(n, 0);
+    int cnt = 0;
+    unordered_map<int, vector<int>> adj;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (graph[i][j] == 1)
+            {
+                adj[i].push_back(j);
+                adj[j].push_back(i);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            bfs(i, adj, vis);
+            cnt++;
+        }
+    }
+    return cnt;
+}
 */
