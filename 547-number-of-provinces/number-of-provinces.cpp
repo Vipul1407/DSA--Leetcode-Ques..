ac9796556bdc1a46203005cd18924c319e51747b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-12 14:55:39 +0530

    Time: 4 ms (32.04%) | Memory: 19.8 MB (56.26%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,5 +1,48 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    //TC= O(N^2)
+    void bfs(int i,vector<vector<int>>& graph,vector<int>& vis)
+    {
+        queue<int>q;
+        q.push(i);
+        vis[i]=1;
+        while(q.size())
+        {
+            int newi= q.front();
+            q.pop();
+            for(int j=0;j<graph.size();j++)
+            {
+                if(!vis[j] && graph[newi][j]==1)
+                {
+                    q.push(j);
+                    vis[j]=1;
+                }
+            }
+        }
+    }
+    int findCircleNum(vector<vector<int>>& graph) 
+    {
+        int n= graph.size();
+        
+        vector<int>vis(n,0);
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                cnt++;
+                bfs(i,graph,vis);
+            }
+        }    
+        return cnt;
+    }
+};
+/*
+//METHOD-1
+    //DFS..
+    //TC= O(N^2)
     void dfs(int i,vector<vector<int>>& graph,vector<int>& vis)
     {
         if(vis[i])
@@ -31,4 +74,4 @@ public:
         }    
         return cnt;
     }
-};
+*/
