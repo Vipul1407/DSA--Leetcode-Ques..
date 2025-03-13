90863970b3b188578b29dd7ff397dbf1debf3aef
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-13 12:25:12 +0530

    Time: 15 ms (24.58%) | Memory: 106.2 MB (12.61%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    //METHOD-1
-    //DFS..
+    //METHOD-2
+    //BFS..
     vector<int>dx= {-1,0,1,0};
     vector<int>dy= {0,1,0,-1};
-    int dfs(int i,int j,vector<vector<int>>& grid)
+    int bfs(int i,int j,vector<vector<int>>& grid)
     {
         int m= grid.size();
         int n= grid[0].size();
-        if(i<0 || j<0 || i>=m || j>=n ||grid[i][j]==0)
-        {
-            return 1;
-        }
-        if(grid[i][j]==-1)
-        {
-            return 0;
-        }
+        
+        queue<pair<int,int>>q;
+        q.push({i,j});
         grid[i][j]=-1;//mark visted..
+
         int peri=0;
-        for(int k=0;k<4;k++)
+        while(q.size())
         {
-            int nx= i+dx[k];
-            int ny= j+dy[k];
-            peri+= dfs(nx,ny,grid);
+            int x= q.front().first;
+            int y= q.front().second;
+            q.pop();
+            for(int k=0;k<4;k++)
+            {
+                int nx= x+dx[k];
+                int ny= y+dy[k];
+                if(nx<0 || ny<0 || nx>=m || ny>=n || grid[nx][ny]==0)
+                {
+                    peri++;
+                }
+                else if(grid[nx][ny]==-1)
+                {
+                    continue;
+                }
+                else
+                {
+                    q.push({nx,ny});
+                    grid[nx][ny]=-1;
+                }
+            }
         }
         return peri;
     }
@@ -37,10 +51,57 @@ public:
             {
                 if(grid[i][j]==1)
                 {
-                    peri+= dfs(i,j,grid);
+                    peri+= bfs(i,j,grid);
                 }
             }
         }
         return peri;
     }
 };
+/*
+// NEW WAY----------------------------
+
+// METHOD-1
+// DFS..
+vector<int> dx = {-1, 0, 1, 0};
+vector<int> dy = {0, 1, 0, -1};
+int dfs(int i, int j, vector<vector<int>> &grid)
+{
+    int m = grid.size();
+    int n = grid[0].size();
+    if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0)
+    {
+        return 1;
+    }
+    if (grid[i][j] == -1)
+    {
+        return 0;
+    }
+    grid[i][j] = -1; // mark visted..
+    int peri = 0;
+    for (int k = 0; k < 4; k++)
+    {
+        int nx = i + dx[k];
+        int ny = j + dy[k];
+        peri += dfs(nx, ny, grid);
+    }
+    return peri;
+}
+int islandPerimeter(vector<vector<int>> &grid)
+{
+    int m = grid.size();
+    int n = grid[0].size();
+    int peri = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (grid[i][j] == 1)
+            {
+                peri += dfs(i, j, grid);
+            }
+        }
+    }
+    return peri;
+}
+*/
