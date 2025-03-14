f4abbb75e56e2e5783aa030ac8ba858b6a9b45e1
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-14 18:35:13 +0530

    Time: 22 ms (54.39%) | Memory: 23.6 MB (48.07%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,39 +1,79 @@
-class Solution {
-public:  
-    //METHOD-2
-    //USING BFS..
-    void bfs(int i,vector<vector<int>> &stones, vector<int> &vis)
+class DSU
+{
+public:
+    vector<int> parent, rank;
+    DSU(int n)
     {
-        int n= stones.size();
-        vis[i]=1;
-        queue<int>q;
-        q.push(i);
-        while(q.size())
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++)
         {
-            int x= stones[q.front()][0];
-            int y= stones[q.front()][1];
-            q.pop();
-            for(int j=0;j<n;j++)
-            {
-                if(!vis[j] && (stones[j][0]==x || stones[j][1]==y))
-                {
-                    q.push(j);
-                    vis[j]=1;
-                }
-            }
+            parent[i] = i;
         }
     }
+    int findupar(int node)
+    {
+        if (node == parent[node])
+        {
+            return node;
+        }
+        return parent[node] = findupar(parent[node]);
+    }
+    void unionbyrank(int u, int v)
+    {
+        int up_u = findupar(u);
+        int up_v = findupar(v);
+        if (up_u == up_v)
+        {
+            return;
+        }
+        if (rank[up_u] < rank[up_v])
+        {
+            parent[up_u] = up_v;
+        }
+        else if (rank[up_u] > rank[up_v])
+        {
+            parent[up_v] = up_u;
+        }
+        else
+        {
+            parent[up_v] = up_u;
+            rank[up_u]++;
+        }
+    }
+};
+
+class Solution
+{
+public:
+//METHOD-3
+//UISNG DISJOINTSET(DSU..)
     int removeStones(vector<vector<int>>& stones) 
     {
         int n= stones.size();
-        vector<int>vis(n,0);
-        int grp=0;
+        int maxrow=0;
+        int maxcol=0;
+        for(auto i:stones)
+        {
+            maxrow= max(maxrow,i[0]);
+            maxcol= max(maxcol,i[1]);
+        }
+        DSU ds(maxrow+maxcol+2);
+        unordered_set<int>st;
         for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            int u= stones[i][0];
+            int v= stones[i][1]+maxrow+1;
+            ds.unionbyrank(u,v);
+            st.insert(u);
+            st.insert(v);
+        }
+        int grp=0;
+        for(auto i:st)
+        {
+            if(ds.parent[i]==i)
             {
                 grp++;
-                bfs(i,stones,vis);
             }
         }
         return n-grp;
@@ -73,4 +113,42 @@ int removeStones(vector<vector<int>> &stones)
     return n - grp;
 }
 
+//METHOD-2
+    //USING BFS..
+    void bfs(int i,vector<vector<int>> &stones, vector<int> &vis)
+    {
+        int n= stones.size();
+        vis[i]=1;
+        queue<int>q;
+        q.push(i);
+        while(q.size())
+        {
+            int x= stones[q.front()][0];
+            int y= stones[q.front()][1];
+            q.pop();
+            for(int j=0;j<n;j++)
+            {
+                if(!vis[j] && (stones[j][0]==x || stones[j][1]==y))
+                {
+                    q.push(j);
+                    vis[j]=1;
+                }
+            }
+        }
+    }
+    int removeStones(vector<vector<int>>& stones) 
+    {
+        int n= stones.size();
+        vector<int>vis(n,0);
+        int grp=0;
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                grp++;
+                bfs(i,stones,vis);
+            }
+        }
+        return n-grp;
+    }
 */
