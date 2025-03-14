ba519cc2003e0f211cb180d11189ba101722dc85
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-14 12:16:44 +0530

    Time: 88 ms (11.66%) | Memory: 28 MB (9.49%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,16 +1,25 @@
 class Solution {
-public:
-    void dfs(int i,vector<vector<int>>& stones, vector<int>&vis)
+public:  
+    //METHOD-2
+    //USING BFS..
+    void bfs(int i,vector<vector<int>> &stones, vector<int> &vis)
     {
         int n= stones.size();
         vis[i]=1;
-        int x= stones[i][0];
-        int y= stones[i][1];
-        for(int j=0;j<n;j++)
+        queue<int>q;
+        q.push(i);
+        while(q.size())
         {
-            if(!vis[j] && (stones[j][0]==x || stones[j][1]==y))
+            int x= stones[q.front()][0];
+            int y= stones[q.front()][1];
+            q.pop();
+            for(int j=0;j<n;j++)
             {
-                dfs(j,stones,vis);
+                if(!vis[j] && (stones[j][0]==x || stones[j][1]==y))
+                {
+                    q.push(j);
+                    vis[j]=1;
+                }
             }
         }
     }
@@ -24,9 +33,44 @@ public:
             if(!vis[i])
             {
                 grp++;
-                dfs(i,stones,vis);
+                bfs(i,stones,vis);
             }
         }
         return n-grp;
     }
 };
+/*
+// METHOD-1
+// DFS...
+void dfs(int i, vector<vector<int>> &stones, vector<int> &vis)
+{
+    int n = stones.size();
+    vis[i] = 1;
+    int x = stones[i][0];
+    int y = stones[i][1];
+    for (int j = 0; j < n; j++)
+    {
+        // present in same row or same col then mark it as visted..
+        if (!vis[j] && (stones[j][0] == x || stones[j][1] == y))
+        {
+            dfs(j, stones, vis);
+        }
+    }
+}
+int removeStones(vector<vector<int>> &stones)
+{
+    int n = stones.size();
+    vector<int> vis(n, 0);
+    int grp = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            grp++;
+            dfs(i, stones, vis);
+        }
+    }
+    return n - grp;
+}
+
+*/
