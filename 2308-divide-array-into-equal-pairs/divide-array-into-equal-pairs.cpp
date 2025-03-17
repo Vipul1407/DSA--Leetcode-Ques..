5f0c529fcac929231e9dc856ede02528dd2af2e9
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-03-17 15:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (66.52%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    //METHOD-2
     bool divideArray(vector<int>& nums) 
     {
         int n= nums.size();
         vector<int>arr(501,0);
-        int cnt=0;
+
         for(auto i:nums)
         {
             arr[i]++;
@@ -15,11 +16,36 @@ public:
             {
                 continue;
             }
-            if(arr[i]%2==0)
+            if(arr[i]%2!=0)
             {
-                cnt+= arr[i]/2;
+                return false;
             }
         }
-        return cnt==n/2;
+        return true;
     }
 };
+/*
+//METHOD-1
+bool divideArray(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> arr(501, 0);
+    int cnt = 0;
+    for (auto i : nums)
+    {
+        arr[i]++;
+    }
+    for (int i = 0; i <= 500; i++)
+    {
+        if (arr[i] == 0)
+        {
+            continue;
+        }
+        if (arr[i] % 2 == 0)
+        {
+            cnt += arr[i] / 2;
+        }
+    }
+    return cnt == n / 2;
+}
+*/
