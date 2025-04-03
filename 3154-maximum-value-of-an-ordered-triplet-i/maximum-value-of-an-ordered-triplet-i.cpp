21e91b1051bcc3e7c7122b5e7beead402ebe9678
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-03 17:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (67.16%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    //METHOD-3
-    //BETTER APPROACH USING 1 VECTOR ONLY..
+    //METHOD-4
+    //OPTIMIZED APPROACH USING NO VECTOR..
     long long maximumTripletValue(vector<int>& nums) 
     {
         int n= nums.size();
         long long ans=0;
-        vector<int>rightmax(n,0);  
-        for(int i=n-2;i>=0;i--)
+        long long maxdiff=0;
+        long long maxi=0;
+        for(int i=0;i<n;i++)
         {
-            rightmax[i]= max(rightmax[i+1],nums[i+1]);
-        }
-        int leftmax=0;
-        for(int i=1;i<n-1;i++)
-        {
-            leftmax= max(leftmax,nums[i-1]);  
-            ans= max(ans,(long long)(leftmax-nums[i])*rightmax[i]);
+            ans= max(ans,maxdiff*nums[i]);
+            maxdiff= max(maxdiff, maxi-nums[i]);
+            maxi= max(maxi,(long long)nums[i]);
         }
         return ans;
     }
 };
 /*
-//METHOD-1
+    //METHOD-1
     //BRUTE FORCE..
     long long maximumTripletValue(vector<int>& nums) 
     {
@@ -60,6 +57,26 @@ public:
         for(int i=1;i<n-1;i++)
         {
             ans= max(ans,(long long)(leftmax[i]-nums[i])*rightmax[i]);
+        }
+        return ans;
+    }
+
+    //METHOD-3
+    //BETTER APPROACH USING 1 VECTOR ONLY..
+    long long maximumTripletValue(vector<int>& nums) 
+    {
+        int n= nums.size();
+        long long ans=0;
+        vector<int>rightmax(n,0);  
+        for(int i=n-2;i>=0;i--)
+        {
+            rightmax[i]= max(rightmax[i+1],nums[i+1]);
+        }
+        int leftmax=0;
+        for(int i=1;i<n-1;i++)
+        {
+            leftmax= max(leftmax,nums[i-1]);  
+            ans= max(ans,(long long)(leftmax-nums[i])*rightmax[i]);
         }
         return ans;
     }
