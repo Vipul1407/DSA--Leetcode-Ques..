827c9cb45d7e5e0b230428992d5c8efb3db9f38d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-03 18:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 91 MB (85.24%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -1,22 +1,64 @@
 class Solution {
 public:
-    // METHOD-2
-    // BETTER APPROACH USING 1 VECTOR ONLY..
+    // METHOD-3
+    // OPTIMIZED APPROACH USING NO VECTOR..
     long long maximumTripletValue(vector<int> &nums)
     {
         int n = nums.size();
         long long ans = 0;
-        vector<int> rightmax(n, 0);
-        for (int i = n - 2; i >= 0; i--)
+        long long maxdiff = 0;
+        long long maxi = 0;
+        for (int i = 0; i < n; i++)
         {
-            rightmax[i] = max(rightmax[i + 1], nums[i + 1]);
-        }
-        int leftmax = 0;
-        for (int i = 1; i < n - 1; i++)
-        {
-            leftmax = max(leftmax, nums[i - 1]);
-            ans = max(ans, (long long)(leftmax - nums[i]) * rightmax[i]);
+            ans = max(ans, maxdiff * nums[i]);
+            maxdiff = max(maxdiff, maxi - nums[i]);
+            maxi = max(maxi, (long long)nums[i]);
         }
         return ans;
     }
 };
+/*
+// METHOD-1
+// BETTER APPROACH USING 2 VECTORS PREFIXMAX, SUFFIXAMX..
+long long maximumTripletValue(vector<int> &nums)
+{
+    int n = nums.size();
+    long long ans = 0;
+    vector<int> leftmax(n, 0);
+    vector<int> rightmax(n, 0);
+
+    for (int i = 1; i < n; i++)
+    {
+        leftmax[i] = max(leftmax[i - 1], nums[i - 1]);
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightmax[i] = max(rightmax[i + 1], nums[i + 1]);
+    }
+    for (int i = 1; i < n - 1; i++)
+    {
+        ans = max(ans, (long long)(leftmax[i] - nums[i]) * rightmax[i]);
+    }
+    return ans;
+}
+
+// METHOD-2
+// BETTER APPROACH USING 1 VECTOR ONLY..
+long long maximumTripletValue(vector<int> &nums)
+{
+    int n = nums.size();
+    long long ans = 0;
+    vector<int> rightmax(n, 0);
+    for (int i = n - 2; i >= 0; i--)
+    {
+        rightmax[i] = max(rightmax[i + 1], nums[i + 1]);
+    }
+    int leftmax = 0;
+    for (int i = 1; i < n - 1; i++)
+    {
+        leftmax = max(leftmax, nums[i - 1]);
+        ans = max(ans, (long long)(leftmax - nums[i]) * rightmax[i]);
+    }
+    return ans;
+}
+*/
