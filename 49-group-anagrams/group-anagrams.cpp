a8812b726336f4e3a13445d44e18a62fecaf0aab
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-03 14:34:49 +0530

    Time: 15 ms (72.82%) | Memory: 29.6 MB (18.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,8 +1,42 @@
 class Solution {
 public:
+
+    //METHOD-2
+    //WITHOUT USING SORTING
+    //TC= O(N*(K+26)) where K is the length of largest word..
+    vector<vector<string>> groupAnagrams(vector<string>& arr) 
+    {
+        int n= arr.size();
+        unordered_map<string,vector<string>>mp;
+        for(auto i:arr)
+        {
+            vector<int>cnt(26,0);
+            for(auto j:i)
+            {
+                cnt[j-'a']++;
+            }
+            string temp="";
+            for(int i=0;i<26;i++)
+            {
+                while(cnt[i]--)
+                {
+                    temp+= i+'a';
+                }
+            }
+            mp[temp].push_back(i);
+        }
+        vector<vector<string>>ans;
+        for(auto i:mp)
+        {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //USING SORTING
-    //TC= O(K*NLOGN)
+    //TC= O(N*KLOGK) where K is the length of largest word..
     vector<vector<string>> groupAnagrams(vector<string>& arr) 
     {
         int n= arr.size();
@@ -20,4 +54,4 @@ public:
         }
         return ans;
     }
-};
+*/
