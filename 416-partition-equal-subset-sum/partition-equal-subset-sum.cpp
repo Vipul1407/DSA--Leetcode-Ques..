358f1ee91029e056dd1f4bf568c8a378aab4468e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-07 17:01:00 +0530

    Time: 103 ms (68.84%) | Memory: 13.4 MB (90.95%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    // METHOD-3
-    // BUA
+    // METHOD-4
+    // SPACE OPTIMIZED
     bool canPartition(vector<int>& nums) 
     {
         int n= nums.size();
@@ -10,23 +10,25 @@ public:
         {
             return false;
         }
-        vector<vector<bool>>dp(n+1,vector<bool>((sum/2)+1,false));
+        int tar= sum/2;
+        vector<bool>curr(tar+1,false);
+        vector<bool>next(tar+1,false);
         //base case: tar==0 return true;
         for(int i=0;i<=n;i++)
         {
-            dp[i][0]= true;
+            next[0]= true;
         }
-        int tar= sum/2;
         for(int i=n-1;i>=0;i--)
         {
             for(int j=0;j<=tar;j++)
             {
-                bool take= j-nums[i]>=0? dp[i+1][j-nums[i]]:false;
-                bool notake= dp[i+1][j];
-                dp[i][j]= take || notake;
+                bool take= j-nums[i]>=0? next[j-nums[i]]:false;
+                bool notake= next[j];
+                curr[j]= take || notake;
             }
+            next=curr;
         }
-        return dp[0][tar];
+        return curr[tar];
     }
 };
 /*
@@ -91,6 +93,35 @@ bool canPartition(vector<int> &nums)
     }
     vector<vector<int>> dp(n, vector<int>((sum / 2) + 1, -1));
     return solve(0, nums, sum / 2, dp) ? true : false;
+}
+
+// METHOD-3
+// BUA
+bool canPartition(vector<int> &nums)
+{
+    int n = nums.size();
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if (sum % 2 != 0)
+    {
+        return false;
+    }
+    vector<vector<bool>> dp(n + 1, vector<bool>((sum / 2) + 1, false));
+    // base case: tar==0 return true;
+    for (int i = 0; i <= n; i++)
+    {
+        dp[i][0] = true;
+    }
+    int tar = sum / 2;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= tar; j++)
+        {
+            bool take = j - nums[i] >= 0 ? dp[i + 1][j - nums[i]] : false;
+            bool notake = dp[i + 1][j];
+            dp[i][j] = take || notake;
+        }
+    }
+    return dp[0][tar];
 }
 
 */
