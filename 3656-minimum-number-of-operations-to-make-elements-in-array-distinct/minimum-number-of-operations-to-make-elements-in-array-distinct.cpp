779cba80b28c2d070cbf021dcf9290781bbae042
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-08 17:38:17 +0530

    Time: 4 ms (45.39%) | Memory: 27.6 MB (77.30%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
-    bool isunique(vector<int>& nums, int i)
-    {
-        int n= nums.size();
-        unordered_set<int>st;
-        for(int j=i;j<n;j++)
-        {
-            if(st.count(nums[j]))
-            {
-                return false;
-            }
-            st.insert(nums[j]);
-        }
-        return true;
-    }
+    //METHOD-2
+    //OPTIMIZED..
     int minimumOperations(vector<int>& nums) 
     {
         int n= nums.size();
-        int cnt=0;
-        
-        for(int i=0;i<n;i+=3)
+        unordered_set<int>st;
+        for(int i=n-1;i>=0;i--)
         {
-            if(isunique(nums,i))
+            if(st.find(nums[i])!=st.end())
             {
-                return cnt;
+                //we have to remove all elements till i=0 
+                return (i/3)+1;
             }
-            cnt++;
+            st.insert(nums[i]);
         }
-        return cnt;
+        return 0;
     }
 };
+/*
+// METHOD-1
+// BRUTE FORCE..
+bool isunique(vector<int> &nums, int i)
+{
+    int n = nums.size();
+    unordered_set<int> st;
+    for (int j = i; j < n; j++)
+    {
+        if (st.count(nums[j]))
+        {
+            return false;
+        }
+        st.insert(nums[j]);
+    }
+    return true;
+}
+int minimumOperations(vector<int> &nums)
+{
+    int n = nums.size();
+    int cnt = 0;
+
+    for (int i = 0; i < n; i += 3)
+    {
+        if (isunique(nums, i))
+        {
+            return cnt;
+        }
+        cnt++;
+    }
+    return cnt;
+}
+*/
