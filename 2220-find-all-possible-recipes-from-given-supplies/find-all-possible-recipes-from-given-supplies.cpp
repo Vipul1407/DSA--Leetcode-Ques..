5c17e7f12a798c7197d0aa98986bd6ac790e4b71
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-09 18:15:59 +0530

    Time: 40 ms (83.53%) | Memory: 101.2 MB (70.46%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,5 +1,57 @@
 class Solution {
 public:
+    //METHOD-2
+    //OPTIMIZED TOPOLOGICAL SORTING..
+    vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ing, vector<string>& supp) 
+    {
+        int n= rec.size();
+        unordered_set<string>st;
+        queue<int>q;
+        vector<int>indegree(n,0);
+        for(auto i:supp)
+        {
+            st.insert(i);
+        }
+        unordered_map<string,vector<int>>adj;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<ing[i].size();j++)
+            {
+                //agar set me ni mila to..
+                if(st.find(ing[i][j])==st.end())
+                {
+                    adj[ing[i][j]].push_back(i);
+                    indegree[i]++;
+                }
+            }
+        }
+        //apply khan Algo (BFS- Toposort..)
+        for(int i=0;i<indegree.size();i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        vector<string>ans;
+        while(q.size())
+        {
+            int i= q.front();
+            q.pop();
+            ans.push_back(rec[i]);
+
+            for(int &idx: adj[rec[i]])
+            {
+                if(--indegree[idx]==0)
+                {
+                    q.push(idx);
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //BRUTE FORCE..
     vector<string> findAllRecipes(vector<string>& rec, vector<vector<string>>& ing, vector<string>& supp) 
@@ -40,4 +92,4 @@ public:
         }
         return ans;
     }
-};
+*/
