1c05e87461916fed7f1c49e04edc7e2d0a08fde6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-11 22:12:01 +0530

    Time: 21 ms (6.25%) | Memory: 127.7 MB (12.52%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,19 +1,44 @@
 class Solution {
-private:
-    long rec(int i, vector<vector<int>>& questions, vector<long>& dp, int n) {
-        if (i >= n) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        long take = questions[i][0] + rec(i + questions[i][1] + 1, questions, dp, n);
-        long dont = rec(i + 1, questions, dp, n);
-
-        return dp[i] = max(take, dont);
-    }
-
 public:
-    long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
-        vector<long> dp(n, -1);
-        return rec(0, questions, dp, n);
+    //METHOD-2
+    //TDA APPROACH...
+    long long tda(int i,vector<vector<int>>& ques, vector<long>&dp)
+    {
+        if(i>=ques.size())
+        {
+            return 0;
+        }
+        if(dp[i]!=-1)
+        {
+            return dp[i];
+        }
+        long long take= ques[i][0]+ tda(i+ques[i][1]+1,ques,dp);
+        long long notake= tda(i+1,ques,dp);
+        return dp[i]= max(take,notake);
+    }
+    long long mostPoints(vector<vector<int>>& ques) 
+    {
+        int n=ques.size();
+        vector<long>dp(n,-1);
+        return tda(0,ques,dp);
     }
 };
+/*
+    //METHOD-1
+    //RECURSIVE APPROACH...
+    long long rec(int i,vector<vector<int>>& ques)
+    {
+        if(i>=ques.size())
+        {
+            return 0;
+        }
+        long long take= ques[i][0]+ rec(i+ques[i][1]+1,ques);
+        long long notake= rec(i+1,ques);
+        return max(take,notake);
+    }
+    long long mostPoints(vector<vector<int>>& ques) 
+    {
+        int n=ques.size();
+        return rec(0,ques);
+    }
+*/
