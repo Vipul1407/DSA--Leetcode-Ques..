b2e1cb7c2d4d76628d8a61217b34c1b923ce7c54
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-11 15:01:59 +0530

    Time: 75 ms (38.62%) | Memory: 9.8 MB (56.70%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/count-symmetric-integers.cpp
@@ -1,33 +1,6 @@
 class Solution {
 public:
-    //METHOD-2
-    //OPTIMIZED..
-    //TC= O(high*d)
-    int countSymmetricIntegers(int low, int high) 
-    {
-        int cnt=0;
-        for(int i=low;i<=high;i++)
-        {
-            if(i>=11 && i<=99 && i%11==0)
-            {
-                cnt++;
-            } 
-            else if(i>=1000 && i<=9999)  
-            {
-                int first= i/1000;
-                int second= (i/100)%10;
-                int third= (i/10)%10;
-                int fourth= i%10;
-                if(first+second == third+fourth)
-                {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-};
-/*
+
     //METHOD-1
     //BRUTE FORCE..
     //TC= O(high*d)
@@ -58,4 +31,33 @@ public:
         }
         return cnt;
     }
+};
+/*
+    //METHOD-2
+    //OPTIMIZED..
+    //TC= O(high)
+    int countSymmetricIntegers(int low, int high) 
+    {
+        int cnt=0;
+        for(int i=low;i<=high;i++)
+        {
+            if(i>=11 && i<=99 && i%11==0)
+            {
+                cnt++;
+            } 
+            else if(i>=1000 && i<=9999)  
+            {
+                int first= i/1000;
+                int second= (i/100)%10;
+                int third= (i/10)%10;
+                int fourth= i%10;
+                if(first+second == third+fourth)
+                {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
 */
+    
