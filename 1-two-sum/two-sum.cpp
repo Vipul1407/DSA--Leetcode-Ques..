0aa61c4b27b166090ce35eb1cd5db5c3239d57f7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-13 12:04:43 +0530

    Time: 3 ms (72.91%) | Memory: 15.9 MB (9.26%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
     //METHOD-3
+    //OPTIMZED..
+    //TC= O(N)
+    vector<int> twoSum(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        vector<int>ans;
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(k-nums[i])!=mp.end())
+            {
+                ans.push_back(i);
+                ans.push_back(mp[k-nums[i]]);
+            }
+            mp[nums[i]]=i;
+        }    
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //BRUTE FORCE..
+    //TC=O(N^2)
+    vector<int> twoSum(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]+nums[j]==k)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }    
+        return ans;
+    }
+
+    //METHOD-2
     //BETTER.. 
     //TC= O(NLOGN)
     vector<int> twoSum(vector<int>& nums, int k) 
@@ -34,46 +76,5 @@ public:
         }    
         return ans;
     }
-};
-/*
-    //METHOD-1
-    //BRUTE FORCE..
-    //TC=O(N^2)
-    vector<int> twoSum(vector<int>& nums, int k) 
-    {
-        int n= nums.size();
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(nums[i]+nums[j]==k)
-                {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
-            }
-        }    
-        return ans;
-    }
-
-    //METHOD-2
-    //OPTIMZED..
-    //TC= O(N)
-    vector<int> twoSum(vector<int>& nums, int k) 
-    {
-        int n= nums.size();
-        vector<int>ans;
-        unordered_map<int,int>mp;
-        for(int i=0;i<n;i++)
-        {
-            if(mp.find(k-nums[i])!=mp.end())
-            {
-                ans.push_back(i);
-                ans.push_back(mp[k-nums[i]]);
-            }
-            mp[nums[i]]=i;
-        }    
-        return ans;
-    }
+    
 */
