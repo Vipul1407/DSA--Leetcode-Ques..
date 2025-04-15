72104d5abd129cc534a6017a5bb94f8108b02da9
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-15 18:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (16.29%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    //OPTIMIZED APPROACH..
     //KADANE ALGO..
+    //OPTIMIZED..
     int maxAbsoluteSum(vector<int>& nums) 
     {
+        int positive_sum=0;
+        int negative_sum=0;
         int ans= INT_MIN;
-        int positive=0;
-        int negative=0;
-
         for(auto i:nums)
         {
-            positive+= i;
-            negative+= i;
-            ans= max(ans,max(abs(positive),abs(negative)));
-            if(positive<0)
+            positive_sum+= i;
+            negative_sum+= i;
+            ans= max(ans,max(abs(positive_sum),abs(negative_sum)));
+            if(positive_sum<0)
             {
-                positive=0;
+                positive_sum=0;
             }
-            if(negative>0)
+            if(negative_sum>0)
             {
-                negative=0;
+                negative_sum=0;
             }
         }
         return ans;
     }
-};  
+};
