39570733f78328f975cb9116d93a4cb43360fe8c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-20 00:49:31 +0530

    Time: 101 ms (17.78%) | Memory: 47.9 MB (33.61%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    //METHOD-1
+    //USING PRIORITY_QUEUE
+    //TC= O(NLOGN)
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) 
+    {
+        unordered_map<int,vector<pair<int,int>>>adj;
+        vector<int>ans(n+1,INT_MAX);
+        int size= times.size();
+        for(auto i:times)
+        {
+            int u= i[0];
+            int v= i[1];
+            int w= i[2];
+            adj[u].push_back({v,w});
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        ans[k]=0;
+        while(pq.size())
+        {
+            int w= pq.top().first;
+            int neigh= pq.top().second;
+            pq.pop();
+            for(auto i:adj[neigh])
+            {
+                int d= i.second;
+                int node= i.first;
+                if(d+w< ans[node])
+                {
+                    ans[node]= d+w;
+                    pq.push({ans[node],node});
+                }
+            }
+        }
+        int maxi= INT_MIN;
+        for(int i=1;i<=n;i++)
+        {
+            if(ans[i]==INT_MAX)
+            {
+                return -1;
+            }
+            maxi= max(maxi,ans[i]);
+        }
+        return maxi;
+    }
+};
