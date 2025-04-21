98813ef502e8ac033f08e845d1e337acc7e2f860
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-21 15:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (45.90%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    typedef pair<int,pair<int,int>> P;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        //src, dest, wt...
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto i:flights)
+        {
+            adj[i[0]].push_back({i[1],i[2]});
+        }
+        vector<int>dis(n,1e9);
+        //min heap.. stops,src,wt.
+        priority_queue<P,vector<P>,greater<P>>pq;
+        pq.push({0,{src,0}});
+        dis[src]=0;
+        while(pq.size())
+        {
+            auto top= pq.top();
+            int stops= top.first;
+            int node= top.second.first;
+            int d= top.second.second;
+            pq.pop();
+            if(stops>k)
+            {
+                continue;
+            }
+            for(auto i:adj[node])
+            {
+                int neigh= i.first;
+                int w= i.second;
+                if(d+w< dis[neigh])
+                {
+                    dis[neigh]= d+w;
+                    pq.push({stops+1,{neigh,dis[neigh]}});
+                }
+            }
+        }
+        return dis[dst]==1e9? -1:dis[dst];
+    }
+};
