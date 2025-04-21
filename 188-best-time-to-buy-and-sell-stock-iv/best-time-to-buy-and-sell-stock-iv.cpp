31b1bf3a1b12709dce26571251cc85b42de5be0e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-21 17:04:08 +0530

    Time: 10 ms (48.19%) | Memory: 18.3 MB (33.97%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-
-    // METHOD-2
-    // TDA..
-    int tda(int i, vector<int> &arr, int canbuy, int limit, vector<vector<vector<int>>> &dp)
-    {
-        if (i >= arr.size() || limit == 0)
-        {
-            return 0;
-        }
-        if (dp[i][canbuy][limit] != -1)
-        {
-            return dp[i][canbuy][limit];
-        }
-        int profit = 0;
-        if (canbuy)
-        {
-            int buy = -arr[i] + tda(i + 1, arr, 0, limit, dp);
-            int skip = tda(i + 1, arr, 1, limit, dp);
-            profit = max(buy, skip);
-        }
-        else
-        {
-            int sell = arr[i] + tda(i + 1, arr, 1, limit - 1, dp);
-            int skip = tda(i + 1, arr, 0, limit, dp);
-            profit = max(sell, skip);
-        }
-        return dp[i][canbuy][limit] = profit;
-    }
+    // METHOD-3
+    // BUA..
     int maxProfit(int k, vector<int> &arr)
     {
         int n = arr.size();
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        // i=0 ,canbuy=1 and limit=k initially..
-        return tda(0, arr, 1, k, dp);
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k+1, 0)));
+        // i=0 ,canbuy=1 and limit=2 initially..
+        int profit = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int canbuy = 0; canbuy <= 1; canbuy++)
+            {
+                for (int limit = 1; limit <= k; limit++)
+                {
+                    if (canbuy)
+                    {
+                        int buy = -arr[i] + dp[i + 1][0][limit];
+                        int skip = dp[i + 1][1][limit];
+                        profit = max(buy, skip);
+                    }
+                    else
+                    {
+                        int sell = arr[i] + dp[i + 1][1][limit - 1];
+                        int skip = dp[i + 1][0][limit];
+                        profit = max(sell, skip);
+                    }
+                    dp[i][canbuy][limit] = profit;
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
 /*
@@ -60,11 +57,11 @@ int rec(int i, vector<int> &arr, int canbuy, int limit)
     }
     return profit;
 }
-int maxProfit(int limit, vector<int> &arr)
+int maxProfit(int k, vector<int> &arr)
 {
     int n = arr.size();
-    // i=0 ,canbuy=1 and limit=2 initially..
-    return rec(0, arr, 1, 2);
+    // i=0 ,canbuy=1 and limit=k initially..
+    return rec(0, arr, 1, k);
 }
 
 // METHOD-2
