0f37d0f8fc9426ae9f9960663441f08ca71ac546
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-21 15:46:11 +0530

    Time: 4 ms (47.81%) | Memory: 18.3 MB (45.90%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,5 +1,50 @@
 class Solution {
 public:
+    //METHOD-2
+    //UISNG SIMPLE QUEUE...
+    typedef pair<int,pair<int,int>> P;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        //src, dest, wt...
+        unordered_map<int,vector<pair<int,int>>>adj;
+        for(auto i:flights)
+        {
+            adj[i[0]].push_back({i[1],i[2]});
+        }
+        vector<int>dis(n,1e9);
+        //min heap.. stops,src,wt.
+        queue<P>pq;
+        pq.push({0,{src,0}});
+        dis[src]=0;
+        while(pq.size())
+        {
+            auto top= pq.front();
+            int stops= top.first;
+            int node= top.second.first;
+            int d= top.second.second;
+            pq.pop();
+            if(stops>k)
+            {
+                continue;
+            }
+            for(auto i:adj[node])
+            {
+                int neigh= i.first;
+                int w= i.second;
+                if(d+w< dis[neigh])
+                {
+                    dis[neigh]= d+w;
+                    pq.push({stops+1,{neigh,dis[neigh]}});
+                }
+            }
+        }
+        return dis[dst]==1e9? -1:dis[dst];
+    }
+};
+/*
+    //METHOD-1
+    //USING PRIORITY QUEUE...
+
     typedef pair<int,pair<int,int>> P;
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
     {
@@ -38,4 +83,4 @@ public:
         }
         return dis[dst]==1e9? -1:dis[dst];
     }
-};
+*/
