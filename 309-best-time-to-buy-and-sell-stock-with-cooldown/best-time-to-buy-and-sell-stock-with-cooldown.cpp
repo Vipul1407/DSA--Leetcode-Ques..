5211fbc1858cfd1d48e242d4c2b67c1d4ea5bc8d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-22 16:14:10 +0530

    Time: 4 ms (21.29%) | Memory: 16.1 MB (49.46%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
 
-    // METHOD-2
-    // TDA..
-    int tda(int i, int canbuy, vector<int> &nums, vector<vector<int>> &dp)
-    {
-        if (i >= nums.size())
-        {
-            return 0;
-        }
-        if (dp[i][canbuy] != -1)
-        {
-            return dp[i][canbuy];
-        }
-        int profit = 0;
-        if (canbuy)
-        {
-            int buy = -nums[i] + tda(i + 1, 0, nums, dp);
-            int skip = tda(i + 1, 1, nums, dp);
-            profit = max(buy, skip);
-        }
-        else
-        {
-            //add 1 day cooldown period..
-            int sell = nums[i] + tda(i + 2, 1, nums, dp);
-            int skip = tda(i + 1, 0, nums, dp);
-            profit = max(sell, skip);
-        }
-        return dp[i][canbuy] = profit;
-    }
+    // METHOD-3
+    // BUA..
     int maxProfit(vector<int> &nums)
     {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(2, -1));
-        return tda(0, 1, nums, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int profit = 0;
+            for (int canbuy = 0; canbuy <= 1; canbuy++)
+            {
+                if (canbuy == 1)
+                {
+                    int buy = -nums[i] + dp[i + 1][0];
+                    int skip = dp[i + 1][1];
+                    profit = max(buy, skip);
+                }
+                else
+                {
+                    //add 1 day cooldown period..
+                    int sell = nums[i] + dp[i + 2][1];
+                    int skip = dp[i + 1][0];
+                    profit = max(sell, skip);
+                }
+                dp[i][canbuy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
 /*
@@ -66,5 +61,40 @@ public:
     {
         int n = nums.size();
         return rec(0, 1, nums);
+    }
+
+        // METHOD-2
+    // TDA..
+    int tda(int i, int canbuy, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if (i >= nums.size())
+        {
+            return 0;
+        }
+        if (dp[i][canbuy] != -1)
+        {
+            return dp[i][canbuy];
+        }
+        int profit = 0;
+        if (canbuy)
+        {
+            int buy = -nums[i] + tda(i + 1, 0, nums, dp);
+            int skip = tda(i + 1, 1, nums, dp);
+            profit = max(buy, skip);
+        }
+        else
+        {
+            //add 1 day cooldown period..
+            int sell = nums[i] + tda(i + 2, 1, nums, dp);
+            int skip = tda(i + 1, 0, nums, dp);
+            profit = max(sell, skip);
+        }
+        return dp[i][canbuy] = profit;
+    }
+    int maxProfit(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2, -1));
+        return tda(0, 1, nums, dp);
     }
 */
