66761f869092592649b07b577fc4c3f7a59f47ac
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-04-23 16:38:59 +0530

    Time: 2 ms (49.15%) | Memory: 8 MB (74.92%) - LeetSync

--- 1500-count-largest-group/count-largest-group.cpp
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    //METHOD-2
+    int countLargestGroup(int n) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=1;i<=n;i++)
+        {
+            int d=i;
+            int sum=0;
+            while(d>0)
+            {
+                sum+= d%10;
+                d/=10;
+            }
+            mp[sum]++;
+        }
+        int maxi=0;
+        int ans=0;
+        for(auto i:mp)
+        {
+            if(i.second>maxi)
+            {
+                maxi= i.second;
+                ans=1;
+            }
+            else if(i.second==maxi)
+            {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
     int countLargestGroup(int n) 
     {
         unordered_map<int,int>mp;
@@ -29,4 +63,4 @@ public:
         }
         return ans;
     }
-};
+*/
