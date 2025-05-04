67f8f22933a86de732ae951fbdf060b6f7f10717
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-05-04 16:23:18 +0530

    Time: 19 ms (9.50%) | Memory: 29.8 MB (8.75%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,7 +1,71 @@
 class Solution {
 public:
+    //METHOD-3
+    //Using Unordered Map..
+    //TC= O(N)
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        int n= dominoes.size();
+        int ans=0;
+        unordered_map<string,int>mp;
+        for(auto i:dominoes)
+        {
+            if(i[1]<i[0])
+            {
+                string key= to_string(i[1]);
+                key+='_';
+                key+= to_string(i[0]);
+                mp[key]++;
+            }
+            else
+            {
+                string key= to_string(i[0]);
+                key+='_';
+                key+= to_string(i[1]);
+                mp[key]++;
+            }
+        }
+        for(auto i:mp)
+        {
+            int cnt=i.second;
+            //find nc2
+            ans+= cnt*(cnt-1)/2;
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //Using Map..
+    //TC= O(NLOGN)
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        int n= dominoes.size();
+        int ans=0;
+        map<pair<int,int>,int>mp;
+        for(auto i:dominoes)
+        {
+            if(i[1]<i[0])
+            {
+                mp[{i[1],i[0]}]++;
+            }
+            else
+            {
+                mp[{i[0],i[1]}]++;
+            }
+        }
+        for(auto i:mp)
+        {
+            int cnt= i.second;
+            //find nc2
+            ans+= cnt*(cnt-1)/2;
+        }
+        return ans;
+    }
+
     //METHOD-2
     //Using Const space of vector of size=99
+    //TC= O(N)
     int numEquivDominoPairs(vector<vector<int>>& dominoes) 
     {
         int n= dominoes.size();
@@ -25,34 +89,6 @@ public:
         {
             //find nc2
             ans+= i*(i-1)/2;
-        }
-        return ans;
-    }
-};
-/*
-    //METHOD-1
-    //Using Unordered Map..
-    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
-    {
-        int n= dominoes.size();
-        int ans=0;
-        map<pair<int,int>,int>mp;
-        for(auto i:dominoes)
-        {
-            if(i[1]<i[0])
-            {
-                mp[{i[1],i[0]}]++;
-            }
-            else
-            {
-                mp[{i[0],i[1]}]++;
-            }
-        }
-        for(auto i:mp)
-        {
-            int cnt= i.second;
-            //find nc2
-            ans+= cnt*(cnt-1)/2;
         }
         return ans;
     }
