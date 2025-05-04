2bfb655d835a9e33a0a84e5a23c32d9268419218
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-05-04 11:36:16 +0530

    Time: 7 ms (41.75%) | Memory: 28.5 MB (12.25%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
+    //METHOD-2
+    //Using Const space of vector of size=99
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) 
+    {
+        int n= dominoes.size();
+        int ans=0;
+        //maxsize can go to 9*10+9= 99
+        vector<int>vec(100,0);
+        for(auto i:dominoes)
+        {
+            if(i[1]<i[0])
+            {
+                int key= i[1]*10+i[0];
+                vec[key]++;
+            }
+            else
+            {
+                int key= i[0]*10+i[1];
+                vec[key]++;
+            }
+        }
+        for(auto i:vec)
+        {
+            //find nc2
+            ans+= i*(i-1)/2;
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
+    //Using Unordered Map..
     int numEquivDominoPairs(vector<vector<int>>& dominoes) 
     {
         int n= dominoes.size();
@@ -25,4 +56,4 @@ public:
         }
         return ans;
     }
-};
+*/
