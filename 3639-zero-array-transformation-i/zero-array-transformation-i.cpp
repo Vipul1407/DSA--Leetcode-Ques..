672f71ab9821e7d9a0e388fbb2985cc0bb7e9289
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-05-20 08:32:02 +0530

    Time: 134 ms (31.10%) | Memory: 325.5 MB (22.43%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    //METHOD-2
+    //DIFFERENCE ARRAY TECHNIQUE OPTIMIZED...
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& quer) 
+    {
+        int n= nums.size();
+        vector<int>pref(n,0);
+        for(auto i:quer)
+        {
+            int s= i[0];
+            int e= i[1];
+            pref[s]++;
+            if(e+1<n)
+            {
+                pref[e+1]--;
+            }
+        }
+        int prefsum=0;
+        for(int i=0;i<n;i++)
+        {
+            prefsum+= pref[i];
+            if(nums[i]-prefsum>0)
+            {
+                return false;
+            }
+        }
+        //if all elements after nums[i]-prefsum <=0 then retun true...
+        return true;
+    }
+};
+/*
+//METHOD-1
+//DIFFERENCE ARRAY TECHNIQUE...
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& quer) 
     {
         int n= nums.size();
@@ -28,4 +60,4 @@ public:
         //if all elements after nums[i]-pref[i] <=0 then retun true...
         return true;
     }
-};
+*/
