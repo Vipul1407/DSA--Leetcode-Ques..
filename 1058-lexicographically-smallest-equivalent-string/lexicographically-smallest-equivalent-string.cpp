f76cdd5cead24078b23a2bb0a34f8beffe424ee6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-07 15:52:16 +0530

    Time: 15 ms (17.73%) | Memory: 11.8 MB (6.68%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -1,7 +1,42 @@
 class Solution {
 public:
+    //METHOD-2
+    //DFS..   return typ is char.
+    //TC= O(M* (E+V))
+    char dfs(char ch, unordered_map<int,vector<int>>&adj, vector<int>&vis)
+    {
+        vis[ch-'a']=1;//mark as visted;
+        char mini= ch;
+        for(auto i:adj[ch])
+        {
+            if(vis[i-'a']==0)
+            {
+                mini= min(mini, dfs(i,adj,vis));
+            }
+        }
+        return mini;
+    }
+    string smallestEquivalentString(string s1, string s2, string base) 
+    {
+        unordered_map<int,vector<int>>adj;
+        int n= s1.size();
+        for(int i=0;i<n;i++)
+        {
+            adj[s1[i]].push_back(s2[i]);
+            adj[s2[i]].push_back(s1[i]);
+        }
+        string ans="";
+        for(auto i:base)
+        {
+            vector<int>vis(26,0);
+            ans+= dfs(i,adj,vis);
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
-    //DFS..
+    //DFS..   return type is void.
     //TC= O(M* (E+V))
     void dfs(char ch, unordered_map<int,vector<int>>&adj, char &mini, vector<int>&vis)
     {
@@ -38,4 +73,4 @@ public:
         }
         return ans;
     }
-};
+*/
