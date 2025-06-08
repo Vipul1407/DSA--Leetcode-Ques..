4b0921bf30ba0a26b66b79cb76387dc3a4d5b1da
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-08 16:47:10 +0530

    Time: 145 ms (45.51%) | Memory: 26.2 MB (63.76%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
+    //METHOD-1
+    //Prioirty_queue
     string clearStars(string s) 
     {
-        int n=s.length();
-        //give in ascending order of char,then if same then it give min index but we need max index so we will push it by -i
-        priority_queue<pair<char,int>,vector<pair<char,int>>,greater<pair<char,int>>>pq;
+        int n= s.size();
+        //we want small element with greater index, so pushing index with -ve sign..
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
         for(int i=0;i<n;i++)
         {
             if(s[i]=='*')
             {
-                if(!pq.empty())
+                if(pq.size())
+                {
+                    auto top= pq.top();
+                    s[-top.second]= '*';
                     pq.pop();
+                }
             }
             else
             {
                 pq.push({s[i],-i});
             }
         }
-        unordered_set<int>st;
-        while(!pq.empty())
-        {
-            st.insert(-pq.top().second);
-            pq.pop();
-        }
         string ans="";
-        for(int i=0;i<n;i++)
+        for(auto i:s)
         {
-            if(st.find(i)!=st.end())
+            if(i!='*')
             {
-                ans+=s[i];
+                ans+=i;
             }
         }
         return ans;
