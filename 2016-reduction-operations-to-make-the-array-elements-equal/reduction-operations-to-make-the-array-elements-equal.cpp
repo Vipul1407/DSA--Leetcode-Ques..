03155672327af0ace917c591a086446c9d3e8ebd
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-14 20:44:36 +0530

    Time: 31 ms (83.46%) | Memory: 86.4 MB (84.65%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
+    //METHOD-2
+    //SORTING..
+    //TC= O(NLOGN)
     int reductionOperations(vector<int>& nums) 
     {
-        //max heap..
-        priority_queue<int>pq(nums.begin(),nums.end());
-
-        int top1;
-        int top2;
-        int prefixcnt=0;
+        int n= nums.size();
         int ans=0;
-        while(pq.size()>=2)
+        sort(nums.begin(),nums.end());
+
+        for(int i=n-1;i>=1;i--)
         {
-            top1= pq.top();
-            prefixcnt++;
-            pq.pop();
-            while(pq.size() && pq.top()==top1)
+           if(nums[i]==nums[i-1])
             {
-                pq.pop();
-                prefixcnt++;
+                continue;
             }
-            top2= pq.top();
-            //we will not pop top2 element..
-            if(pq.size())
-            {
-                ans+= prefixcnt;
-            }
+            ans+= n-i;
         }
         return ans;
     }
 };
+/*
+    
+//METHOD-1
+//MAX HEAP..
+//TC= O(NLOGN)
+int reductionOperations(vector<int> &nums)
+{
+    // max heap..
+    priority_queue<int> pq(nums.begin(), nums.end());
+
+    int top1;
+    int top2;
+    int prefixcnt = 0;
+    int ans = 0;
+    while (pq.size() >= 2)
+    {
+        top1 = pq.top();
+        prefixcnt++;
+        pq.pop();
+        while (pq.size() && pq.top() == top1)
+        {
+            pq.pop();
+            prefixcnt++;
+        }
+        top2 = pq.top();
+        // we will not pop top2 element..
+        // but if we have nothing in our pq, means all lements equal now in that case we don't need to add anything in ans..
+        //for eg. [1,1,1] here ans must be 0
+        if (pq.size())
+        {
+            ans += prefixcnt;
+        }
+    }
+    return ans;
+}
+
+*/
