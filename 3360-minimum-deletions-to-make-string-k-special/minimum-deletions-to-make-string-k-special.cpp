ca414d704e2ce7657fffbe4cb9f45c6c9b10b71d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-21 21:45:24 +0530

    Time: 3 ms (82.61%) | Memory: 19.3 MB (86.59%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -1,5 +1,39 @@
 class Solution {
 public:
+    //METHOD-2
+    //Using sorting...
+    int minimumDeletions(string word, int k) 
+    {
+        int n= word.size();
+        vector<int>vec(26,0);
+        for(auto i:word)
+        {
+            vec[i-'a']++;
+        }
+        sort(vec.begin(),vec.end());
+        int ans= n;
+        int commdel =0;
+        //we are assuming number to be in range of vec[i]---vec[i]+k
+        for(int i=0;i<26;i++)
+        {
+            int del= commdel;
+            for(int j=25;j>i;j--)
+            {
+                if(vec[j]-vec[i]<=k)
+                {
+                    break;
+                }
+                del+= vec[j]-vec[i]-k;
+            }
+            ans= min(ans,del);
+            commdel+= vec[i];
+        }
+        return ans==INT_MAX? 0:ans;
+    }
+};
+/*
+    //METHOD-1
+    //Without using sorting...
     int minimumDeletions(string word, int k) 
     {
         int n= word.size();
@@ -33,4 +67,4 @@ public:
         }
         return ans==INT_MAX? 0:ans;
     }
-};
+*/
