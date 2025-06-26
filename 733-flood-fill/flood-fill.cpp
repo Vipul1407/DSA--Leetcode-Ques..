16fcff53c77c4a485fa2102df7176fe67dda4f56
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-26 23:34:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.7 MB (6.03%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,5 +1,46 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    vector<int>dx= {0,1,0,-1};
+    vector<int>dy= {1,0,-1,0};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        int m= image.size();
+        int n=  image[0].size();
+        int prev= image[sr][sc];
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        vis[sr][sc]=1;
+        image[sr][sc]=color;
+        while(!q.empty())
+        {
+            auto top= q.front();
+            q.pop();
+            int x= top.first;
+            int y= top.second;
+
+            for(int k=0;k<4;k++)
+            {
+                int nx= x+ dx[k];
+                int ny= y+ dy[k];
+                if(nx<0 || ny<0 || nx>=m || ny>=n || vis[nx][ny])
+                {
+                    continue;
+                }
+                if(image[nx][ny]==prev)
+                {
+                    q.push({nx,ny});
+                    vis[nx][ny]=1;
+                    image[nx][ny]= color;
+                }
+            }
+        }
+        return image;
+    }
+};
+/*
     //METHOD-1
     //DFS..
     vector<int>dx= {0,1,0,-1};
@@ -31,4 +72,4 @@ public:
         dfs(sr,sc,prev,color,image,vis);
         return image;
     }
-};
+*/
