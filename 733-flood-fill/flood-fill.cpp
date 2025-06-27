3924454f29b2ec44426ac1bddc5e7eba9624c189
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-27 15:08:41 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (26.56%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,5 +1,132 @@
 class Solution {
 public:
+    //METHOD-3
+    //DSU..
+    class DSU
+    {
+        public:
+        vector<int>par,rank;
+        DSU(int n)
+        {
+            par.resize(n+1);
+            rank.resize(n+1,0);
+            for(int i=0;i<=n;i++)
+            {
+                par[i]=i;
+            }
+        }
+        
+        int findupar(int node)
+        {
+           if(node==par[node])
+           {
+                return node;
+           }
+           return par[node]= findupar(par[node]);
+        }
+        void unionbyrank(int u,int v)
+        {
+            int up_u= findupar(u);
+            int up_v= findupar(v);
+            if(up_u == up_v)
+            {
+                return;
+            }
+            if(rank[up_u]<rank[up_v])
+            {
+                par[up_u]= up_v;
+            }
+            else if(rank[up_v]<rank[up_u])
+            {
+                par[up_v]= up_u;
+            }
+            else
+            {
+                par[up_v]= up_u;
+                rank[up_u]++;
+            }
+        }
+    };
+    vector<int>dx= {0,1,0,-1};
+    vector<int>dy= {1,0,-1,0};
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        int m= image.size();
+        int n=  image[0].size();
+        int prev= image[sr][sc];
+
+        DSU dsu(m*n);
+
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int idx= i*n+j;
+                for(int k=0;k<4;k++)
+                {
+                    int nx= i+ dx[k];
+                    int ny= j+ dy[k];
+                    if(nx<0 || ny<0 || nx>=m || ny>=n )
+                    {
+                        continue;
+                    }
+                    if(image[i][j]==prev && image[nx][ny]==prev)
+                    {
+                        int nidx= nx*n+ny;
+                        dsu.unionbyrank(idx,nidx);
+                    }
+                }
+            }
+        }
+        int parent= dsu.findupar(sr*n+sc);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                int idx= i*n+j;
+                if(dsu.findupar(idx)==parent && image[i][j]==prev)
+                {
+                    image[i][j] = color;
+                }
+            }
+        }
+        image[sr][sc]=color;    
+        return image;
+    }
+};
+/*
+    //METHOD-1
+    //DFS..
+    vector<int>dx= {0,1,0,-1};
+    vector<int>dy= {1,0,-1,0};
+    void dfs(int i,int j,int &prev,int &color, vector<vector<int>>& image, vector<vector<int>>& vis)
+    {
+        vis[i][j]=1;
+        image[i][j]= color;
+        for(int k=0;k<4;k++)
+        {
+            int nx= i+dx[k];
+            int ny= j+dy[k];
+            if(nx<0 || ny<0 || nx>=image.size() || ny>=image[0].size() || vis[nx][ny])
+            {
+                continue;
+            }
+            if(image[nx][ny]==prev)
+            {
+                dfs(nx,ny,prev,color,image,vis);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        int m= image.size();
+        int n=  image[0].size();
+        int prev= image[sr][sc];
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        dfs(sr,sc,prev,color,image,vis);
+        return image;
+    }
+
     //METHOD-2
     //BFS..
     vector<int>dx= {0,1,0,-1};
@@ -37,39 +164,6 @@ public:
                 }
             }
         }
-        return image;
-    }
-};
-/*
-    //METHOD-1
-    //DFS..
-    vector<int>dx= {0,1,0,-1};
-    vector<int>dy= {1,0,-1,0};
-    void dfs(int i,int j,int &prev,int &color, vector<vector<int>>& image, vector<vector<int>>& vis)
-    {
-        vis[i][j]=1;
-        image[i][j]= color;
-        for(int k=0;k<4;k++)
-        {
-            int nx= i+dx[k];
-            int ny= j+dy[k];
-            if(nx<0 || ny<0 || nx>=image.size() || ny>=image[0].size() || vis[nx][ny])
-            {
-                continue;
-            }
-            if(image[nx][ny]==prev)
-            {
-                dfs(nx,ny,prev,color,image,vis);
-            }
-        }
-    }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
-    {
-        int m= image.size();
-        int n=  image[0].size();
-        int prev= image[sr][sc];
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        dfs(sr,sc,prev,color,image,vis);
         return image;
     }
 */
