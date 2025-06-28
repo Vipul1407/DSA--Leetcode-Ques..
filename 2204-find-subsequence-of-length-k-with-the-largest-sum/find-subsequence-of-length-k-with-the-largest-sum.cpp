9596eafea62c43914473690a37223da6ac1340a9
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-28 16:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (78.54%) - LeetSync

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,7 +1,44 @@
 class Solution {
 public:
     //METHOD-1
+    //Using Min Heap..
+    //TC= O(NLOGK + KLOGK)
+    //SC= O(K)
+    vector<int> maxSubsequence(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        //min heap..
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+        for(int i=0;i<n;i++)
+        {
+            pq.push({nums[i],i});
+            if(pq.size()>k)
+            {
+                pq.pop();
+            }
+        }
+
+        vector<pair<int,int>>arr;
+        while(pq.size())
+        {
+            arr.push_back({pq.top().second,pq.top().first});
+            pq.pop();
+        }
+        sort(arr.begin(),arr.end());
+        vector<int>res;
+        for(auto i:arr)
+        {
+            res.push_back(i.second);
+        }
+        return res;
+    }
+};
+/*
+    //METHOD-1
     //Using Vector..
+    //TC= O(NLOGN + KLOGK)
+    //SC= O(N+K)
     vector<int> maxSubsequence(vector<int>& nums, int k) 
     {
         int n= nums.size();
@@ -30,4 +67,4 @@ public:
         }
         return res;
     }
-};
+*/
