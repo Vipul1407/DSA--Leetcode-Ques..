49b2a75314cc424fd5ec8d2a415acf6596aee23d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-28 19:39:42 +0530

    Time: 483 ms (47.14%) | Memory: 108.6 MB (37.86%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -1,71 +1,81 @@
 class Solution {
 public:
-    bool issubseq(string &sub, string &s, int k)
+    bool issubseq(string &curr, string &s, int &k)
     {
         int i=0,j=0;
-        int L= sub.size();
         int n= s.size();
-        while(i<n && j<k*L)
+        int L= curr.size();
+        for(int i=0;i<n && j<k*L;i++)
         {
-            if(s[i]==sub[j%L])
+            if(s[i]==curr[j%L])
             {
                 j++;
             }
-            i++;
         }
         return j==k*L;
     }
-    void solve(string &curr,string &s,vector<int>&canuse,vector<int>&reqfreq,string &ans,int &k,int maxlen)
+    bool solve(string &curr, string &s, vector<int>reqfreqtemp, vector<int>canuse, string &ans, int &k, int len)
     {
-        if(curr.length()>maxlen)
-        {
-            return;
-        }
-        if(curr.length()>ans.length() || (curr.length()==ans.length() && curr>ans))
+        if(curr.length()==len)
         {
             if(issubseq(curr,s,k))
             {
                 ans= curr;
+                return true;
             }
+            return false;
         }
-        for(int i=0;i<26;i++)
+        for(int i=25;i>=0;i--)
         {
-            if(canuse[i]==false || reqfreq[i]==0)
+            if(canuse[i]==0 || reqfreqtemp[i]==0)
             {
                 continue;
             }
             char ch= i+'a';
             curr.push_back(ch);
-            reqfreq[i]--;
+            reqfreqtemp[i]--;
 
             //explore..
-            solve(curr,s,canuse,reqfreq,ans,k,maxlen);
-
+            if(solve(curr,s,reqfreqtemp,canuse,ans,k,len))
+            {
+                return true;
+            }
             //undo...
             curr.pop_back();
-            reqfreq[i]++;
+            reqfreqtemp[i]++;
         }
-
+        return false;
     }
-    string longestSubsequenceRepeatedK(string s, int k)
+    string longestSubsequenceRepeatedK(string s, int k) 
     {
         int n= s.size();
-        vector<int>canuse(26,0);
         vector<int>freq(26,0);
         vector<int>reqfreq(26,0);
-        
+        vector<int>canuse(26,0);
+
+        int maxlen= n/k;
         for(auto i:s)
         {
             freq[i-'a']++;
-            if(freq[i-'a']>=k)
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>=k)
             {
-                canuse[i-'a']=1;
-                reqfreq[i-'a']= freq[i-'a']/k;
+                canuse[i]=1;
+                reqfreq[i]= freq[i]/k;
             }
         }
-        int maxlen= n/k;
-        string ans,curr;
-        solve(curr,s,canuse,reqfreq,ans,k,maxlen);
+        string ans;
+        for(int len= maxlen; len>0; len--)
+        {
+            string curr="";
+            vector<int>reqfreqtemp= reqfreq;
+            if(solve(curr,s,reqfreqtemp,canuse,ans,k,len))
+            {
+                return ans;
+            }
+        }
         return ans;
     }
 };
