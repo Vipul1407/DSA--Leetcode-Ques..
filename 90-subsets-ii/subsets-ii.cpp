7cce7fc1989ad1ac168306bb40a7a14c844b7d52
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-06-30 21:58:30 +0530

    Time: 1 ms (50.48%) | Memory: 10.5 MB (67.91%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    //METHOD-2
+    //Using Sorting only..
+    void solve(int i,vector<int>&curr,vector<vector<int>>&ans,vector<int>& nums)
+    {
+        if(i>= nums.size())
+        {
+            ans.push_back(curr);
+            return;
+        }
+        //include..
+        curr.push_back(nums[i]);
+        solve(i+1,curr,ans,nums);
+        curr.pop_back();
+
+        //skip adjacent same elements before exclude..
+        while(i+1<nums.size() && nums[i]==nums[i+1])
+        {
+            i++;
+        }
+        
+        //exclude..
+        solve(i+1,curr,ans,nums);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) 
+    {
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+        vector<int>curr;
+        vector<vector<int>>ans;
+        solve(0,curr,ans,nums);
+        
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //Using Unordered-set + sorting
     void solve(int i,vector<int>&curr,set<vector<int>>&st,vector<int>& nums)
@@ -28,4 +63,4 @@ public:
         
         return ans;
     }
-};
+*/
