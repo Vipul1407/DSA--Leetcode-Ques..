3b66b5de06e35aae938389eb4030bfa77e37328e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-01 17:31:50 +0530

    Time: 4 ms (43.84%) | Memory: 14.1 MB (54.84%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,5 +1,36 @@
 class Solution {
 public:
+    //METHOD-2
+    void solve(int i,vector<int>&curr,vector<int>& nums, int tar, vector<vector<int>>&ans)
+    {
+        if(i>=nums.size() || tar<0)
+        {
+            return;
+        }
+        if(tar==0)
+        {
+            ans.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        //will not pass i+1 as we can have repeatitions..
+        solve(i,curr,nums,tar-nums[i],ans);
+        curr.pop_back();
+
+        solve(i+1,curr,nums,tar,ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int tar) 
+    {
+        int n= nums.size(); 
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(0,curr,nums,tar,ans);
+        return ans;
+    }
+};
+/*
+    //METHOD-1
     void solve(int i,vector<int>&curr,vector<int>& nums, int tar, vector<vector<int>>&ans)
     {
         if(i>=nums.size())
@@ -27,4 +58,4 @@ public:
         solve(0,curr,nums,tar,ans);
         return ans;
     }
-};
+*/
