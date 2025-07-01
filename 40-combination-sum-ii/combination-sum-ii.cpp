83d131d6a6aaba70009c08bf26e6211dadf1f7de
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-01 18:56:08 +0530

    Time: 4 ms (26.49%) | Memory: 14 MB (82.83%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,105 +1,73 @@
 class Solution {
 public:
     //METHOD-2
-    // Without For Loop.. + Freq(used) vector..
-    void solve(int i, int sum, vector<int>&used,vector<int>&temp, vector<int>&cand, int k, vector<vector<int>>&ans)
+    //Sorting only..
+    //TC= O(NLOGN + 2^N)
+    void solve(int i,vector<int>&curr,vector<int>& nums,int tar,vector<vector<int>>&ans)
     {
-        if(sum>k || i>=cand.size())
+        if(tar==0)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        if(i>=nums.size() || tar<0)
         {
             return;
         }
-        if(sum==k)
-        {
-            ans.push_back(temp);
-            return;
-        }
-        if(used[cand[i]]==0)
-        {
-            //call exclude case when i cannot have that freq.
-            solve(i+1,sum,used,temp,cand,k,ans);
-            return;
-        }
-       
-        //include case..
-        used[cand[i]]--;
-        temp.push_back(cand[i]);
-        solve(i,sum+cand[i],used,temp,cand,k,ans);
 
-        //backtracking..
-        temp.pop_back();
-        used[cand[i]]++;
-        //do this extra thing to prevent traversing the same recursive tree again.. before this we must sort cand array
-        while(i+1<cand.size() && cand[i]==cand[i+1])
+        curr.push_back(nums[i]);
+        //we should not like to have repetition..
+        solve(i+1,curr,nums,tar-nums[i],ans);
+        curr.pop_back();
+
+        while(i+1<nums.size() && nums[i]==nums[i+1])
         {
             i++;
         }
-        //exclude case..
-        solve(i+1,sum,used,temp,cand,k,ans);
+            
+        solve(i+1,curr,nums,tar,ans);
     }
-    vector<vector<int>> combinationSum2(vector<int>& cand, int k) 
+    vector<vector<int>> combinationSum2(vector<int>& nums, int tar) 
     {
+        sort(nums.begin(),nums.end());
+        vector<int>curr;
         vector<vector<int>>ans;
-        //must step...
-        sort(cand.begin(),cand.end());
-        vector<int>temp;
-        vector<int>used(51,0);
-        for(auto i:cand)
-        {
-            used[i]++;
-        }
-        solve(0,0,used,temp,cand,k,ans);
+        solve(0,curr,nums,tar,ans);
         return ans;
     }
 };
 /*
-// METHOD-1
-// Using For Loop.. + Freq(used) vector
-// BACKTRACKING..
-void solve(int i, int sum, vector<int> &used, vector<int> &temp, vector<int> &cand, int k, vector<vector<int>> &ans)
-{
-    if (sum > k || i >= cand.size())
+    //METHOD-1
+    // Set + Sorting
+    //TLE..
+    //TC= O(NLOGN + 2^N + N*(2^N))
+    void solve(int i,vector<int>&curr,vector<int>& nums,int tar,set<vector<int>>&st)
     {
-        return;
-    }
-    if (sum == k)
-    {
-        ans.push_back(temp);
-        return;
-    }
-    // include case..
-    for (int j = i; j < cand.size(); j++)
-    {
-        if (used[cand[j]] == 0)
+        if(i>=nums.size())
         {
-            continue;
+            if(tar==0)
+            {
+                st.insert(curr);
+            }
+            return;
         }
-        used[cand[j]]--;
-        temp.push_back(cand[j]);
-        solve(j, sum + cand[j], used, temp, cand, k, ans);
-
-        // backtracking..
-        temp.pop_back();
-        used[cand[j]]++;
-
-        // do this extra thing to prevent traversing the same recursive tree again.. before this we must sort cand array
-        while (j + 1 < cand.size() && cand[j] == cand[j + 1])
+        if(nums[i]<=tar)
         {
-            j++;
+            curr.push_back(nums[i]);
+            //we should not like to have repetition
+            solve(i+1,curr,nums,tar-nums[i],st);
+            curr.pop_back();
         }
+        solve(i+1,curr,nums,tar,st);
     }
-}
-vector<vector<int>> combinationSum2(vector<int> &cand, int k)
-{
-    vector<vector<int>> ans;
-    // must step...
-    sort(cand.begin(), cand.end());
-    vector<int> temp;
-    vector<int> used(51, 0);
-    for (auto i : cand)
+    vector<vector<int>> combinationSum2(vector<int>& nums, int tar) 
     {
-        used[i]++;
+        sort(nums.begin(),nums.end());
+        vector<int>curr;
+
+        set<vector<int>>st;
+        solve(0,curr,nums,tar,st);
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
     }
-    solve(0, 0, used, temp, cand, k, ans);
-    return ans;
-}
 */
