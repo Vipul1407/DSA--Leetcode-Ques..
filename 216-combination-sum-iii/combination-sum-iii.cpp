e98b1775eb54e25eed8b109ea496f22b87de289d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-01 19:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.77%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,64 +1,33 @@
 class Solution {
 public:
-    void solve(int start, int sum, vector<int>&temp, int k, int &tar, vector<vector<int>>&ans)
+    void solve(int i,int &k,int tar,vector<int> &curr,vector<vector<int>> &ans)
     {
-        if(sum>tar || k<0)
+        if (tar < 0 || curr.size()>k)
         {
             return;
         }
-        if(sum==tar && k==0)
+        if(tar == 0 && curr.size()==k)
         {
-            ans.push_back(temp);
+            ans.push_back(curr);
+            return;
         }
-        for(int i=start;i<10;i++)
+        if(i>9)
         {
-            temp.push_back(i);
-            //will pass i+1 as we want only unique elements...
-            solve(i+1,sum+i,temp,k-1,tar,ans);
-            temp.pop_back();
+            return;
         }
+
+        curr.push_back(i);
+        // we should not like to have repetition..
+        solve(i + 1, k, tar - i, curr, ans);
+        curr.pop_back();
+
+        solve(i + 1,  k, tar, curr, ans);
     }
     vector<vector<int>> combinationSum3(int k, int tar) 
     {
+        vector<int> curr;
         vector<vector<int>> ans;
-        vector<int> temp;
-        solve(1, 0, temp, k, tar, ans);
+        solve(1,k,tar,curr,ans);
         return ans;
     }
 };
-/*
-// METHOD-1
-// Without using For Loop..
-void solve(int start, int sum, vector<int> &temp, int k, int &tar, vector<vector<int>> &ans)
-{
-    if (sum > tar || k < 0)
-    {
-        return;
-    }
-    if (sum == tar && k == 0)
-    {
-        ans.push_back(temp);
-        return;
-    }
-    if (start > 9)
-    {
-        return;
-    }
-    temp.push_back(start);
-    // we will increment start here to ensure that each element occurrs atmost once..
-    // so need to take freq vector..
-    solve(start + 1, sum + start, temp, k - 1, tar, ans);
-    // backtracking...
-    temp.pop_back();
-
-    solve(start + 1, sum, temp, k, tar, ans);
-}
-vector<vector<int>> combinationSum3(int k, int tar)
-{
-    vector<vector<int>> ans;
-    vector<int> temp;
-
-    solve(1, 0, temp, k, tar, ans);
-    return ans;
-}
-*/
