dcc73b77858fe7cf1fbd6a5ec6a01b0b012cd2f7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-01 17:17:06 +0530

    Time: 1 ms (66.92%) | Memory: 14 MB (90.91%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,61 +1,30 @@
 class Solution {
 public:
-    // METHOD-2
-    // BACKTRACKING..
-    // Using For Loop..
-    void solve(int i,int sum, vector<int>&temp, int &k, vector<int>&cand, vector<vector<int>>&ans)
+    void solve(int i,vector<int>&curr,vector<int>& nums, int tar, vector<vector<int>>&ans)
     {
-        if(sum>k || i>=cand.size())
+        if(i>=nums.size())
         {
+            if(tar==0)
+            {
+                ans.push_back(curr);
+            }
             return;
         }
-        if(sum==k)
+        if(nums[i]<=tar)
         {
-            ans.push_back(temp);
-            return;
+            curr.push_back(nums[i]);
+            //will not pass i+1 as we can have repeatitions..
+            solve(i,curr,nums,tar-nums[i],ans);
+            curr.pop_back();
         }
-        for(int j=i;j<cand.size();j++)
-        {
-            temp.push_back(cand[j]);
-            solve(j,sum+cand[j],temp,k,cand,ans);
-            temp.pop_back();
-        }
+        solve(i+1,curr,nums,tar,ans);
     }
-    vector<vector<int>> combinationSum(vector<int>& cand, int k) 
+    vector<vector<int>> combinationSum(vector<int>& nums, int tar) 
     {
+        int n= nums.size(); 
         vector<vector<int>>ans;
-        vector<int>temp;
-        solve(0,0,temp,k,cand,ans);
+        vector<int>curr;
+        solve(0,curr,nums,tar,ans);
         return ans;
     }
 };
-/*
-// METHOD-1
-// BACKTRACKING..
-// Without using for loop...
-void solve(int i, int sum, vector<int> &temp, int &k, vector<int> &cand, vector<vector<int>> &ans)
-{
-    if (sum == k)
-    {
-        ans.push_back(temp);
-        return;
-    }
-    if (i >= cand.size() || sum > k)
-    {
-        return;
-    }
-    temp.push_back(cand[i]);
-    // include..
-    solve(i, sum + cand[i], temp, k, cand, ans);
-    temp.pop_back();
-    // exclude..
-    solve(i + 1, sum, temp, k, cand, ans);
-}
-vector<vector<int>> combinationSum(vector<int> &cand, int k)
-{
-    vector<vector<int>> ans;
-    vector<int> temp;
-    solve(0, 0, temp, k, cand, ans);
-    return ans;
-}
-*/
