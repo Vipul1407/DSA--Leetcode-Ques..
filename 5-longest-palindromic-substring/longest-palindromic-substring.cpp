86d9686a00a7978f0d1d5d4fd323641eb55d4c4d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-02 17:20:22 +0530

    Time: 336 ms (17.41%) | Memory: 291.8 MB (11.31%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,6 +1,45 @@
 class Solution {
 public:
     //METHOD-1
+    //TDA..
+    bool palindrome(int i,int j, string &s, vector<vector<int>>&dp)
+    {
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        if(i>=j)
+        {
+            return true;
+        }
+        if(s[i]!=s[j])
+        {
+            return false;
+        }
+        return dp[i][j]= palindrome(i+1,j-1,s,dp);
+    }
+    string longestPalindrome(string s) 
+    {
+        int n= s.size();
+        int maxlen=0;
+        int start=0;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(palindrome(i,j,s,dp) && j-i+1> maxlen)
+                {
+                    start=i;
+                    maxlen= max(maxlen,j-i+1);
+                }
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+};
+/*
+    //METHOD-1
     //RECURSIVE..
     bool palindrome(int i,int j, string &s)
     {
@@ -32,4 +71,4 @@ public:
         }
         return s.substr(start,maxlen);
     }
-};
+*/
