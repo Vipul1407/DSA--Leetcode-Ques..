f46c6f563382570ac3b78820ed86aeae94dedb29
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-02 23:57:12 +0530

    Time: 11 ms (49.09%) | Memory: 16.3 MB (37.48%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS...
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        int n= s.size();
+        unordered_set<string>st(wordDict.begin(),wordDict.end());
+        queue<int>q;
+        vector<int>vis(n+1,0);
+        q.push(0);
+        vis[0]=1;
+        while(!q.empty())
+        {
+            auto idx= q.front();
+            q.pop();
+            for(int end=idx+1; end<=n; end++)
+            {
+                if(vis[end])
+                {
+                    continue;
+                }
+                string temp= s.substr(idx,end-idx);
+                if(st.find(temp)!=st.end())
+                {
+                    if(end==n)
+                    {
+                        return true;
+                    }
+                    if(!vis[end])
+                    {
+                        q.push(end);
+                        vis[end]=1;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+/*
+    //METHOD-1
+    //DFS...
     bool solve(int i,string &s,unordered_set<string>&st,vector<int>&dp)
     {
         if(dp[i]!=-1)
@@ -14,6 +56,7 @@ public:
         {
             return true;
         }
+        //for(int len=0;len<s.size();len++)
         for(int len=0;len+i<=s.size();len++)
         {
             string temp= s.substr(i,len);
@@ -32,4 +75,4 @@ public:
         vector<int>dp(n+1,-1);
         return solve(0,s,st,dp);
     }
-};
+*/
