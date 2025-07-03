0f41756ac6795c6c8589cb1050cdbd58212da961
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-03 20:17:57 +0530

    Time: 2 ms (64.66%) | Memory: 11.2 MB (13.63%) - LeetSync

---
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    //METHOD-1
+    //RECURSIVE..
+    //TC= O(1+2+4+8+..2^N)= O(2^N -1)= O(K)
+    //SC= O(LOG2(N)+K)= O(K)
+    void solve(int k,string str,string &ans)
+    {
+        if(str.size()>=k)
+        {
+            ans= str;
+            return;
+        }
+        string temp;
+        for(auto i:str)
+        {
+            int ascii= i-'a';
+            temp+= (ascii+1)%26 + 'a';
+        }
+    
+        solve(k,str+temp,ans);
+    }
+    char kthCharacter(int k) 
+    {
+        string ans="";
+        solve(k,"a",ans);
+        return ans[k-1];//since k is 1 based
+    }
+};
