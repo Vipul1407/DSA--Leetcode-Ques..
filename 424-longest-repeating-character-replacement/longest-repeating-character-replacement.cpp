7d8bc596ee4aedc9adf8b42cd5d882761c85cc21
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-05 16:03:44 +0530

    Time: 4 ms (62.04%) | Memory: 10.7 MB (92.04%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,7 +1,38 @@
 class Solution {
 public:
+    //METHOD-2
+    //TC= O(N) USING IF
+    int characterReplacement(string s, int k) 
+    {
+        int n= s.size();
+        int l=0,r=0,ans=0,maxfreq=0;
+        vector<int>mp(26,0);
+        while(r<n)
+        {
+            mp[s[r]-'A']++;
+            maxfreq= max(maxfreq,mp[s[r]-'A']);
+
+            if((r-l+1)-maxfreq>k)
+            {
+                mp[s[l]-'A']--;
+                for(auto i:mp)
+                {
+                    maxfreq= max(maxfreq,i);
+                }
+                l++;
+            }
+            if((r-l+1)-maxfreq<=k)
+            {
+                ans= max(ans,r-l+1);
+            }
+            r++;
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
-    //TC= O(2N)
+    //TC= O(2N)  USING WHILE
     int characterReplacement(string s, int k) 
     {
         int n= s.size();
@@ -26,4 +57,4 @@ public:
         }
         return ans;
     }
-};
+*/
