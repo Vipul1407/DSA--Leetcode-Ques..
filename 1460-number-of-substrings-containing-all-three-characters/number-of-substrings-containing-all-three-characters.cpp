bf8855fc7a43fdb1577bfd7babd031865588fc50
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-05 19:19:54 +0530

    Time: 7 ms (68.58%) | Memory: 10.7 MB (99.34%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    //METHOD-2
+    //SIMPLE APPROACH
+    int numberOfSubstrings(string s) 
+    {
+        int n= s.size();
+        //recent index..
+        int a=-1,b=-1,c=-1,ans=0;
+ 
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='a')
+            {
+                a=i;
+            }
+            else if(s[i]=='b')
+            {
+                b=i;
+            }
+            else
+            {
+                c=i;
+            }
+            ans+= 1+min(a,min(b,c));
+        }   
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //NORMAL SLIDIING WINDOW
     int numberOfSubstrings(string s) 
     {
         int n= s.size();
@@ -18,4 +48,4 @@ public:
         }   
         return ans;
     }
-};
+*/
