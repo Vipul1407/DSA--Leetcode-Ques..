d54e1b84c5c97df89fccc448408ddaf4d741b274
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-05 18:56:27 +0530

    Time: 2 ms (91.96%) | Memory: 11.1 MB (34.38%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,70 +1,21 @@
 class Solution {
 public:
-    //METHOD-2
-    //Using Vector..
     int numberOfSubstrings(string s) 
     {
         int n= s.size();
-        vector<int>cnt(3,0);
-        int ans=0,i=0,j=0;
-        while(j<n)
+        vector<int>mp(3,0);
+        int l=0,r=0,ans=0;
+        while(r<n)
         {
-            cnt[s[j]-'a']++;
-            while(cnt[0]>0 && cnt[1]>0 && cnt[2]>0)
+            mp[s[r]-'a']++;
+            while(mp[0]>0 && mp[1]>0 && mp[2]>0)
             {
-                ans+= n-j;
-                cnt[s[i]-'a']--;
-                i++;
+                ans+= n-r;
+                mp[s[l]-'a']--;
+                l++;
             }
-            j++;
-        }
+            r++;
+        }   
         return ans;
     }
 };
-/*
-//METHOD-1
-//Using Variables..
-int numberOfSubstrings(string s)
-{
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int n = s.size();
-    int i = 0, j = 0;
-    int ans = 0;
-    while (j < n)
-    {
-        if (s[j] == 'a')
-        {
-            a++;
-        }
-        else if (s[j] == 'b')
-        {
-            b++;
-        }
-        else
-        {
-            c++;
-        }
-        while (a > 0 && b > 0 && c > 0)
-        {
-            ans += n - j;
-            if (s[i] == 'a')
-            {
-                a--;
-            }
-            else if (s[i] == 'b')
-            {
-                b--;
-            }
-            else
-            {
-                c--;
-            }
-            i++;
-        }
-        j++;
-    }
-    return ans;
-}
-*/
