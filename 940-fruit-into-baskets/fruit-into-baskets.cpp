9c660e8f2aa318b349431d2faa0ebeab99ae9d17
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-05 12:24:53 +0530

    Time: 32 ms (80.27%) | Memory: 74.9 MB (76.43%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //METHOD-2
+    //TC= O(N) USING IF
     int totalFruit(vector<int>& fruits) 
     {
         int n= fruits.size();
@@ -8,7 +10,6 @@ public:
         while(r<n)
         {
             mp[fruits[r]]++;
-            //while(l<r && mp.size()>2)
             if(l<r && mp.size()>2)
             {
                 if(--mp[fruits[l]]==0)
@@ -26,3 +27,28 @@ public:
         return ans;
     }
 };
+/*
+    //METHOD-1
+    //TC= O(2N) USING WHILE
+    int totalFruit(vector<int>& fruits) 
+    {
+        int n= fruits.size();
+        unordered_map<int,int>mp;
+        int l=0,r=0,ans=0;
+        while(r<n)
+        {
+            mp[fruits[r]]++;
+            while(l<r && mp.size()>2)
+            {
+                if(--mp[fruits[l]]==0)
+                {
+                    mp.erase(fruits[l]);
+                }
+                l++;
+            }
+            ans= max(ans,r-l+1);    
+            r++;
+        }
+        return ans;
+    }
+*/
