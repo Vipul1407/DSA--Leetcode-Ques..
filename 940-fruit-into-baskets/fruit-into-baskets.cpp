6c1e639d3e295de5d9a776ac3b35d9217742cb28
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-05 12:18:04 +0530

    Time: 23 ms (89.98%) | Memory: 75.1 MB (56.85%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,57 +1,24 @@
 class Solution {
 public:
-    // METHOD-2
-    // OPTIMIZED
-    // SLIDING WINDOW..
     int totalFruit(vector<int>& fruits) 
     {
         int n= fruits.size();
-        int i=0,j=0;
-        int ans=0;
         unordered_map<int,int>mp;
-        while(j<n)
+        int l=0,r=0,ans=0;
+        while(r<n)
         {
-            mp[fruits[j]]++;
-            //shrink window..
-            if(mp.size()>2)
+            mp[fruits[r]]++;
+            while(l<r && mp.size()>2)
             {
-                mp[fruits[i]]--;
-                if(mp[fruits[i]]==0)
+                if(--mp[fruits[l]]==0)
                 {
-                    mp.erase(fruits[i]);
+                    mp.erase(fruits[l]);
                 }
-                i++;
+                l++;
             }
-            ans= max(ans,j-i+1);
-            j++;
+            ans= max(ans,r-l+1);
+            r++;
         }
         return ans;
     }
 };
-/*
-// Ques is same as Find substring with atmost 2 distinct char...
-// METHOD-1
-// BRUTE FORCE..
-int totalFruit(vector<int> &fruits)
-{
-    int n = fruits.size();
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-    {
-        unordered_set<int> st;
-        for (int j = i; j < n; j++)
-        {
-            st.insert(fruits[j]);
-            if (st.size() <= 2)
-            {
-                ans = max(ans, j - i + 1);
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-    return ans;
-}
-*/
