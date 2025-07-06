70416f0ca02870f072555910cb22f68cde24715a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-06 10:41:46 +0530

    Time: 2 ms (57.43%) | Memory: 32.5 MB (97.31%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    //METHOD-2
+    //SLIDING WINDOW APPROACH..
+    int solve(vector<int>& nums, int k) 
+    {
+        int n= nums.size();
+        int l=0,r=0,ans=0,sum=0;
+        while(r<n)
+        {
+            sum+= nums[r];
+            while(l<r && sum>k)
+            {
+                sum-= nums[l];
+                l++;
+            }
+            if(sum<=k)
+                ans+= r-l+1;
+            r++;
+        }
+        return ans;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int k) 
+    {
+        return solve(nums,k)-solve(nums,k-1);
+    }
+};
+/*
     //METHOD-1
     //PREFIXSUM APPROACH..
     int numSubarraysWithSum(vector<int>& nums, int k) 
@@ -17,4 +43,4 @@ public:
         }
         return ans;
     }
-};
+*/
