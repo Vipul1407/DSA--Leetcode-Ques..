d1ce1aa898d027901a9f7cd3984931f08d723d01
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-06 20:05:10 +0530

    Time: 107 ms (6.27%) | Memory: 28.3 MB (91.98%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    //METHOD-2
+    //BETTER FORCE..
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        int n= nums.size();
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            bool flag= false;
+            for(int j=i+1;j<i+n;j++)
+            {
+                if(nums[j%n]>nums[i])
+                {
+                    ans.push_back(nums[j%n]);
+                    flag= true;
+                    break;
+                }
+            }
+            if(!flag)
+            {
+                ans.push_back(-1);
+            }
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //BRUTE FORCE..
     vector<int> nextGreaterElements(vector<int>& nums) 
@@ -37,4 +64,4 @@ public:
         }
         return ans;
     }
-};
+*/
