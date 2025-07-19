067f88066c8c4e5c30760730b205b75c66b1f100
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-07-19 23:59:29 +0530

    Time: 123 ms (33.12%) | Memory: 77.4 MB (32.47%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,36 +1,5 @@
 class Solution {
 public:
-    //sorting of strings a,ab,cd,cf,cde will be-->
-    //a,ab,cd,cde,cf based on lexicographical order..
-    vector<string> removeSubfolders(vector<string>& folder) 
-    {
-        sort(folder.begin(),folder.end());
-        vector<string>ans;
-
-        //sorted h isliye folder[0] ka koi parent ni hoga
-        ans.push_back(folder[0]);
-
-        int n=folder.size();
-        for(int i=1;i<n;i++)
-        {
-            string curr= folder[i];
-            string last= ans.back();
-            //this is added so to differentiate b/w a/b/c and a/b/ca
-            //since a/b/ca is not a substring of a/b/c/ but it will be assumed as substring is we seee only a/b/c..
-            last+='/';
-            //agr currfolder substring ni h lastfolder ke to ans me include krenge..
-            if(curr.find(last)!=0)
-            {
-                ans.push_back(curr);
-            }
-        }
-        return ans;
-    }
-};
-
-/*
-class Solution {
-public:
     vector<string> removeSubfolders(vector<string>& folder) 
     {
         vector<string>ans;
@@ -39,7 +8,7 @@ public:
         {
             bool include=true;
             int n=str.size();
-            int i=n-1;//starting form last index to find last '/'
+            int i=n-1;
             while(i>=0)
             {
                 while(i>=0 && str[i]!='/')
@@ -56,10 +25,9 @@ public:
                 //set me ni mila..
                 else
                 {
-                    i--;//process repeat kr
+                    i--;
                 }
             }
-            //flag true h mtlb set me ni mila kbhi hence ans me include krenge..
             if(include)
             {
                 ans.push_back(str);
@@ -68,4 +36,3 @@ public:
         return ans;
     }
 };
-*/
