b142812f10144deb7da3e56541a5489b0cf9e708
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-07 18:08:37 +0530

    Time: 35 ms (41.23%) | Memory: 47.8 MB (19.40%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,50 +1,14 @@
 class Solution {
 public:
-    //METHOD-2
-    // Without Using Heap..
-    //TC= O(N)
-    int leastInterval(vector<char>& task, int n) 
-    {
-        vector<int>freq(26,0);
-        for(auto i:task)
-        {
-            freq[i-'A']++;
-        }
-        sort(freq.begin(),freq.end());
-        int time=0;
-        //maxfreq= freq[25]
-        int gadhe= freq[25]-1;
-        int idle= gadhe*n;
-        for(int i=24;i>=0;i--)
-        {
-            if(freq[i]==0)
-            {
-                break;
-            }
-            idle-= min(gadhe,freq[i]);
-        }
-        if(idle>0)
-        {
-            return task.size()+idle;
-        }
-        else
-        {
-            return task.size();
-        }
-    }
-};
-/*
     //METHOD-1
-    //MAX HEAP..(PRIORITY_QUEUE)
-    //TC= O(N*LOG26)
-    int leastInterval(vector<char>& task, int n) 
+    //MAX HEAP..
+    int leastInterval(vector<char>& tasks, int n) 
     {
         vector<int>freq(26,0);
-        for(auto i:task)
+        for(auto i:tasks)
         {
             freq[i-'A']++;
         }
-        //max heap storing freq..
         priority_queue<int>pq;
         for(auto i:freq)
         {
@@ -57,25 +21,29 @@ public:
         while(pq.size())
         {
             vector<int>temp;
-            //run loop n+1 times
-            for(int i=1;i<=n+1;i++)
+            //run loop n+1 times..
+            for(int i=0;i<=n;i++)
             {
                 if(pq.size())
                 {
                     int cnt= pq.top();
-                    pq.pop();
                     temp.push_back(--cnt);
+                    pq.pop();
                 }
                 else
                 {
                     break;
                 }
             }
-            for(auto &f:temp)
+            for(auto i:temp)
             {
-                if(f>0)
+                if(i>0)
                 {
-                    pq.push(f);
+                    pq.push(i);
+                }
+                else
+                {
+                    break;
                 }
             }
             if(pq.size())
@@ -89,4 +57,4 @@ public:
         }
         return time;
     }
-*/
+};
