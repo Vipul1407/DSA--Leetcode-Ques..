ce5ef4755fab2326809207704e8111e6a4d9ac14
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-07 15:43:17 +0530

    Time: 8 ms (67.86%) | Memory: 91.6 MB (66.84%) - LeetSync

--- 3178-minimum-increment-operations-to-make-array-beautiful/minimum-increment-operations-to-make-array-beautiful.cpp
+++ 3178-minimum-increment-operations-to-make-array-beautiful/minimum-increment-operations-to-make-array-beautiful.cpp
@@ -1,6 +1,54 @@
 class Solution {
 public:
     //METHOD-2
+    //BUA APPROACH..
+    long long minIncrementOperations(vector<int>& nums, int k)
+    {
+        int n= nums.size();
+    
+        vector<long long>dp(n+3,0);
+        for(int i=0;i<n;i++)
+        {
+            nums[i]= max(0,k-nums[i]);
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            long long a= nums[i]+ dp[i+1];
+            long long b= nums[i]+ dp[i+2];
+            long long c= nums[i]+ dp[i+3];
+            dp[i]= min(a,min(b,c));
+        }
+        return min(dp[0],min(dp[1],dp[2]));
+    }
+};
+/*
+    //METHOD-1
+    //RECUSIVE APPROACH..
+    long long rec(int i,vector<int>& nums, int k)
+    {
+        if(i>=nums.size())
+        {
+            return 0;
+        }
+        long long a= nums[i]+ rec(i+1,nums,k);
+        long long b= nums[i]+ rec(i+2,nums,k);
+        long long c= nums[i]+ rec(i+3,nums,k);
+        return min(a,min(b,c));
+    }
+    long long minIncrementOperations(vector<int>& nums, int k)
+    {
+        int n= nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums[i]= max(0,k-nums[i]);
+        }
+        long long ans= rec(0,nums,k);
+        ans= min(ans,rec(1,nums,k));
+        ans= min(ans,rec(2,nums,k));
+        return ans;
+    }
+
+    //METHOD-2
     //TDA APPROACH..
     long long rec(int i,vector<int>& nums, int k, vector<long long>&dp)
     {
@@ -29,33 +77,6 @@ public:
         long long ans= rec(0,nums,k,dp);
         ans= min(ans,rec(1,nums,k,dp));
         ans= min(ans,rec(2,nums,k,dp));
-        return ans;
-    }
-};
-/*
-    //METHOD-1
-    //RECUSIVE APPROACH..
-    long long rec(int i,vector<int>& nums, int k)
-    {
-        if(i>=nums.size())
-        {
-            return 0;
-        }
-        long long a= nums[i]+ rec(i+1,nums,k);
-        long long b= nums[i]+ rec(i+2,nums,k);
-        long long c= nums[i]+ rec(i+3,nums,k);
-        return min(a,min(b,c));
-    }
-    long long minIncrementOperations(vector<int>& nums, int k)
-    {
-        int n= nums.size();
-        for(int i=0;i<n;i++)
-        {
-            nums[i]= max(0,k-nums[i]);
-        }
-        long long ans= rec(0,nums,k);
-        ans= min(ans,rec(1,nums,k));
-        ans= min(ans,rec(2,nums,k));
         return ans;
     }
 */
