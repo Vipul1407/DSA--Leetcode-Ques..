f3bd9a3ddfb2f710fd1ee25b51b125eaaaaf4f4d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-09 15:18:04 +0530

    Time: 1 ms (29.50%) | Memory: 8.3 MB (72.06%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,96 +1,50 @@
 class Solution {
 public:
-    // METHOD-2
-    // TC= O(N)
-    string reorganizeString(string s)
+    //METHOD-1
+    //Max Heap..
+    string reorganizeString(string s) 
     {
-        int n = s.size();
-        vector<int> vec(26, 0);
-        string ans(n,' ');
-        int maxfreq=0;
-        char ch;
-        for (auto i : s)
+        vector<int>freq(26,0);
+        string ans="";
+        int n= s.size(); 
+        for(auto i:s)
         {
-            vec[i - 'a']++;
-            if (vec[i - 'a'] > (n + 1) / 2)
+            freq[i-'a']++;
+            if(freq[i-'a']> (n+1)/2)
             {
-                return "";
-            }
-            if(vec[i-'a']>maxfreq)
-            {
-                ch= i;
-                maxfreq= vec[i-'a'];
+                return ""; 
             }
         }
-        int i=0;
-        int cnt= vec[ch-'a'];
-        while(i<n && cnt>0)
+        //max heap..
+        priority_queue<pair<int,char>>pq;
+        for(int i=0;i<26;i++)
         {
-            ans[i]= ch;
-            cnt--;
-            i+=2;
-        }
-        vec[ch-'a']= 0;
-        for(char ch='a';ch<='z';ch++)
-        {
-            while(vec[ch-'a']>0)
+            if(freq[i]>0)
             {
-                if(i>=n)
-                {
-                    i=1;
-                }
-                ans[i]= ch;
-                vec[ch-'a']--;
-                i+=2;
+                pq.push({freq[i],i+'a'});
             }
+        }
+        while(pq.size()>=2)
+        {
+            auto top1= pq.top();
+            pq.pop();
+            auto top2= pq.top();
+            pq.pop();
+            ans+= top1.second;
+            ans+= top2.second;
+            if(top1.first>1)
+            {
+                pq.push({top1.first-1,top1.second});
+            }
+            if(top2.first>1)
+            {
+                pq.push({top2.first-1,top2.second});
+            }
+        }
+        if(!pq.empty())
+        {
+            ans+= pq.top().second;
         }
         return ans;
     }
 };
-/*
-    // METHOD-1
-    // PRIORITY_QUEUE
-    // TC= O(NLOGN)
-    string reorganizeString(string s)
-    {
-        int n = s.size();
-        priority_queue<pair<int, char>> pq;
-        vector<int> vec(26, 0);
-        for (auto i : s)
-        {
-            vec[i - 'a']++;
-            if (vec[i - 'a'] > (n + 1) / 2)
-            {
-                return "";
-            }
-        }
-        for (int i = 0; i < 26; i++)
-        {
-            if (vec[i] > 0)
-                pq.push({vec[i], i + 'a'});
-        }
-        string ans = "";
-        while (pq.size() >= 2)
-        {
-            auto ele1 = pq.top();
-            pq.pop();
-            auto ele2 = pq.top();
-            pq.pop();
-            ans += ele1.second;
-            ans += ele2.second;
-            if (ele1.first > 1)
-            {
-                pq.push({ele1.first - 1, ele1.second});
-            }
-            if (ele2.first > 1)
-            {
-                pq.push({ele2.first - 1, ele2.second});
-            }
-        }
-        if (!pq.empty())
-        {
-            ans += pq.top().second;
-        }
-        return ans;
-    }
-*/
