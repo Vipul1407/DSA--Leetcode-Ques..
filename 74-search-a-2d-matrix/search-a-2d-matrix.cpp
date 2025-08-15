86e6cca981d1783b1919db778d77d2ea760003ca
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-15 09:11:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.01%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,8 +1,59 @@
 class Solution {
 public:
+    //METHOD-3
+    //OPTIMIZED APPROACH..
+    //TC= O(LOG(M*N))
+    bool searchMatrix(vector<vector<int>>& mat, int target) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        int l= 0;
+        int r= m*n-1;
+        while(l<=r)
+        {
+            int mid= l+(r-l)/2;
+            int row= mid/n;
+            int col= mid%n;
+            if(mat[row][col]==target)
+            {
+                return true;
+            }
+            else if(mat[row][col]<target)
+            {
+                l= mid+1;
+            }
+            else
+            {
+                r= mid-1;
+            }
+        }
+        return false;
+    }
+};
+/*
+    //METHOD-1
+    //BRUTE FORCE..
+    //TC= O(M*N)
+    bool searchMatrix(vector<vector<int>>& mat, int target) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     //METHOD-2
     //BETTER APPROACH..
-    //TC= O(M + LOGN)
+    //TC= O(M*LOGN)
     bool searchMatrix(vector<vector<int>>& mat, int target) 
     {
         int m= mat.size();
@@ -25,27 +76,6 @@ public:
                 else
                 {
                     r= mid-1;
-                }
-            }
-        }
-        return false;
-    }
-};
-/*
-    //METHOD-1
-    //BRUTE FORCE..
-    //TC= O(M*N)
-    bool searchMatrix(vector<vector<int>>& mat, int target) 
-    {
-        int m= mat.size();
-        int n= mat[0].size();
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(mat[i][j]==target)
-                {
-                    return true;
                 }
             }
         }
