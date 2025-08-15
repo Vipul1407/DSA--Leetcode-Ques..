2cd003042cebf2713c397cc0e1d3b45d603bad2d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-15 08:40:56 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.01%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,7 +1,40 @@
 class Solution {
 public:
+    //METHOD-2
+    //BETTER APPROACH..
+    //TC= O(M + LOGN)
+    bool searchMatrix(vector<vector<int>>& mat, int target) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        for(int i=0;i<m;i++)
+        {
+            int l= 0;
+            int r= n-1;
+            while(l<=r)
+            {
+                int mid= l+(r-l)/2;
+                if(mat[i][mid]==target)
+                {
+                    return true;
+                }
+                else if(mat[i][mid]<target)
+                {
+                    l= mid+1;
+                }
+                else
+                {
+                    r= mid-1;
+                }
+            }
+        }
+        return false;
+    }
+};
+/*
     //METHOD-1
     //BRUTE FORCE..
+    //TC= O(M*N)
     bool searchMatrix(vector<vector<int>>& mat, int target) 
     {
         int m= mat.size();
@@ -18,4 +51,4 @@ public:
         }
         return false;
     }
-};
+*/
