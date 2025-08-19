bf2a2484058c0dcc015e353cb53b28ab799eac2b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-19 19:40:01 +0530

    Time: 5 ms (58.98%) | Memory: 21.1 MB (9.79%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,50 @@
 class Solution {
 public:
+    //METHOD-2
+    //DFS... Cycle detection in directed graph;;
+    bool dfs(int i, unordered_map<int,vector<int>>&adj, vector<int>&vis)
+    {
+        vis[i]=1;
+        for(auto &neigh: adj[i])
+        {
+            if(vis[neigh]==1)
+            {
+                return false;
+            }
+            if(vis[neigh]==0)
+            {
+                if(!dfs(neigh,adj,vis))
+                {
+                    return false;
+                }
+            }
+        }
+        vis[i]=2;
+        return true;
+    }
+    bool canFinish(int n, vector<vector<int>>& prerequisites) 
+    {
+        unordered_map<int,vector<int>>adj;
+        vector<int>vis(n,0);
+        for(auto i:prerequisites)
+        {
+            adj[i[1]].push_back(i[0]);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                if(!dfs(i,adj,vis))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+/*
     //METHOD-1
     //BFS using Kahn Algo..
     bool canFinish(int n, vector<vector<int>>& prerequisites) 
@@ -37,4 +82,4 @@ public:
         }
         return cnt==n;
     }
-};
+*/
