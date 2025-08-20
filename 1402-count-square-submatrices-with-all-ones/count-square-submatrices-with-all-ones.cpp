875fe7e6f44cde08a9b1be343cb66affaaa41d1c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-20 15:28:01 +0530

    Time: 13 ms (18.92%) | Memory: 27.7 MB (88.20%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    //METHOD-2
-    //BUA APPROACH..
+    //METHOD-3
+    //Space Optimized APPROACH..
     int countSquares(vector<vector<int>>& mat) 
     {
         int cnt=0;
         int m= mat.size();
         int n= mat[0].size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        vector<int>curr(n+1,0);
+        vector<int>next(n+1,0);
 
         for(int i=m-1;i>=0;i--)
         {
@@ -15,10 +16,15 @@ public:
             {
                 if(mat[i][j]==1)
                 {
-                    dp[i][j]= 1+min({dp[i+1][j],dp[i][j+1],dp[i+1][j+1]});
-                }               
-                cnt+= dp[i][j];
+                    curr[j]= 1+min({next[j],curr[j+1],next[j+1]});
+                }   
+                else
+                {
+                    curr[j]=0;
+                }
+                cnt+= curr[j];
             }
+            next=curr;
         }
         return cnt;
     }
