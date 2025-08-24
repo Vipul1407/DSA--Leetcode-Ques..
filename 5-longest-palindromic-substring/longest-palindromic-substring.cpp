1ee9b7524a39311f282ce56d32fbba594b6dd3c3
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-24 22:37:31 +0530

    Time: 533 ms (13.89%) | Memory: 292 MB (9.72%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,8 +1,8 @@
 class Solution {
-public:
-    //METHOD-1
+public: 
+    //METHOD-2
     //TDA..
-    bool palindrome(int i,int j, string &s, vector<vector<int>>&dp)
+    bool solve(int i,int j,string &s,vector<vector<int>>&dp)
     {
         if(dp[i][j]!=-1)
         {
@@ -10,13 +10,16 @@ public:
         }
         if(i>=j)
         {
-            return true;
+            return 1;
         }
-        if(s[i]!=s[j])
+        if(s[i]==s[j])
         {
-            return false;
+            return dp[i][j]= solve(i+1,j-1,s,dp);
         }
-        return dp[i][j]= palindrome(i+1,j-1,s,dp);
+        else
+        {
+            return dp[i][j]= 0;
+        }
     }
     string longestPalindrome(string s) 
     {
@@ -26,49 +29,18 @@ public:
         vector<vector<int>>dp(n,vector<int>(n,-1));
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<n;j++)
+            for(int j=0;j<n;j++)
             {
-                if(palindrome(i,j,s,dp) && j-i+1> maxlen)
+                if(solve(i,j,s,dp))
                 {
-                    start=i;
-                    maxlen= max(maxlen,j-i+1);
+                    if(j-i+1>maxlen)
+                    {
+                        maxlen= j-i+1;
+                        start=i;
+                    }
                 }
             }
         }
         return s.substr(start,maxlen);
     }
 };
-/*
-    //METHOD-1
-    //RECURSIVE..
-    bool palindrome(int i,int j, string &s)
-    {
-        if(i>=j)
-        {
-            return true;
-        }
-        if(s[i]!=s[j])
-        {
-            return false;
-        }
-        return palindrome(i+1,j-1,s);
-    }
-    string longestPalindrome(string s) 
-    {
-        int n= s.size();
-        int maxlen=0;
-        int start=0;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-            {
-                if(palindrome(i,j,s) && j-i+1> maxlen)
-                {
-                    start=i;
-                    maxlen= max(maxlen,j-i+1);
-                }
-            }
-        }
-        return s.substr(start,maxlen);
-    }
-*/
