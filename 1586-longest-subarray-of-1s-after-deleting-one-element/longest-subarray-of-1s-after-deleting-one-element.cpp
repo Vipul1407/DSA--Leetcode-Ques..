26be3bd66faf2fc3236cc78456cd154cc0e966d1
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-24 17:48:09 +0530

    Time: 2 ms (29.51%) | Memory: 60 MB (85.53%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,5 +1,40 @@
 class Solution {
 public:
+    //METHOD-2
+    //Prefix Approach
+    int longestSubarray(vector<int>& nums) 
+    {
+        int curr=0,prev=-1,ans=0;
+        int n= nums.size();
+        for(auto i:nums)
+        {
+            if(i==1)
+            {
+                curr++;
+            }
+            else
+            {
+                if(prev==-1)
+                {
+                    ans= max(ans,curr);
+                }
+                else
+                {
+                    ans= max(ans,prev+curr);
+                }
+                prev= curr;
+                curr=0;
+            }
+        }
+        //update at last..
+        ans= max(ans,prev+curr);
+        return ans==n? ans-1:ans;
+    }
+};
+/*
+    //METHOD-1
+    //Sliding Window
+    // Same as Maximum Consecutive ones iii for K=1.
     int longestSubarray(vector<int>& nums) 
     {
         int k=1;
@@ -27,4 +62,4 @@ public:
         }
         return ans==n? ans-1:ans;
     }
-};
+*/
