e837aa4c2adc199476ce797d6246e29320c06626
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-24 16:30:00 +0530

    Time: 0 ms (100.00%) | Memory: 69.7 MB (12.81%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
+    //Slding Window==> Asked in Salescode.ai OA
     int longestOnes(vector<int>& nums, int k) 
     {
         int n= nums.size();
         int l=0,r=0,ans=0;
-
         while(r<n)
         {
             if(nums[r]==0)
             {
                 k--;
             }
-            if(l<r && k<0)//optimization...
-            //while(l<r && k<0)
+            while(l<r && k<0)
             {
                 if(nums[l]==0)
                 {
