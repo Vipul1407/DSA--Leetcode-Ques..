07a873e50fb3d595d4a50d147c2211461a32ecf4
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-25 15:22:09 +0530

    Time: 194 ms (7.82%) | Memory: 112.9 MB (11.63%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
-    //METHOD-1
+    //METHOD-2
+    //USING Unordered_Map Approach..
+    //TC= O(M*N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
+    {
+        vector<int>ans;
+        map<int,vector<int>>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums[i].size();j++)
+            {
+                mp[i+j].push_back(nums[i][j]);
+            }
+        }
+        int dia=0;
+        while(mp.find(dia)!=mp.end())
+        {
+            vector<int>vec= mp[dia];
+            reverse(vec.begin(),vec.end());
+            for(auto &j:vec)
+            {
+                ans.push_back(j);
+            }
+            dia++;
+        }
+        return ans;
+    }
+};
+/*
+    /METHOD-1
     //USING Map Approach..
     //TC= O(M*N*LOG(M+N))
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
@@ -26,4 +55,4 @@ public:
         }
         return ans;
     }
-};
+*/
