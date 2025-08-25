61ed3e3cc866add7cfc0179e24ccacb2c2fc8c91
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-25 11:33:20 +0530

    Time: 16 ms (42.53%) | Memory: 28.3 MB (31.51%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    //METHOD-1
+    //METHOD-3
     //BFS approach
+    //Only chnage is that we are pushing into ans earlier....
     //TC= O(M*N) since we are inserting every element if not want to insert then O(M+N)
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
     {
@@ -12,8 +13,7 @@ public:
         vector<vector<int>>vis(m,vector<int>(n,0));
         q.push({0,0});
         vis[0][0]=1;
-        ans.push_back(mat[0][0]);
-        bool flip= 1;
+        bool flip= 0;
 
         while(!q.empty())
         {
@@ -23,16 +23,15 @@ public:
             {
                 int x= q.front().first;
                 int y= q.front().second;
-            
+                vec.push_back(mat[x][y]);
+
                 if(x+1<m && vis[x+1][y]==0)
                 {
-                    vec.push_back(mat[x+1][y]);
                     q.push({x+1,y});
                     vis[x+1][y]=1;
                 } 
                 if(y+1<n && vis[x][y+1]==0)
                 {
-                    vec.push_back(mat[x][y+1]);
                     q.push({x,y+1});
                     vis[x][y+1]=1;
                 }
@@ -72,6 +71,57 @@ public:
         for(auto &i:mp)
         {
             vector<int>vec= i.second;
+            if(flip)
+            {
+                reverse(vec.begin(),vec.end());
+            }
+            for(auto &j:vec)
+            {
+                ans.push_back(j);
+            }
+            flip= !flip;
+        }
+        return ans;
+    }
+
+    //METHOD-2
+    //BFS approach
+    //TC= O(M*N) since we are inserting every element if not want to insert then O(M+N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        vector<int>ans;
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        q.push({0,0});
+        vis[0][0]=1;
+        ans.push_back(mat[0][0]);
+        bool flip= 1;
+
+        while(!q.empty())
+        {
+            vector<int>vec;
+            int size= q.size();
+            while(size--)
+            {
+                int x= q.front().first;
+                int y= q.front().second;
+            
+                if(x+1<m && vis[x+1][y]==0)
+                {
+                    vec.push_back(mat[x+1][y]);
+                    q.push({x+1,y});
+                    vis[x+1][y]=1;
+                } 
+                if(y+1<n && vis[x][y+1]==0)
+                {
+                    vec.push_back(mat[x][y+1]);
+                    q.push({x,y+1});
+                    vis[x][y+1]=1;
+                }
+                q.pop();
+            } 
             if(flip)
             {
                 reverse(vec.begin(),vec.end());
