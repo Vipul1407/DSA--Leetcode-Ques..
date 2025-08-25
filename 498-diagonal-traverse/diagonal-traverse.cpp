28c7f1c9a5ccaf072c12e1ab342365843e8997b0
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-25 15:55:18 +0530

    Time: 1 ms (63.98%) | Memory: 26.7 MB (35.43%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,36 +1,46 @@
 class Solution {
 public:
-    //METHOD-4
-    //Unordered_Map approach
-    //TC= O(M*N)
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
+    //METHOD-5
+    //BFS More approach...
+    //Only chnage is that we are pushing into ans earlier....
+    //TC= O(M*N) since we are inserting every element if not want to insert then O(M+N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
     {
-        int m= mat.size();
-        int n= mat[0].size();
-        unordered_map<int,vector<int>>mp;
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                mp[i+j].push_back(mat[i][j]);
-            }
-        }
         vector<int>ans;
-        bool flip= 1;
-        int dia=0;
-        while(mp.find(dia)!=mp.end())
+        int m= nums.size();
+        int n= nums[0].size();
+        bool flip= 0;
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        
+        while(q.size())
         {
-            vector<int>vec= mp[dia];
+            int size= q.size();
+            vector<int>vec;
+            while(size--)
+            {
+                int x= q.front().first;
+                int y= q.front().second;
+                vec.push_back(nums[x][y]);
+                if(y==0 && x+1<m)
+                {
+                    q.push({x+1,y});
+                }
+                if(y+1<n)
+                {
+                    q.push({x,y+1});
+                }
+                q.pop();
+            }
             if(flip)
             {
-                reverse(vec.begin(),vec.end());
+                reverse(begin(vec),end(vec));
             }
-            for(auto &j:vec)
+            for(auto i:vec)
             {
-                ans.push_back(j);
+                ans.push_back(i);
             }
-            flip= !flip;
-            dia++;
+            flip=!flip;
         }
         return ans;
     }
@@ -166,6 +176,41 @@ public:
                 ans.push_back(j);
             }
             flip= !flip;
+        }
+        return ans;
+    }
+
+    //METHOD-4
+    //Unordered_Map approach
+    //TC= O(M*N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        unordered_map<int,vector<int>>mp;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                mp[i+j].push_back(mat[i][j]);
+            }
+        }
+        vector<int>ans;
+        bool flip= 1;
+        int dia=0;
+        while(mp.find(dia)!=mp.end())
+        {
+            vector<int>vec= mp[dia];
+            if(flip)
+            {
+                reverse(vec.begin(),vec.end());
+            }
+            for(auto &j:vec)
+            {
+                ans.push_back(j);
+            }
+            flip= !flip;
+            dia++;
         }
         return ans;
     }
