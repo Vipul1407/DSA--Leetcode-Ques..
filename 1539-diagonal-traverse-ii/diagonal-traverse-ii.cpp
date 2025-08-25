fd6c513fa0447a3834aac55a4de0c4c4bfe3bab1
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-25 15:46:00 +0530

    Time: 15 ms (83.15%) | Memory: 70.1 MB (90.43%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    //METHOD-2
-    //USING Unordered_Map Approach..
-    //TC= O(M*N)
+    //METHOD-3 
+    //BFS approach
+    //Only chnage is that we are pushing into ans earlier....
+    //TC= O(M*N) since we are inserting every element if not want to insert then O(M+N)
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
     {
         vector<int>ans;
-        map<int,vector<int>>mp;
-        for(int i=0;i<nums.size();i++)
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        
+        while(q.size())
         {
-            for(int j=0;j<nums[i].size();j++)
+            int size= q.size();
+            while(size--)
             {
-                mp[i+j].push_back(nums[i][j]);
+                int x= q.front().first;
+                int y= q.front().second;
+                ans.push_back(nums[x][y]);
+                if(y==0 && x+1<nums.size())
+                {
+                    q.push({x+1,y});
+                }
+                if(y+1<nums[x].size())
+                {
+                    q.push({x,y+1});
+                }
+                q.pop();
             }
-        }
-        int dia=0;
-        while(mp.find(dia)!=mp.end())
-        {
-            vector<int>vec= mp[dia];
-            reverse(vec.begin(),vec.end());
-            for(auto &j:vec)
-            {
-                ans.push_back(j);
-            }
-            dia++;
         }
         return ans;
     }
@@ -52,6 +56,34 @@ public:
             {
                 ans.push_back(j);
             }
+        }
+        return ans;
+    }
+
+    //METHOD-2
+    //USING Unordered_Map Approach..
+    //TC= O(M*N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
+    {
+        vector<int>ans;
+        map<int,vector<int>>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums[i].size();j++)
+            {
+                mp[i+j].push_back(nums[i][j]);
+            }
+        }
+        int dia=0;
+        while(mp.find(dia)!=mp.end())
+        {
+            vector<int>vec= mp[dia];
+            reverse(vec.begin(),vec.end());
+            for(auto &j:vec)
+            {
+                ans.push_back(j);
+            }
+            dia++;
         }
         return ans;
     }
