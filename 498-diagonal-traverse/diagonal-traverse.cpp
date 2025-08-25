65e0fedc7f16977b73337a7b6bdae5d0f8133637
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-25 10:58:30 +0530

    Time: 17 ms (41.60%) | Memory: 28.2 MB (31.73%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,5 +1,60 @@
 class Solution {
 public:
+    //METHOD-1
+    //BFS approach
+    //TC= O(M*N) since we are inserting every element if not want to insert then O(M+N)
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        vector<int>ans;
+        queue<pair<int,int>>q;
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        q.push({0,0});
+        vis[0][0]=1;
+        ans.push_back(mat[0][0]);
+        bool flip= 1;
+
+        while(!q.empty())
+        {
+            vector<int>vec;
+            int size= q.size();
+            while(size--)
+            {
+                int x= q.front().first;
+                int y= q.front().second;
+            
+                if(x+1<m && vis[x+1][y]==0)
+                {
+                    vec.push_back(mat[x+1][y]);
+                    q.push({x+1,y});
+                    vis[x+1][y]=1;
+                } 
+                if(y+1<n && vis[x][y+1]==0)
+                {
+                    vec.push_back(mat[x][y+1]);
+                    q.push({x,y+1});
+                    vis[x][y+1]=1;
+                }
+                q.pop();
+            } 
+            if(flip)
+            {
+                reverse(vec.begin(),vec.end());
+            }
+            for(auto &j:vec)
+            {
+                ans.push_back(j);
+            }
+            flip= !flip;
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //Map approach
+    //TC= O(M*N*LOG(M+N))
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) 
     {
         int m= mat.size();
@@ -29,4 +84,4 @@ public:
         }
         return ans;
     }
-};
+*/
