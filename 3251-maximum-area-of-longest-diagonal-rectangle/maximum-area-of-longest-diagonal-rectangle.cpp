50ada83455929e179dafbe3dcbb11480248486e7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-08-26 12:17:11 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (8.78%) - LeetSync

--- 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -3,6 +3,28 @@ public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) 
     {
         int ans=0;
+        int maxdia=0;
+        for(auto i:dimensions)
+        {
+            int dialen= i[0]*i[0]+i[1]*i[1];
+            if(dialen>maxdia)
+            {
+                maxdia= dialen;
+                ans=i[0]*i[1];
+            }
+            else if(dialen==maxdia)
+            {
+                ans= max(ans,i[0]*i[1]);
+            }
+        }
+        return ans;
+    }
+};
+/*
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) 
+    {
+        int ans=0;
+        //float can also be used
         double maxdia=0;
         for(auto i:dimensions)
         {
@@ -19,4 +41,4 @@ public:
         }
         return ans;
     }
-};
+*/
