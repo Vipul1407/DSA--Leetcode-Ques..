87b71790e0b5d6f2e4817272710d828cbe14bc36
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-08 18:44:13 +0530

    Time: 787 ms (9.33%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -2,4 +2,4 @@
 -- METHOD-1.. SIMPLE WAY..
 -- select c.name as Customers from Customers c where c.id not in (select o.customerId from Orders o)
 -- METHOD-2.. LEFT JOIN..
-select c.name as Customers from Customers c left join Orders o on c.id= o.customerId where o.customerId is NULL
+select c.name as Customers from Customers c left join Orders o on c.id= o.customerId where o.id is NULL
