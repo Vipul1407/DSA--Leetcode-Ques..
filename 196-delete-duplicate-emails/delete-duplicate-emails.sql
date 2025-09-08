2960eb2c9f0ac5c91c8ca057bc932875e5a12855
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-08 22:58:30 +0530

    Time: 469 ms (99.90%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
--- METHOD-1 Using inner keyword..
+-- METHOD-1 Using inner Join..
 -- delete p from Person p inner join Person q on p.email=q.email and p.id>q.id
--- METHOD-2 Without using delete..
+-- METHOD-2 Using subquery..
 delete from Person where id not in (select id from (select min(id) as id from Person group by email) as id)
