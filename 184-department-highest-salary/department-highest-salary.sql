96c0636bb24950c3ad0a0b48db95bede099394df
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-09 16:19:15 +0530

    Time: 556 ms (99.08%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,2 +1,11 @@
 # Write your MySQL query statement below
-select b.name as Department, a.name as Employee, a.salary from Employee a left join Department b on a.departmentId = b.id where a.salary= (select max(salary) from Employee where departmentId = a.departmentId)
+-- METHOD-1
+-- Using left join + subquery..
+-- select b.name as Department, a.name as Employee, a.salary from Employee a left join Department b on a.departmentId = b.id where a.salary= (select max(salary) from Employee where departmentId = a.departmentId)
+
+-- METHOD-2
+-- Using left join + Window fun..
+select Department, Employee, Salary 
+from(
+select b.name as Department, a.name as Employee, a.salary, rank() over(partition by a.departmentId order by a.Salary desc) as rnk from Employee a left join Department b on a.departmentId = b.id)
+t where rnk=1;
