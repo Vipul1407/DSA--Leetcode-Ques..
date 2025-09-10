39390869d5036b2eed19b0f94f038073a52c5699
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-10 21:31:37 +0530

    Time: 671 ms (10.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select today.id from Weather today inner join Weather yesterday on DATEDIFF(today.recordDate,yesterday.recordDate)=1 and today.temperature > yesterday.temperature;
