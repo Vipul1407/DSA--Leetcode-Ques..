aa0432daf09996ebd44b7102659b491f7975c4a3
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-11 15:32:30 +0530

    Time: 26 ms (14.36%) | Memory: 16.8 MB (28.03%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,5 +1,37 @@
 class Solution {
 public:
+    //METHOD-2
+    //TC= O(N) Without Using Sorting..
+    string sortVowels(string s) 
+    {
+        vector<char>vow;
+        int n= s.size();
+        unordered_map<char,int>freq;
+        for(auto i:s)
+        {
+            freq[i]++;
+        }
+        string order= "AEIOUaeiou";
+        int idx=0;
+
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='a'|| s[i]=='e' || s[i]=='i' || s[i]=='o'|| s[i]=='u' || s[i]=='A'|| s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U')
+            {
+                while(freq[order[idx]]==0 && idx<order.size())
+                {
+                    idx++;
+                }
+                s[i]= order[idx];
+                freq[order[idx]]--;
+            }
+        }
+        return s;
+    }
+};
+/*
+    //METHOD-1
+    //TC= O(NLOGN) Using Sorting..
     string sortVowels(string s) 
     {
         vector<char>vow;
@@ -27,4 +59,4 @@ public:
         }
         return s;
     }
-};
+*/
