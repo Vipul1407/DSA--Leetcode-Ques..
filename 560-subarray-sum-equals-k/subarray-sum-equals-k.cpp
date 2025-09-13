8057c5bec4525be5a53f0a6115113c8d7fd959e6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-13 11:21:34 +0530

    Time: 56 ms (44.48%) | Memory: 51 MB (21.82%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,18 +1,43 @@
 class Solution {
 public:
-    //OPTIMIZED...
+    //METHOD-2
+    //OPTIMIZED..
     int subarraySum(vector<int>& nums, int k) 
     {
+        int ans=0, prefix=0;
+        int n= nums.size();
         unordered_map<int,int>mp;
         mp[0]=1;
-        int sum=0;
-        int ans=0;
-        for(auto i:nums)
+        for(int i=0;i<n;i++)
         {
-            sum+= i;
-            ans+= mp[sum-k];
-            mp[sum]++;
+            prefix+= nums[i];
+            ans+= mp[prefix-k];
+            mp[prefix]++;
         }
         return ans;
     }
 };
+/*
+    //METHOD-1
+    //BRUTE FORCE..
+    int subarraySum(vector<int>& nums, int k) 
+    {
+        int ans=0;
+        int n= nums.size();
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=i;j<n;j++)
+            {
+                sum+= nums[j];
+                if(sum==k)
+                {
+                    ans++;
+                    //we will not break as our nums may contains negative values
+                    //break;
+                }
+            }
+        }
+        return ans;
+    }
+*/
