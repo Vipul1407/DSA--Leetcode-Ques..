794ce8ae1d3f7f908a71c4c3734e23abb97d5776
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-15 19:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (95.02%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,5 +1,61 @@
 class Solution {
 public:
+    //METHOD-2
+    //Without using stringstream..
+    int canBeTypedWords(string text, string brokenLetters) 
+    {
+        vector<int>broken(26,0);
+        for(auto i:brokenLetters)
+        {
+            broken[i-'a']=1;
+        }
+        string str="";
+        int cnt=0;
+        int n= text.size();
+        int i=0;
+        while(i<n)
+        {
+            if(text[i]==' ')
+            {
+                int flag=0;
+                for(auto i:str)
+                {
+                    if(broken[i-'a']==1 && flag==0)
+                    {
+                        flag=1;
+                        break;
+                    }
+                }
+                if(flag==0)
+                {
+                    cnt++;
+                }
+                str="";//reset str..
+            }
+            else
+            {
+                str+= text[i];
+            }
+            i++;
+        }
+        //same thing must be done after loop also..
+        int flag=0;
+        for(auto i:str)
+        {
+            if(broken[i-'a']==1 && flag==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            cnt++;
+        }
+        return cnt;
+    }
+};
+/*
     //METHOD-1
     //Using Stringstream..
     int canBeTypedWords(string text, string brokenLetters) 
@@ -31,4 +87,4 @@ public:
         }
         return cnt;
     }
-};
+*/
