1b73b31635fc20147eed40cfb6afbb9f1685b4b7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-21 17:49:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (27.29%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -3,41 +3,37 @@ public:
     stack<int>s1;
     stack<int>s2;
     MyQueue() {
-        
     }
     
     void push(int x) 
     {
         //s1-->s2
-        //s1 me push kr
+        //push in s1
         //s2-->s1
-        while(!s1.empty())
+        while(s1.size())
         {
             s2.push(s1.top());
             s1.pop();
         }
         s1.push(x);
-        while(!s2.empty())
+        while(s2.size())
         {
             s1.push(s2.top());
             s2.pop();
         }
     }
     
-    int pop() 
-    {
+    int pop() {
         int val= s1.top();
         s1.pop();
         return val;
     }
     
-    int peek() 
-    {
+    int peek() {
         return s1.top();
     }
     
-    bool empty() 
-    {
+    bool empty() { 
         return s1.empty();
     }
 };
