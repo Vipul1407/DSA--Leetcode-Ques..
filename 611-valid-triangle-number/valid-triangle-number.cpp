9e2a442a0c044715844c5c1fcaceb3226ec66b39
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-26 23:22:56 +0530

    Time: 23 ms (98.13%) | Memory: 16.4 MB (99.90%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,5 +1,59 @@
 class Solution {
 public:
+    //METHOD-3
+    //OPTIMIZED USING 1 LOOP + 2 POINTERS..
+    //TC= O(N^3)
+    int triangleNumber(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int ans=0;
+        sort(begin(nums),end(nums));
+        
+        for(int k=n-1;k>=2;k--)
+        {
+            int i=0;
+            int j=k-1;
+            while(i<j)
+            {
+                int sum= nums[i]+nums[j];
+                if(sum>nums[k])
+                {
+                    ans+= j-i;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //BRUTE FORCE..
+    //TC= O(N^3)
+    int triangleNumber(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int ans=0;
+        for(int i=0;i<=n-3;i++)
+        {
+            for(int j=i+1;j<=n-2;j++)
+            {
+                for(int k=j+1;k<=n-1;k++)
+                {
+                    if(nums[i]+nums[j]>nums[k])
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
     //METHOD-2
     //BINARY SEARCH..
     //TC= O(N^2 * LOGN)
@@ -32,30 +86,6 @@ public:
             for(int j=i+1;j<=n-2;j++)
             {
                 ans+= binary(i,j,nums);
-            }
-        }
-        return ans;
-    }
-};
-/*
-    //METHOD-1
-    //BRUTE FORCE..
-    //TC= O(N^3)
-    int triangleNumber(vector<int>& nums) 
-    {
-        int n= nums.size();
-        int ans=0;
-        for(int i=0;i<=n-3;i++)
-        {
-            for(int j=i+1;j<=n-2;j++)
-            {
-                for(int k=j+1;k<=n-1;k++)
-                {
-                    if(nums[i]+nums[j]>nums[k])
-                    {
-                        ans++;
-                    }
-                }
             }
         }
         return ans;
