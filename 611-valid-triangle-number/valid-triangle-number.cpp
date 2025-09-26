254cea91cd01d0db6b42781c54592e5b4c6ba87f
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-26 22:25:51 +0530

    Time: 147 ms (22.86%) | Memory: 16.7 MB (42.88%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    //METHOD-2
+    //BINARY SEARCH..
+    //TC= O(N^2 * LOGN)
+    int binary(int i, int j, vector<int>& nums)
+    {
+        int sum= nums[i]+nums[j];
+        int l= j+1;
+        int r= nums.size()-1;
+        while(l<=r)
+        {
+            int mid= l+(r-l)/2;
+            if(sum>nums[mid])
+            {
+                l= mid+1;//to increase the size..
+            }
+            else
+            {
+                r= mid-1;
+            }
+        }
+        return r-j;
+    }
+    int triangleNumber(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int ans=0;
+        sort(begin(nums),end(nums));
+        for(int i=0;i<=n-3;i++)
+        {
+            for(int j=i+1;j<=n-2;j++)
+            {
+                ans+= binary(i,j,nums);
+            }
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //BRUTE FORCE..
+    //TC= O(N^3)
+    int triangleNumber(vector<int>& nums) 
+    {
+        int n= nums.size();
+        int ans=0;
+        for(int i=0;i<=n-3;i++)
+        {
+            for(int j=i+1;j<=n-2;j++)
+            {
+                for(int k=j+1;k<=n-1;k++)
+                {
+                    if(nums[i]+nums[j]>nums[k])
+                    {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+*/
