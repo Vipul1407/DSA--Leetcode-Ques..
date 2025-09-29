dcb9b3f028bb7d828c4461d1b3185830d1589550
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-29 14:42:27 +0530

    Time: 2 ms (66.77%) | Memory: 11.1 MB (70.70%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -1,5 +1,49 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA APPROACH..
+    int minScoreTriangulation(vector<int>& val) 
+    {
+        int n= val.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int ans= INT_MAX;
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=i+2;j<n;j++)
+            {
+                int ans= INT_MAX;
+                for(int k=i+1;k<j;k++)
+                {
+                    ans= min(ans,val[i]*val[j]*val[k] + dp[i][k] + dp[k][j]);
+                }
+                dp[i][j]= ans;
+            }
+        }
+        return dp[0][n-1];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE APPROACH..
+    int rec(int i,int j,vector<int>& val)
+    {
+        if(i+1==j)
+        {
+            return 0;
+        }
+        int ans= INT_MAX;
+        for(int k=i+1;k<j;k++)
+        {
+            ans= min(ans,val[i]*val[j]*val[k] + rec(i,k,val) + rec(k,j,val));
+        }
+        return ans;
+    }
+    int minScoreTriangulation(vector<int>& val) 
+    {
+        int n= val.size();
+        return rec(0,n-1,val);
+    }
+
     //METHOD-2
     //TDA APPROACH..
     int tda(int i,int j,vector<int>& val, vector<vector<int>>&dp)
@@ -24,27 +68,5 @@ public:
         int n= val.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
         return tda(0,n-1,val,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE APPROACH..
-    int rec(int i,int j,vector<int>& val)
-    {
-        if(i+1==j)
-        {
-            return 0;
-        }
-        int ans= INT_MAX;
-        for(int k=i+1;k<j;k++)
-        {
-            ans= min(ans,val[i]*val[j]*val[k] + rec(i,k,val) + rec(k,j,val));
-        }
-        return ans;
-    }
-    int minScoreTriangulation(vector<int>& val) 
-    {
-        int n= val.size();
-        return rec(0,n-1,val);
     }
 */
