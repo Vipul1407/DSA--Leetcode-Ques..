148518ccfef5e8926deff7b4ba6c44a606823d6e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-30 16:03:30 +0530

    Time: 47 ms (12.86%) | Memory: 85.8 MB (28.37%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    //METHOD-2
+    //2 PASS ONLY + STACK..
+    int largestRectangleArea(vector<int>& heights) 
+    {
+        int n= heights.size();
+
+        //next smaller..
+        vector<int>nse(n,n);
+        stack<int>st2;
+        st2.push(n);
+        for(int i=n-1;i>=0;i--)
+        {
+            while(st2.top()!=n && heights[st2.top()]>=heights[i])
+            {
+                st2.pop();
+            }
+            nse[i]= st2.top();
+            st2.push(i);
+        }
+
+        //prev smaller + finding ans....
+        vector<int>pse(n,-1);
+        stack<int>st1;
+        st1.push(-1);
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            while(st1.top()!=-1 && heights[st1.top()]>=heights[i])
+            {
+                st1.pop();
+            }
+            pse[i]= st1.top();
+            st1.push(i);
+            ans= max(ans, heights[i]* (nse[i]-pse[i]-1));
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //BRUTE FORCE..
     //3 PASS + STACK..
@@ -43,4 +82,4 @@ public:
         }
         return ans;
     }
-};
+*/
