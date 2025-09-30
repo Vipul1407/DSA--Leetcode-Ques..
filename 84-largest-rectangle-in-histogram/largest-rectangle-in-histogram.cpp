a48a2ecd01a1cf76ed2abde3e353c0d38b238e4c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-09-30 15:59:56 +0530

    Time: 54 ms (9.23%) | Memory: 85.8 MB (37.92%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,89 +1,46 @@
 class Solution {
 public:
-    //METHOD-2
-    //OPTIMIZED...
+    //METHOD-1
+    //BRUTE FORCE..
+    //3 PASS + STACK..
     int largestRectangleArea(vector<int>& heights) 
     {
         int n= heights.size();
-        vector<int>prevsmallidx(n,-1);
-        stack<int>st;
+
+        //prev smaller..
+        vector<int>pse(n,-1);
+        stack<int>st1;
+        st1.push(-1);
         for(int i=0;i<n;i++)
         {
-            while(st.size() && heights[st.top()]>=heights[i])
+            while(st1.top()!=-1 && heights[st1.top()]>=heights[i])
             {
-                st.pop();
+                st1.pop();
             }
-            if(!st.empty())
-            {
-                prevsmallidx[i]= st.top();
-            }
-            st.push(i);//placing idx only..
+            pse[i]= st1.top();
+            st1.push(i);
         }
 
+        //next smaller..
+        vector<int>nse(n,n);
         stack<int>st2;
-        int ans=0;
-        int nextsmallidx= n;
+        st2.push(n);
         for(int i=n-1;i>=0;i--)
         {
-            while(st2.size() && heights[st2.top()]>=heights[i])
+            while(st2.top()!=n && heights[st2.top()]>=heights[i])
             {
                 st2.pop();
             }
-            if(!st2.empty())
-            {
-                nextsmallidx= st2.top();
-            }
-            else
-            {
-                nextsmallidx= n;
-            }
-            st2.push(i);//placing idx only..
-            ans= max(ans,heights[i]*(nextsmallidx-prevsmallidx[i]-1));
+            nse[i]= st2.top();
+            st2.push(i);
+        }
+
+        //finding ans..
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans= max(ans, heights[i]* (nse[i]-pse[i]-1));
         }
         return ans;
     }
 };
-/*
-    //METHOD-1
-    //BRUTE FORCE..
-    int largestRectangleArea(vector<int>& heights) 
-    {
-        int n= heights.size();
-        vector<int>prevsmallidx(n,-1);
-        stack<int>st;
-        for(int i=0;i<n;i++)
-        {
-            while(st.size() && heights[st.top()]>=heights[i])
-            {
-                st.pop();
-            }
-            if(!st.empty())
-            {
-                prevsmallidx[i]= st.top();
-            }
-            st.push(i);//placing idx only..
-        }
-
-        vector<int>nextsmallidx(n,n);
-        stack<int>st2;
-        for(int i=n-1;i>=0;i--)
-        {
-            while(st2.size() && heights[st2.top()]>=heights[i])
-            {
-                st2.pop();
-            }
-            if(!st2.empty())
-            {
-                nextsmallidx[i]= st2.top();
-            }
-            st2.push(i);//placing idx only..
-        }
-
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            ans= max(ans,heights[i]*(nextsmallidx[i]-prevsmallidx[i]-1));
-        }
-        return ans;
-    }
-*/
