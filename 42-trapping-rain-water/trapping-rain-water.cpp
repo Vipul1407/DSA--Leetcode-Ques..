17363b060587318c4834dfd75f278f740ee09402
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-10-03 15:23:06 +0530

    Time: 3 ms (18.02%) | Memory: 27.4 MB (19.64%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,92 +1,31 @@
 class Solution {
 public:
-    //METHOD-3
-    //2 POINTERS..
+    //METHOD-1
+    //BRUTE FORCE..
     int trap(vector<int>& height) 
     {
         int n= height.size();
-        int ans=0,l=0,r=n-1;
-        int lmax=0,rmax=0;
-        while(l<r)
+        vector<int>rightmax(n,0);
+        vector<int>leftmax(n,0);
+        leftmax[0]= height[0];
+        rightmax[n-1]= height[n-1];
+        for(int i=1;i<n;i++)
         {
-            if(height[l]<=height[r])
+            leftmax[i]= max(height[i],leftmax[i-1]);
+        }
+        for(int i=n-2;i>=0;i--)
+        {
+            rightmax[i]= max(height[i],rightmax[i+1]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int mini= min(leftmax[i],rightmax[i]);
+            if(mini>height[i])
             {
-                if(lmax>height[l])
-                {
-                    ans+= lmax-height[l];
-                }
-                else
-                {
-                    lmax= max(lmax,height[l]);
-                }
-                l++;
-            }
-            else
-            {
-                if(rmax>height[r])
-                {
-                    ans+= rmax-height[r];
-                }
-                else
-                {
-                    rmax= max(rmax,height[r]);
-                }
-                r--;
+                ans+= mini-height[i];
             }
         }
         return ans;
     }
 };
-/*
-    //METHOD-1
-    //PREFIX METHOD USING 2VECTORS..
-    int trap(vector<int>& height) 
-    {
-        int n= height.size();
-        vector<int>leftmax(n,0);
-        leftmax[0]= height[0];
-        vector<int>rightmax(n,0);
-        rightmax[n-1]= height[n-1];
-
-        for(int i=1;i<n;i++)
-        {
-            leftmax[i]= max(leftmax[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            rightmax[i]= max(rightmax[i+1],height[i]);
-        }
-
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            int val= min(rightmax[i],leftmax[i]);
-            ans+= max(0,val-height[i]);
-        }
-        return ans;
-    }
-
-    //METHOD-2
-    //PREFIX METHOD USING 1VECTORS..
-    int trap(vector<int>& height) 
-    {
-        int n= height.size();
-        vector<int>leftmax(n,0);
-        leftmax[0]= height[0];
-    
-        for(int i=1;i<n;i++)
-        {
-            leftmax[i]= max(leftmax[i-1],height[i]);
-        }
-
-        int ans=0;
-        int rightmax=0;
-        for(int i=n-1;i>=0;i--)
-        {
-            int val= min(leftmax[i],rightmax);
-            ans+= max(0,val-height[i]);
-            rightmax= max(rightmax,height[i]);
-        }
-        return ans;
-    }
-*/
