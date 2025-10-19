91cb29c2162fc4d52b750df7cfd17bf14bcc502e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-10-19 22:28:16 +0530

    Time: 183 ms (72.57%) | Memory: 86.7 MB (54.87%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,5 +1,51 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    //TC= n*(10*n)= n^2 since 10 additions can be applied to a single digit hence 10n and outer n for rotation
+    //SC= n^2
+    void rotate(string &s, int b)
+    {
+        reverse(begin(s),end(s));
+        reverse(begin(s),begin(s)+b);
+        reverse(begin(s)+b,end(s));
+    }
+    string findLexSmallestString(string s, int a, int b) 
+    {
+        unordered_set<string>st;
+        string ans= s;
+        queue<string>q;
+        q.push(s);
+        st.insert(s);
+        while(q.size())
+        {
+            auto temp= q.front();
+            ans= min(ans,temp);
+            q.pop();
+            //adding a to odd indexes..
+            string curr1= temp;
+            for(int i=1;i<s.size();i+=2)
+            {
+                curr1[i]= (curr1[i]-'0'+a)%10 +'0';
+            }
+            if(!st.count(curr1))
+            {
+                q.push(curr1);
+                st.insert(curr1);
+            }
+            //rotate by b..
+            string curr2= temp;
+            rotate(curr2,b);
+            if(!st.count(curr2))
+            {
+                q.push(curr2);
+                st.insert(curr2);
+            }
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //DFS..
     //TC= n*(10*n)= n^2 since 10 additions can be applied to a single digit hence 10n and outer n for rotation
@@ -39,4 +85,4 @@ public:
         dfs(s,ans,a,b,st);
         return ans;
     }
-};
+*/
