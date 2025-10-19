c78037b3966dfd4aed685badd38f10b6b71182bd
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-10-19 19:49:29 +0530

    Time: 256 ms (36.28%) | Memory: 78.4 MB (65.93%) - LeetSync

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    //METHOD-1
+    //DFS..
+    //TC= n*(10*n)= n^2 since 10 additions can be applied to a single digit hence 10n and outer n for rotation
+    //SC= n^2
+    void rotate(string &s, int b)
+    {
+        reverse(begin(s),end(s));
+        reverse(begin(s),begin(s)+b);
+        reverse(begin(s)+b,end(s));
+    }
+    void dfs(string s, string &ans, int &a, int &b, unordered_set<string>&st)
+    {
+        st.insert(s);
+        ans= min(ans,s);
+        //adding a to odd indexes..
+        string curr1= s;
+        for(int i=1;i<s.size();i+=2)
+        {
+            curr1[i]= (curr1[i]-'0'+a)%10 +'0';
+        }
+        if(!st.count(curr1))
+        {
+            dfs(curr1,ans,a,b,st);
+        }
+        //rotate by b..
+        string curr2= s;
+        rotate(curr2,b);
+        if(!st.count(curr2))
+        {
+            dfs(curr2,ans,a,b,st);
+        }
+    }
+    string findLexSmallestString(string s, int a, int b) 
+    {
+        unordered_set<string>st;
+        string ans= s;
+        dfs(s,ans,a,b,st);
+        return ans;
+    }
+};
