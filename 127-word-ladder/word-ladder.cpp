0bd166cefaf494535156a195f9b399c0130f500e
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-10-21 21:40:00 +0530

    Time: 55 ms (71.08%) | Memory: 21.3 MB (46.89%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,8 +1,57 @@
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    //TC= O(N*D)
+    //No vis set..
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
+    {
+        unordered_set<string>st(begin(wordList),end(wordList));
+        if(!st.count(endWord))
+        {
+            return 0;
+        }
+        unordered_set<string>vis;
+        queue<pair<string,int>>q;
+        q.push({beginWord,1});
+        st.erase(beginWord);
+        int ans= INT_MAX;
+        while(q.size())
+        {
+            int n= q.size();
+            while(n--)
+            {
+                string curr= q.front().first;
+                int cnt= q.front().second;
+                q.pop();
+                if(curr==endWord)
+                {
+                    // ans= min(ans,cnt);
+                    ans=cnt;
+                }
+                for(int i=0;i<curr.size();i++)
+                {
+                    string temp= curr;
+                    for(char ch='a';ch<='z';ch++)
+                    {
+                        temp[i]=ch;
+                        if(st.count(temp))
+                        {
+                            q.push({temp,cnt+1});
+                            st.erase(temp);
+                        }
+                    }
+                }
+            }
+        }
+        return ans==INT_MAX? 0:ans;
+    }
+};
+/*
     //METHOD-1
     //BFS..
     //TC= O(N*D)
+    //Using vis set
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) 
     {
         unordered_set<string>st(begin(wordList),end(wordList));
@@ -44,4 +93,4 @@ public:
         }
         return ans==INT_MAX? 0:ans;
     }
-};
+*/
