6820c352368fafe016b944a8d361b982067a201a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-05 08:56:09 +0530

    Time: 234 ms (96.15%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,17 +1,4 @@
 # Write your MySQL query statement below
-# METHOD-3
-select(
-select distinct salary from Employee e1 where 1= (
-    select count(distinct salary) from Employee e2 where e1.salary<e2.salary 
-)
-) as SecondHighestSalary
-/*
+
 # METHOD-1
-select distinct max(salary) as SecondHighestSalary  from Employee where salary not IN (
-    select distinct max(salary) from Employee
-)
-# METHOD-2
-select(
-select distinct salary from Employee order by salary desc limit 1 offset 1
-) as SecondHighestSalary
-*/
+select max(salary) as SecondHighestSalary from Employee where salary not in (select distinct max(salary) from Employee);
