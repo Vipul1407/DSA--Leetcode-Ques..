c4a9efb532559ead32fcaff5bb885d5d798c37ed
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-05 10:10:35 +0530

    Time: 671 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -4,4 +4,7 @@
 -- select max(salary) as SecondHighestSalary from Employee where salary not in (select distinct max(salary) from Employee);
 
 # METHOD-2
-select( select distinct salary from Employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
+-- select( select distinct salary from Employee order by salary desc limit 1 offset 1) as SecondHighestSalary;
+
+# METHOD-3
+select (select distinct salary from Employee e1 where 1= (select count(distinct salary) from Employee e2 where e2.salary>e1.salary )) as SecondHighestSalary
