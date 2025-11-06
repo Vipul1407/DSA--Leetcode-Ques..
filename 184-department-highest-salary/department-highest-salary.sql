fcdcfc504be0d3e1234a4c522b9958b037312b57
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-06 15:44:58 +0530

    Time: 672 ms (52.55%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -4,8 +4,8 @@
 -- select b.name as Department, a.name as Employee, a.salary from Employee a left join Department b on a.departmentId = b.id where a.salary= (select max(salary) from Employee where departmentId = a.departmentId)
 
 -- METHOD-2
--- Using left join + Window fun..
+-- Using inner join + Window fun..
 select Department, Employee, Salary 
 from(
-select b.name as Department, a.name as Employee, a.salary, rank() over(partition by a.departmentId order by a.Salary desc) as rnk from Employee a left join Department b on a.departmentId = b.id)
+select b.name as Department, a.name as Employee, a.salary, rank() over(partition by a.departmentId order by a.Salary desc) as rnk from Employee a inner join Department b on a.departmentId = b.id)
 t where rnk=1;
