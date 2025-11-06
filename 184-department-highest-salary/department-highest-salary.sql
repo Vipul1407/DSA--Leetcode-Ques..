fb0da68e0ae8d67b7c83af32a141f1941297e8e7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-06 16:02:17 +0530

    Time: 872 ms (39.04%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -5,7 +5,14 @@
 
 -- METHOD-2
 -- Using inner join + Window fun..
-select Department, Employee, Salary 
-from(
-select b.name as Department, a.name as Employee, a.salary, rank() over(partition by a.departmentId order by a.Salary desc) as rnk from Employee a inner join Department b on a.departmentId = b.id)
-t where rnk=1;
+-- select Department, Employee, Salary 
+-- from(
+-- select b.name as Department, a.name as Employee, a.salary, rank() over(partition by a.departmentId order by a.Salary desc) as rnk from Employee a inner join Department b on a.departmentId = b.id)
+-- t where rnk=1;
+
+# METHOD-3
+-- Using inner join + Window fun.. storing in temptable..
+# can use rank or dense rank anything since we have to find the highest one only..
+with cte as (select d.name as Department, e.name as Employee, e.salary as Salary, rank() over (partition by e.departmentId order by e.salary desc) as rnk from Employee e inner join Department d on e.departmentId = d.id
+)
+select Department, Employee, Salary from cte where rnk=1;
