38e0b8db9f691264390d549ae833f959c9156ff6
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-11 12:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (38.15%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,27 +1,75 @@
 class Solution {
- public:
-  int minPathSum(vector<vector<int>>& grid) {
-    const int m = grid.size();
-    const int n = grid[0].size();
-
-    // Update the first row
-    for (int j = 1; j < n; ++j) {
-      grid[0][j] += grid[0][j - 1];
+public:
+    //METHOD-2
+    //TDA..
+    int rec(int i,int j,vector<vector<int>>& grid, vector<vector<int>>&dp)
+    {
+        int m= grid.size();
+        int n= grid[0].size();
+        if(i==m-1 && j==n-1)
+        {
+            return grid[i][j];
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int ans= INT_MAX;
+        //last row=> can go only right.. j+1
+        if(i==m-1)
+        {
+            ans= min(ans, rec(i,j+1,grid,dp));
+        }
+        //last col=> can gol only down.. i+1
+        else if(j==n-1)
+        {
+            ans= min(ans, rec(i+1,j,grid,dp));
+        }
+        else
+        {
+            //take min of right or down..
+            ans= min(ans,min(rec(i+1,j,grid,dp),rec(i,j+1,grid,dp)));
+        }
+        return dp[i][j]= grid[i][j]+ans;//addign curr grid element to ans..
     }
-
-    // Update the first column
-    for (int i = 1; i < m; ++i) {
-      grid[i][0] += grid[i - 1][0];
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+        int m= grid.size();
+        int n= grid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return rec(0,0,grid,dp);
     }
-
-    // Update the rest of the grid
-    for (int i = 1; i < m; ++i) {
-      for (int j = 1; j < n; ++j) {
-        grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
-      }
-    }
-
-    // Return the bottom-right corner value which is the answer
-    return grid[m - 1][n - 1];
-  }
 };
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,int j,vector<vector<int>>& grid)
+    {
+        int m= grid.size();
+        int n= grid[0].size();
+        if(i==m-1 && j==n-1)
+        {
+            return grid[i][j];
+        }
+        int ans= INT_MAX;
+        //last row=> can go only right.. j+1
+        if(i==m-1)
+        {
+            ans= min(ans, rec(i,j+1,grid));
+        }
+        //last col=> can gol only down.. i+1
+        else if(j==n-1)
+        {
+            ans= min(ans, rec(i+1,j,grid));
+        }
+        else
+        {
+            ans= min(ans,min(rec(i+1,j,grid),rec(i,j+1,grid)));
+        }
+        return grid[i][j]+ans;
+    }
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+        return rec(0,0,grid);
+    }
+*/
