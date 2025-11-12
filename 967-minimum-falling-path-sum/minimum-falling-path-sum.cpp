6aa829f14cc0d215617c4d7d01ad5b40deb4036c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-11-12 15:12:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (60.56%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,143 +1,77 @@
 class Solution {
 public:
- 
+    //METHOD-2
+    //TDA..
+    int rec(int i,int j,vector<vector<int>>& mat,vector<vector<int>>&dp)
+    {
+        int m= mat.size();
+        int n= mat[0].size();
+        if(i==m-1)
+        {
+            return mat[i][j];
+        }
+        if(dp[i][j]!=INT_MAX)
+        {
+            return dp[i][j];
+        }
+        int ans= INT_MAX;
+        for(int shift=-1;shift<=1;shift++)
+        {
+            int newj= j+shift;
+            if(newj>=0 && newj<n)
+            {
+                ans= min(ans,rec(i+1,newj,mat,dp));
+            }
+        }
+        return dp[i][j]= mat[i][j]+ans;
+    }
     int minFallingPathSum(vector<vector<int>>& mat) 
     {
         int m= mat.size();
         int n= mat[0].size();
-        vector<int>next(n,1e5);
-
-        // if(i>=m)
-        // {
-        //     return 0;
-        // }
+        vector<vector<int>>dp(m,vector<int>(n,INT_MAX));
+        int ans= INT_MAX;
+        //starting from 1st row..
         for(int j=0;j<n;j++)
         {
-            next[j]= mat[m-1][j];
-        }
-        // if(j>=n || j<0)
-        // {
-        //     return 1e5;
-        // }
-        //Already handled by initializing dp table by 1e5
-
-        //starting last second row as we have alreay used i=m-1 row to fill dp in base case..
-        for(int i=m-2;i>=0;i--)
-        {
-            vector<int>curr(n,1e5);
-            for(int j=n-1;j>=0;j--)
-            {
-                if(j-1>=0 && i+1<m)
-                {
-                    curr[j]= min(curr[j], mat[i][j]+ next[j-1]);
-                }
-                if(i+1<m)
-                {
-                    curr[j]= min(curr[j], mat[i][j]+ next[j]);
-                }
-                if(i+1<m && j+1<n)
-                {
-                    curr[j]= min(curr[j], mat[i][j]+ next[j+1]);
-                }
-            }
-            next=curr;
-        }
-        int ans=INT_MAX;
-        //starting form any cell in 1st row..
-        for(int j=0;j<n;j++)
-        {
-            ans= min(ans, next[j]);
+            ans= min(ans,rec(0,j,mat,dp));
         }
         return ans;
     }
 };
-
 /*
-// TDA APPROACH
-// TLE.. \U0001f43b\U0001f43b\U0001f43b\U0001f43b
-int ans = INT_MAX;
-int rec(int i, int j, vector<vector<int>> &mat, vector<vector<int>> &dp)
-{
-    int m = mat.size();
-    int n = mat[0].size();
-    // becoz we are traversing row-wise..
-    if (i >= m)
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,int j,vector<vector<int>>& mat)
     {
-        return 0;
-    }
-    if (j >= n || j < 0)
-    {
-        return 1e5;
-    }
-    if (dp[i][j] != -1)
-    {
-        return dp[i][j];
-    }
-    int left = mat[i][j] + rec(i + 1, j - 1, mat, dp);
-    int down = mat[i][j] + rec(i + 1, j, mat, dp);
-    int right = mat[i][j] + rec(i + 1, j + 1, mat, dp);
-    return dp[i][j] = min(left, min(down, right));
-}
-int minFallingPathSum(vector<vector<int>> &mat)
-{
-    int m = mat.size();
-    int n = mat[0].size();
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    // starting form any cell in 1st row..
-    for (int j = 0; j < n; j++)
-    {
-        ans = min(ans, rec(0, j, mat, dp));
-    }
-    return ans;
-}
-
-// BUA APPROACH
-// BEATS 32%
-int minFallingPathSum(vector<vector<int>> &mat)
-{
-    int m = mat.size();
-    int n = mat[0].size();
-    vector<vector<int>> dp(m, vector<int>(n, 1e9));
-
-    // if(i>=m)
-    // {
-    //     return 0;
-    // }
-    for (int j = 0; j < n; j++)
-    {
-        dp[m - 1][j] = mat[m - 1][j];
-    }
-    // if(j>=n || j<0)
-    // {
-    //     return 1e5;
-    // }
-    // Already handled by initializing dp table by 1e5
-
-    for (int i = m - 1; i >= 0; i--)
-    {
-        for (int j = n - 1; j >= 0; j--)
+        int m= mat.size();
+        int n= mat[0].size();
+        //last row reached..
+        if(i==m-1)
         {
-            if (j - 1 >= 0 && i + 1 < m)
+            return mat[i][j];
+        }
+        int ans= INT_MAX;
+        for(int shift=-1;shift<=1;shift++)
+        {
+            int newj= j+shift;
+            if(newj>=0 && newj<n)
             {
-                dp[i][j] = min(dp[i][j], mat[i][j] + dp[i + 1][j - 1]);
-            }
-            if (i + 1 < m)
-            {
-                dp[i][j] = min(dp[i][j], mat[i][j] + dp[i + 1][j]);
-            }
-            if (i + 1 < m && j + 1 < n)
-            {
-                dp[i][j] = min(dp[i][j], mat[i][j] + dp[i + 1][j + 1]);
+                ans= min(ans,rec(i+1,newj,mat));
             }
         }
+        return mat[i][j]+ ans;
     }
-    int ans = INT_MAX;
-    // starting form any cell in 1st row..
-    for (int j = 0; j < n; j++)
+    int minFallingPathSum(vector<vector<int>>& mat) 
     {
-        ans = min(ans, dp[0][j]);
+        int m= mat.size();
+        int n= mat[0].size();
+        int ans= INT_MAX;
+        //starting from 1st row..
+        for(int j=0;j<n;j++)
+        {
+            ans= min(ans,rec(0,j,mat));
+        }
+        return ans;
     }
-    return ans;
-}
-
 */
