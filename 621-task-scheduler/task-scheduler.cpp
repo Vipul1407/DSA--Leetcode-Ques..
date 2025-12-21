be6b533f416d088fde761c9e9035c8a33525e8d2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-21 22:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 38.2 MB (82.33%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+    //METHOD-2
+    //GREEDY
+    int leastInterval(vector<char>& tasks, int n) 
+    {
+        vector<int>vec(26,0);
+        for(auto i:tasks)
+        {
+            vec[i-'A']++;
+        }
+        sort(begin(vec),end(vec));
+        int gadhe= vec[25]-1;
+        int idle= gadhe*n;
+        for(int i=24;i>=0;i--)
+        {
+            if(vec[i]==0)
+            {
+                continue;
+            }
+            idle-= min(vec[i],gadhe);
+        }
+        if(idle>0)
+        {
+            return tasks.size()+idle;
+        }
+        return tasks.size();
+    }
+};
+/*
+    //METHOD-1
+    //PRIORITY_QUEUE
     int leastInterval(vector<char>& tasks, int n) 
     {
         priority_queue<int>pq;
@@ -48,4 +78,4 @@ public:
         }
         return ans;
     }
-};
+*/
