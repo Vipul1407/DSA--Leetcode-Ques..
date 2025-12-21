a076752ee0fef13233439a0a25794c01d75ad011
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-21 22:03:37 +0530

    Time: 39 ms (31.13%) | Memory: 47.8 MB (17.81%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,67 +1,31 @@
 class Solution {
 public:
-    //METHOD-2
-    //WITHOUT USING HEAP..
-    //TC= O(N) assuming sorting in O(1)
     int leastInterval(vector<char>& tasks, int n) 
     {
-        vector<int>freq(26,0);
-        for(auto i:tasks)
-        {
-            freq[i-'A']++;
-        }
-        sort(begin(freq),end(freq));
-        int gadhe= freq[25]-1;
-        int idle= gadhe*n;
-        for(int i=24;i>=0;i--)
-        {
-            if(freq[i]==0)
-            {
-                continue;
-            }
-            idle-= min(gadhe,freq[i]);
-        }
-        if(idle>0)
-        {
-            return tasks.size()+idle;
-        }
-        else
-        {
-            return tasks.size();
-        }
-    }
-};
-/*
-    //METHOD-1
-    //MAX HEAP..
-    //TC= O(N*LOG(26))
-    int leastInterval(vector<char>& tasks, int n) 
-    {
-        vector<int>freq(26,0);
-        for(auto i:tasks)
-        {
-            freq[i-'A']++;
-        }
         priority_queue<int>pq;
-        for(auto i:freq)
+        int ans=0;
+        vector<int>vec(26,0);
+        for(auto i:tasks)
         {
-            if(i>0)
+            vec[i-'A']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(vec[i]>0)
             {
-                pq.push(i);
+                pq.push(vec[i]);
             }
         }
-        int time=0;
         while(pq.size())
         {
             vector<int>temp;
-            //run loop n+1 times..
             for(int i=0;i<=n;i++)
             {
                 if(pq.size())
                 {
-                    int cnt= pq.top();
-                    temp.push_back(--cnt);
+                    int freq= pq.top();
                     pq.pop();
+                    temp.push_back(--freq);
                 }
                 else
                 {
@@ -71,23 +35,17 @@ public:
             for(auto i:temp)
             {
                 if(i>0)
-                {
                     pq.push(i);
-                }
-                else
-                {
-                    break;
-                }
             }
-            if(pq.size())
+            if(pq.empty())
             {
-                time+= n+1;
+                ans+= temp.size();
             }
             else
             {
-                time+= temp.size();
+                ans+= n+1;
             }
         }
-        return time;
+        return ans;
     }
-*/
+};
