454b486afff2fa11ff617e8c7e1c7b88f1138f3a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-21 18:47:56 +0530

    Time: 97 ms (60.35%) | Memory: 88.4 MB (26.33%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.cpp
@@ -11,55 +11,24 @@
  */
 class Solution {
 public:
-    //METHOD-2
-    //Not Taking &cnt in fun call making int return type....
-    //will not call maxi by using & as we have to track maxi for seperate paths..
-    int solve(TreeNode* root, int maxi) 
+    void dfs(TreeNode* root, int maxi, int &cnt)
     {
         if(!root)
         {
-            return 0;
-        }
-        if(root->val>=maxi)
-        {
-            maxi= max(maxi,root->val);
-            return 1+ solve(root->left,maxi)+ solve(root->right,maxi);
-        }
-        maxi= max(maxi,root->val);
-        return 0+ solve(root->left,maxi)+ solve(root->right,maxi);
-    }
-    int goodNodes(TreeNode* root) 
-    {
-        if(!root)
-        {
-            return 0;
-        }
-        int maxi= root->val;
-        return solve(root,maxi);
-    }
-};
-/*
-    //METHOD-1
-    //Taking &cnt in fun call....
-    //will not call maxi by using & as we have to track maxi for seperate paths..
-
-    void solve(TreeNode* root, int &cnt, int maxi)
-    {
-        if(!root){
             return;
         }
-        if(root->val>=maxi)
+        if(root->val>= maxi)
         {
             cnt++;
         }
         maxi= max(maxi,root->val);
-        solve(root->left,cnt,maxi);
-        solve(root->right,cnt,maxi);
+        dfs(root->left,maxi,cnt);
+        dfs(root->right,maxi,cnt);
     }
     int goodNodes(TreeNode* root) 
     {
-        int cnt=0;
-        solve(root,cnt,root->val);
-        return cnt;
+       int cnt=0;
+       dfs(root,root->val,cnt);
+       return cnt;
     }
-*/
+};
