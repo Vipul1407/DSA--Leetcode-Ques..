bdd4dde1e97c5b779c88eab40d944a9295152f0c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-26 20:51:20 +0530

    Time: 4 ms (33.73%) | Memory: 22.7 MB (26.01%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -11,6 +11,51 @@
  */
 class Solution {
 public:
+    //METHOD-2
+    //  BFS..
+    string smallestFromLeaf(TreeNode* root) 
+    {
+        if(!root)
+        {
+            return "";
+        }
+        string ans;
+        queue<pair<TreeNode*,string>>q;
+       
+        q.push({root,""});
+
+        while(q.size())
+        {
+            int n= q.size();
+            while(n--)
+            {
+                auto top= q.front().first;
+                auto curr= q.front().second;
+                q.pop();
+
+
+                string temp= char(top->val+'a')+curr;
+                if(!top->left && !top->right)
+                {
+                    if(ans=="" || temp<ans)
+                    {
+                        ans= temp;
+                    }
+                }
+                if(top->left)
+                {
+                    q.push({top->left,temp});
+                }
+                if(top->right)
+                {
+                    q.push({top->right,temp});
+                }
+            }
+        }
+        return ans;
+    }
+};
+/*
     //METHOD-1
     //DFS..
     void dfs(TreeNode* root, string curr, string &ans)
@@ -38,4 +83,4 @@ public:
         dfs(root,"",ans);
         return ans;
     }
-};
+*/
