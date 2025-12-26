c45bbdc1f23e8bac5c60b4734f46f6a18bd1b65c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-27 00:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (69.01%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,6 +11,57 @@
  */
 class Solution {
 public:
+    //METHOD-2
+    //BFS..
+    vector<vector<int>> verticalTraversal(TreeNode* root) 
+    {
+        vector<vector<int>>ans;
+        map<pair<int,int>,vector<int>>mp;
+        queue<pair<TreeNode*,pair<int,int>>>q;
+        q.push({root,{0,0}});
+
+        while(q.size())
+        {
+            int n= q.size();
+            while(n--)
+            {
+                int row= q.front().second.first;
+                int col= q.front().second.second;
+                auto top= q.front().first;
+                mp[{row,col}].push_back(top->val);
+                q.pop();
+
+                if(top->left)
+                {
+                    q.push({top->left,{row+1,col-1}});
+                }
+                if(top->right)
+                {
+                    q.push({top->right,{row+1,col+1}});
+                }
+            }
+        }
+        map<int,vector<int>>colmp;
+        for(auto i:mp)
+        {
+            vector<int>temp= i.second;
+            sort(begin(temp),end(temp));
+            int col= i.first.second;
+            for(auto i:temp)
+            {
+                colmp[col].push_back(i);
+            }
+        }
+        for(auto i:colmp)
+        {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
+/*
+    //METHOD-1
+    //DFS..
     void dfs(TreeNode* root, int row, int col, map<pair<int,int>,vector<int>>&mp)
     {
         if(!root)
@@ -46,4 +97,4 @@ public:
         }
         return ans;
     }
-};
+*/
