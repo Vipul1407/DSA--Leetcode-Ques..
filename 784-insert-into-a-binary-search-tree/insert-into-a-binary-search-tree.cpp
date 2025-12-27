6c22edece7ecfad5ff6bac684e5c61a6fe569881
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-28 00:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (79.53%) - LeetSync

---
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) 
+    {
+        if(!root)
+        {
+            return new TreeNode(val);
+        }
+        TreeNode* curr= root;
+        while(curr!=NULL)
+        {
+            if(curr->val<=val)
+            {
+                if(curr->right!=NULL)
+                {
+                    curr= curr->right;
+                }
+                else
+                {
+                    TreeNode* newnode= new TreeNode(val);
+                    curr->right= newnode;
+                    break;
+                }
+            }
+            else
+            {
+                if(curr->left!=NULL)
+                {
+                    curr= curr->left;
+                }
+                else
+                {
+                    TreeNode* newnode= new TreeNode(val);
+                    curr->left= newnode;
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+};
