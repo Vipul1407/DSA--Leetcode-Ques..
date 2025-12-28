1626059456e371688ae371ee9f3d4333fbe0b1d2
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-28 14:33:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (33.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA..
+    int climbStairs(int n) 
+    {
+        vector<int>dp(n+2,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--)
+        {
+            dp[i]= dp[i+1]+dp[i+2];
+        }
+        return dp[0];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,int n)
+    {
+        if(i==n)
+        {
+            return 1;
+        }
+        if(i>n)
+        {
+            return 0;
+        }
+        return rec(i+1,n)+rec(i+2,n);
+    }
+    int climbStairs(int n) 
+    {
+        return rec(0,n);
+    }
+
     //METHOD-2
     //TDA..
     int tda(int i,int n, vector<int>&dp)
@@ -22,25 +55,5 @@ public:
     {
         vector<int>dp(n,-1);
         return tda(0,n,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE..
-    int rec(int i,int n)
-    {
-        if(i==n)
-        {
-            return 1;
-        }
-        if(i>n)
-        {
-            return 0;
-        }
-        return rec(i+1,n)+rec(i+2,n);
-    }
-    int climbStairs(int n) 
-    {
-        return rec(0,n);
     }
 */
