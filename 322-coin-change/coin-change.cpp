562ffd741bb12257c9da19ffc869df57a270e74a
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-28 22:39:18 +0530

    Time: 23 ms (65.85%) | Memory: 17.9 MB (67.15%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,5 +1,56 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA..
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++)
+        {
+            for (int j = 0; j < coins.size(); j++)
+            {
+                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX)
+                {
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(vector<int>& coins, int amount)
+    {
+        if(amount==0)
+        {
+            return 0;
+        }
+        if(amount<0)
+        {
+            return INT_MAX;
+        }
+        int ans= INT_MAX;
+        for(int i=0;i<coins.size();i++)
+        {
+            int mini= rec(coins,amount-coins[i]);
+            if(mini!=INT_MAX)
+            {
+                ans= min(ans, 1+mini);
+            }
+        }
+        return ans;
+    }
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        int ans= rec(coins,amount);
+        return ans==INT_MAX ? -1:ans;
+    }
+
     //METHOD-2
     //TDA..
     int tda(vector<int>& coins, int amount, vector<int>&dp)
@@ -31,36 +82,6 @@ public:
     {
         vector<int>dp(amount+1,-1); 
         int ans= tda(coins,amount,dp);
-        return ans==INT_MAX ? -1:ans;
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE..
-    int rec(vector<int>& coins, int amount)
-    {
-        if(amount==0)
-        {
-            return 0;
-        }
-        if(amount<0)
-        {
-            return INT_MAX;
-        }
-        int ans= INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {
-            int mini= rec(coins,amount-coins[i]);
-            if(mini!=INT_MAX)
-            {
-                ans= min(ans, 1+mini);
-            }
-        }
-        return ans;
-    }
-    int coinChange(vector<int>& coins, int amount) 
-    {
-        int ans= rec(coins,amount);
         return ans==INT_MAX ? -1:ans;
     }
 */
