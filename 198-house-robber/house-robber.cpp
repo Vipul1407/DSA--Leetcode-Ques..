29b3daa22c73d3cd899f8462d975a087507cb89d
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-28 12:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (44.02%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA
+    int rob(vector<int>& nums) 
+    {
+        int n= nums.size();
+        vector<int>dp(n+2,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            int take= nums[i]+dp[i+2];
+            int notake= dp[i+1];
+            dp[i]= max(take,notake);
+        }
+        return dp[0];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,vector<int>& nums)
+    {
+        if(i>= nums.size())
+        {
+            return 0;
+        }
+        int take= nums[i]+rec(i+2,nums);
+        int notake= rec(i+1,nums);
+        return max(take,notake);
+    }
+    int rob(vector<int>& nums) 
+    {
+        return rec(0,nums);
+    }
+
     //METHOD-2
     //TDA
     int tda(int i,vector<int>& nums, vector<int>&dp)
@@ -20,23 +53,5 @@ public:
     {
         vector<int>dp(nums.size(),-1);
         return tda(0,nums,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE..
-    int rec(int i,vector<int>& nums)
-    {
-        if(i>= nums.size())
-        {
-            return 0;
-        }
-        int take= nums[i]+rec(i+2,nums);
-        int notake= rec(i+1,nums);
-        return max(take,notake);
-    }
-    int rob(vector<int>& nums) 
-    {
-        return rec(0,nums);
     }
 */
