7c4caad2c24e60cac82546aa8ca0449c33e01b6b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-29 16:06:04 +0530

    Time: 51 ms (17.73%) | Memory: 36.7 MB (40.35%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    //METHOD-2
+    //TDA..
+    int tda(int i,int amount, vector<int>& coins,vector<vector<int>>&dp)
+    {
+        if(amount==0)
+        {
+            return 1;
+        }
+        if(i>= coins.size() || amount<0)
+        {
+            return 0;
+        }
+        if(dp[i][amount]!=-1)
+        {
+            return dp[i][amount];
+        }
+        //can reuse coin..   so stay on same i..
+        int take= tda(i,amount-coins[i],coins,dp);
+        int notake= tda(i+1,amount,coins,dp);
+        return dp[i][amount]= take+notake;
+    }
+    int change(int amount, vector<int>& coins) 
+    {
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+        return tda(0,amount,coins,dp);
+    }
+};
+/*
+    //METHOD-1
+    int rec(int i,int amount, vector<int>& coins)
+    {
+        if(amount==0)
+        {
+            return 1;
+        }
+        if(i>= coins.size() || amount<0)
+        {
+            return 0;
+        }
+        //can reuse coin..   so stay on same i..
+        int take= rec(i,amount-coins[i],coins);
+        int notake= rec(i+1,amount,coins);
+        return take+notake;
+    }
+    int change(int amount, vector<int>& coins) 
+    {
+        return rec(0,amount,coins);
+    }
+*/
