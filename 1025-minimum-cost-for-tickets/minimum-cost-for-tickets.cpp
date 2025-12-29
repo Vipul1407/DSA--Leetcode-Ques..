0ce78ec99b8624f5c841ff7de7ca300fb0d1fdfd
Vipul <129517280+Vipul1407@users.noreply.github.com>
2025-12-29 12:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (33.70%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,5 +1,69 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA APPROACH..
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {
+        int n=days.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            //1 day pass..
+            int option1= costs[0]+ dp[i+1];
+
+            //7 day pass..
+            int idx= i;
+            while(idx<n && days[idx]<days[i]+7)
+            {
+                idx++;
+            }
+            int option2= costs[1]+ dp[idx];
+
+            //30 day pass..
+            int newidx= i;
+            while(newidx<n && days[newidx]<days[i]+30)
+            {
+                newidx++;
+            }
+            int option3= costs[2]+ dp[newidx];
+            dp[i]= min(option1,min(option2,option3));
+        }
+        return dp[0];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE APPROACH..
+    int rec(int i,vector<int>& days, vector<int>& costs)
+    {
+        int n= days.size();
+        if(i>=n)
+        {
+            return 0;
+        }
+        int option1= costs[0]+ rec(i+1,days,costs);
+
+        int idx= i;
+        while(idx<n && days[idx]<days[i]+7)
+        {
+            idx++;
+        }
+        int option2= costs[1]+ rec(idx,days,costs);
+
+        int newidx= i;
+        while(newidx<n && days[newidx]<days[i]+30)
+        {
+            newidx++;
+        }
+        int option3= costs[2]+ rec(newidx,days,costs);
+        return min(option1,min(option2,option3));
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) 
+    {
+        return rec(0,days,costs);
+    }
+
+
     //METHOD-2
     //TDA APPROACH..
     int tda(int i,vector<int>& days, vector<int>& costs, vector<int>&dp)
@@ -39,37 +103,5 @@ public:
     {
         vector<int>dp(days.size(),-1);
         return tda(0,days,costs,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE APPROACH..
-    int rec(int i,vector<int>& days, vector<int>& costs)
-    {
-        int n= days.size();
-        if(i>=n)
-        {
-            return 0;
-        }
-        int option1= costs[0]+ rec(i+1,days,costs);
-
-        int idx= i;
-        while(idx<n && days[idx]<days[i]+7)
-        {
-            idx++;
-        }
-        int option2= costs[1]+ rec(idx,days,costs);
-
-        int newidx= i;
-        while(newidx<n && days[newidx]<days[i]+30)
-        {
-            newidx++;
-        }
-        int option3= costs[2]+ rec(newidx,days,costs);
-        return min(option1,min(option2,option3));
-    }
-    int mincostTickets(vector<int>& days, vector<int>& costs) 
-    {
-        return rec(0,days,costs);
     }
 */
