d22c992fdfff7339498ce1c53200b46d4e37f5bc
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-03 10:06:46 +0530

    Time: 21 ms (28.71%) | Memory: 17.2 MB (38.25%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //METHOD-1..
     //FLOYD WARSHAL'S ALGO..
     int findTheCity(int n, vector<vector<int>>& edges, int threshold)
     {
@@ -56,3 +57,60 @@ public:
         return city;
     }
 };
+/*
+    //METHOD-2
+    //USING DIKSHTRA..
+    int findTheCity(int n, vector<vector<int>>& edges, int td) 
+    {
+        vector<pair<int,int>>adj[n];
+        for(auto i:edges)
+        {
+            int u= i[0];
+            int v= i[1];
+            int w= i[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        //MIN HEAP..
+        int city=0;
+        int mincnt= n+1;
+        for(int i=0;i<n;i++)
+        {
+            vector<int>dis(n,INT_MAX);
+            dis[i]=0;
+            //distance, node
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+            pq.push({0,i});
+            while(!pq.empty())
+            {
+                int d= pq.top().first;
+                int node= pq.top().second;
+                pq.pop();
+                for(auto it:adj[node])
+                {
+                    int neigh= it.first;
+                    int wt= it.second;
+                    if(d+wt< dis[neigh])
+                    {
+                        dis[neigh]= d+wt;
+                        pq.push({dis[neigh],neigh});
+                    }
+                }
+            }
+            int cnt=0;
+            for(int j=0;j<n;j++)
+            {
+                if(dis[j]<=td)
+                {
+                    cnt++;
+                }
+            }
+            if(cnt<=mincnt)
+            {
+                mincnt= cnt;
+                city= i;
+            }
+        }
+        return city;
+    }
+*/
