a43d534cd62fe474fc9d13d41fb0f5abdd6118ff
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-07 15:39:25 +0530

    Time: 6 ms (37.56%) | Memory: 78.1 MB (55.87%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -11,6 +11,38 @@
  */
 class Solution {
 public:
+    //METHOD-2
+    //1 TIME DFS ONLY..
+    long long total=0;
+    int solve(TreeNode* root, long long &ans)
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        int leftsum= solve(root->left,ans);
+        int rightsum= solve(root->right,ans);
+        long long subtreesum= root->val+ leftsum+ rightsum;
+        ans= max(ans, subtreesum*(total-subtreesum));
+        return subtreesum;
+    }
+    int maxProduct(TreeNode* root) 
+    {
+        if(!root)
+        {
+            return 0;
+        }
+        const int mod= 1000000007;
+        long long ans=0;
+        total= solve(root,ans);
+        ans=0;
+        solve(root,ans);
+        return ans%mod;
+    }
+};
+/*
+    //METHOD-1
+    //2 TIMES DFS..
     long long getsum(TreeNode* root)
     {
         if(!root)
@@ -43,4 +75,4 @@ public:
         solve(root,total,ans);
         return ans%mod;
     }
-};
+*/
