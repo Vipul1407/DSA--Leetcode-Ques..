c916352c75ba79d8135d88b6c35dbdba5b7efce7
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-12 17:21:07 +0530

    Time: 7 ms (49.21%) | Memory: 12.3 MB (92.82%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,5 +1,49 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA..
+    int maxSumAfterPartitioning(vector<int>& arr, int k) 
+    {
+        int n=arr.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            int maxi=0;
+            int ans=0;
+            for(int j=i;j<n && j-i+1<=k;j++)
+            {
+                maxi= max(maxi,arr[j]);
+                ans= max(ans, maxi*(j-i+1)+ dp[j+1]);
+            }
+            dp[i]= ans;
+        }
+        return dp[0];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,vector<int>& arr, int k)
+    {
+        int n= arr.size();
+        if(i>=n)
+        {
+            return 0;
+        }
+        int ans= 0;
+        int maxi= 0;
+        for(int j=i; j<n && j-i+1<=k; j++)
+        {
+            maxi= max(maxi,arr[j]);
+            ans= max(ans, maxi*(j-i+1)+ rec(j+1,arr,k));
+        }
+        return ans;
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) 
+    {
+        return rec(0,arr,k);
+    }
+
     //METHOD-2
     //TDA..
     int tda(int i, vector<int>& arr, int k, vector<int>&dp)
@@ -26,29 +70,5 @@ public:
     {
         vector<int>dp(arr.size(),-1);
         return tda(0,arr,k,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE..
-    int rec(int i,vector<int>& arr, int k)
-    {
-        int n= arr.size();
-        if(i>=n)
-        {
-            return 0;
-        }
-        int ans= 0;
-        int maxi= 0;
-        for(int j=i; j<n && j-i+1<=k; j++)
-        {
-            maxi= max(maxi,arr[j]);
-            ans= max(ans, maxi*(j-i+1)+ rec(j+1,arr,k));
-        }
-        return ans;
-    }
-    int maxSumAfterPartitioning(vector<int>& arr, int k) 
-    {
-        return rec(0,arr,k);
     }
 */
