5c9c17954acdcc2677f0c74e08c9ff85f94c339c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-14 14:15:10 +0530

    Time: 119 ms (13.54%) | Memory: 140.7 MB (12.44%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    bool check(string str, int i,int j)
+    bool palindrome(string &a)
     {
-        int n= str.size();
-        while(i<=j)
+        int l=0;
+        int r= a.size()-1;
+        while(l<=r)
         {
-            if(str[i]==str[j])
-            {
-                i++;
-                j--;
-            }
-            else
+            if(a[l]!=a[r])
             {
                 return false;
             }
+            l++;r--;
         }
         return true;
     }
-    void solve(int i, vector<string> &curr, string &s, vector<vector<string>> &ans)
+    void rec(int i, string &s, vector<string>curr, vector<vector<string>>&ans)
     {
-        if(i==s.size())
+        int n= s.size();
+        if(i==n)
         {
             ans.push_back(curr);
             return;
         }
-
-        for(int j=i;j<s.size();j++)
+        for(int j=i;j<n;j++)
         {
-            if(check(s,i,j))
+            string newstr= s.substr(i,j-i+1);
+            if(palindrome(newstr))
             {
-                curr.push_back(s.substr(i,j-i+1));
-                solve(j + 1, curr, s, ans);
+                curr.push_back(newstr);
+                rec(j+1,s,curr,ans);
                 curr.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) 
     {
-        int n= s.size();
         vector<vector<string>>ans;
         vector<string>curr;
-        solve(0,curr,s,ans);
+        rec(0,s,curr,ans);
         return ans;
     }
 };
