44c7a8076437febba5934270bed6bc479094e86b
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-18 19:51:21 +0530

    Time: 959 ms (43.96%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 -- METHOD-1
-with cte as (select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by e.departmentId order by e.salary desc) as drnk from Employee e inner join Department d on e.departmentId=d.id)
+-- with cte as (select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by e.departmentId order by e.salary desc) as drnk from Employee e inner join Department d on e.departmentId=d.id)
 
-select Department,Employee,Salary from cte where drnk<=3;
+-- select Department,Employee,Salary from cte where drnk<=3;
+
+-- METHOD-2
+select Department,Employee,Salary from (select d.name as Department, e.name as Employee, e.salary as Salary, dense_rank() over (partition by e.departmentId order by e.salary desc)as drnk from Employee e inner join Department d on e.departmentId=d.id)t where t.drnk<=3;
