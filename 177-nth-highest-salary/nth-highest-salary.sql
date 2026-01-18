1d32a29eddf8db6d3dfd5cee346d88207457c34c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-18 20:37:05 +0530

    Time: 417 ms (58.45%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,3 +1,28 @@
+-- METHOD-3
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+    select distinct(salary) from(select salary, dense_rank() over (order by salary desc) as drnk from Employee)t where t.drnk=N 
+  );
+END
+
+
+
+/*
+-- METHOD-1
+
+    CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    set N= N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+    select distinct salary from Employee order by salary desc limit 1 offset N
+  );
+END*
+
+-- METHOD-2
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     set N= N-1;
@@ -6,12 +31,4 @@ BEGIN
     select distinct(salary) from Employee e1 where N=(select count(distinct salary) from Employee e2 where e2.salary>e1.salary)
   );
 END
-/*
-    CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
-BEGIN
-    set N= N-1;
-  RETURN (
-      # Write your MySQL query statement below.
-    select distinct salary from Employee order by salary desc limit 1 offset N
-  );
-END*/
+*/
