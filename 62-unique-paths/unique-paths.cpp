8514c7bc6231ad67c31db6d329aa295f2985dba4
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-23 23:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (66.43%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,5 +1,49 @@
 class Solution {
 public:
+    //METHOD-3
+    //BUA..
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                if(i==m-1 && j==n-1)
+                {
+                    continue;
+                }
+                int left= dp[i][j+1];
+                int right= dp[i+1][j];
+                dp[i][j]= left+right;
+            }
+        }
+        return dp[0][0];
+    }
+};
+/*
+    //METHOD-1
+    //RECURSIVE..
+    int rec(int i,int j,int &m, int &n)
+    {
+        if(i>=m || j>=n)
+        {
+            return 0;
+        }
+        if(i==m-1 && j==n-1)
+        {
+            return 1;
+        }
+        int left= rec(i,j+1,m,n);
+        int right= rec(i+1,j,m,n);
+        return left+right;
+    }
+    int uniquePaths(int m, int n) 
+    {
+        return rec(0,0,m,n);
+    }
+
     //METHOD-2
     //TDA..
     int tda(int i,int j,int &m, int &n, vector<vector<int>>&dp)
@@ -24,27 +68,5 @@ public:
     {
         vector<vector<int>>dp(m,vector<int>(n,-1));
         return tda(0,0,m,n,dp);
-    }
-};
-/*
-    //METHOD-1
-    //RECURSIVE..
-    int rec(int i,int j,int &m, int &n)
-    {
-        if(i>=m || j>=n)
-        {
-            return 0;
-        }
-        if(i==m-1 && j==n-1)
-        {
-            return 1;
-        }
-        int left= rec(i,j+1,m,n);
-        int right= rec(i+1,j,m,n);
-        return left+right;
-    }
-    int uniquePaths(int m, int n) 
-    {
-        return rec(0,0,m,n);
     }
 */
