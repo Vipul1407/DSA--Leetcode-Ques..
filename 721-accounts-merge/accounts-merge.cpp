e2a678ffbfd61cbd65a1cde1e7de1828a0f55fef
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-01-27 12:45:27 +0530

    Time: 38 ms (48.42%) | Memory: 40.7 MB (42.28%) - LeetSync

--- 721-accounts-merge/accounts-merge.cpp
+++ 721-accounts-merge/accounts-merge.cpp
@@ -1,90 +1,85 @@
 class DSU
-{
-    public:
-    vector<int>parent,rank;
-    DSU(int n)
     {
-        parent.resize(n);
-        rank.resize(n,0);
-        for(int i=0;i<n;i++)
+        public:
+        vector<int>par,rank;
+        DSU(int n)
         {
-            parent[i]=i;
+            par.resize(n);
+            rank.resize(n,0);
+            for(int i=0;i<n;i++)
+            {
+               par[i]=i; 
+            }
         }
-    }
-    int findupar(int node)
-    {
-        if(node==parent[node])
+        int findupar(int node)
         {
-            return node;
+            if(node==par[node])
+            {
+                return node;
+            }
+            return par[node]= findupar(par[node]);
         }
-        return parent[node]= findupar(parent[node]);
-    }
-    void unionbyrank(int u,int v)
-    {
-        int up_u= findupar(u);
-        int up_v= findupar(v);
-        if(up_u==up_v)
+        void unionbyrank(int u,int v)
         {
-            return;
+            int pu= findupar(u);
+            int pv= findupar(v);
+            if(pu==pv)
+            {
+                return;
+            }
+            if(rank[pu]<rank[pv])
+            {
+                par[pu]= pv;
+            }
+            else if(rank[pv]<rank[pu])
+            {
+                par[pv]= pu;
+            }
+            else
+            {
+                par[pv]= pu;
+                rank[pu]++;
+            }
         }
-        if(rank[up_u]<rank[up_v])
-        {
-            parent[up_u]= up_v;
-        }
-        else if(rank[up_u]>rank[up_v])
-        {
-            parent[up_v]= up_u;
-        }
-        else
-        {
-            parent[up_v]= up_u;
-            rank[up_u]++;
-        }
-    }
-};
+    };
+
 class Solution {
 public:
-    vector<vector<string>> accountsMerge(vector<vector<string>>& acc) 
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) 
     {
         unordered_map<string,int>mp;
-        int n= acc.size();
-        DSU ds(n+1);
+        int n= accounts.size(); 
+
+        DSU ds(n);
         for(int i=0;i<n;i++)
         {
-            for(int j=1;j<acc[i].size();j++)
+            for(int j=1;j<accounts[i].size();j++)
             {
-                string email= acc[i][j];
+                string email= accounts[i][j];
                 if(mp.find(email)!=mp.end())
                 {
                     ds.unionbyrank(i,mp[email]);
                 }
                 else
                 {
-                    mp[email]= i;
+                    mp[email]=i;
                 }
             }
-        }
-        vector<vector<string>>merged(n);
-        
+        }   
+        vector<vector<string>>ans;
+        unordered_map<int,vector<string>>vec;
         for(auto i:mp)
         {
-            int par= ds.findupar(i.second);
-            merged[par].push_back(i.first);
+            string email= i.first;
+            int node= i.second;
+            int node_par= ds.findupar(node);
+            vec[node_par].push_back(email);
         }
-        vector<vector<string>>ans;
-        for(int i=0;i<n;i++)
+        for(auto i:vec)
         {
-            if(merged[i].size()==0)
-            {
-                continue;
-            }
-            sort(merged[i].begin(),merged[i].end());
-            vector<string>temp;
-            temp.push_back(acc[i][0]);
-            for(auto j:merged[i])
-            {
-                temp.push_back(j);
-            }
+            auto temp= i.second;
+            sort(begin(temp),end(temp));
+            temp.insert(begin(temp),accounts[i.first][0]);
             ans.push_back(temp);
         }
         return ans;
