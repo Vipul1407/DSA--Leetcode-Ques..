efafd0e21ec853bf314e073b039495248a8e2091
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-02-07 17:25:47 +0530

    Time: 25 ms (58.88%) | Memory: 55.2 MB (10.50%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int minimumDeletions(string s) 
-    {
-        stack<char>st;
-        int ans=0;
-        int cb=0;
-        for(auto i:s)
-        {
-            if(i=='b')
-            {
-                cb++;
-            }
-            else if(i=='a')
-            {
-                if(cb>0)
-                {
-                    ans++;
-                    cb--;
-                }
-            }
+    int minimumDeletions(string s) {
+        int n = s.length();
+
+        vector<int> leftB(n, 0);
+        leftB[0] = (s[0] == 'b');
+
+        for (int i = 1; i < n; i++) {
+            leftB[i] = leftB[i - 1] + (s[i] == 'b');
         }
+
+        vector<int> rightA(n, 0);
+        rightA[n - 1] = (s[n - 1] == 'a');
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightA[i] = rightA[i + 1] + (s[i] == 'a');
+        }
+
+        int ans = min(leftB[n - 1], rightA[0]); // all b’s or all a’s
+
+        for (int j = 1; j < n; j++) {
+            ans = min(ans, leftB[j - 1] + rightA[j]);
+        }
+
         return ans;
     }
 };
