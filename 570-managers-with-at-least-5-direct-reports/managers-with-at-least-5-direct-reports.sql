05a7843056b1fb98ea649e37a8d11df6f66b6abf
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-02-11 16:55:45 +0530

    Time: 316 ms (83.90%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
 -- METHOD-1
 -- Using SubQuery..
-select name from Employee where id IN(
-    select managerId from Employee group by managerId having count(managerId)>=5
-);
+-- select name from Employee where id IN(
+--     select managerId from Employee group by managerId having count(managerId)>=5
+-- );
 
+-- METHOD-2
+-- Using inner join
+select e2.name from Employee e1 inner join Employee e2 on e1.managerId= e2.id group by e1.managerId having count(e1.managerId)>=5;
