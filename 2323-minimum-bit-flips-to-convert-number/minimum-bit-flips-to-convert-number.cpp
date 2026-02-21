d4877ef68b5c410444245192e2ecd71ff6db0bde
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-02-21 20:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (59.29%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
+    // int setbits(int n)
+    // {
+    //     int cnt=0;
+    //     while(n>0)
+    //     {
+    //         if(n&1)
+    //         {
+    //             cnt++;
+    //         }
+    //         n>>=1;
+    //     }
+    //     return cnt;
+    // }
     int setbits(int n)
     {
         int cnt=0;
-        while(n>0)
+        for(int i=0;i<32;i++)
         {
-            if(n&1)
+            if(n & (1<<i))
             {
                 cnt++;
             }
-            n>>=1;
         }
         return cnt;
     }
