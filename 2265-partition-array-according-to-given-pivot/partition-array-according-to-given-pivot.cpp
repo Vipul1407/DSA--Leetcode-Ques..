af4b96255ef54070184b02a4b54bf8a20e3e8946
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-06-08 12:13:49 +0530

    Time: 4 ms (81.51%) | Memory: 127.8 MB (78.53%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,7 +1,34 @@
 class Solution {
 public:
+    //METHOD-2
+    vector<int> pivotArray(vector<int>& nums, int pivot) 
+    {
+        int n= nums.size();
+        vector<int>ans(n,pivot);
+
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]<pivot)
+            {
+                ans[k++]=nums[i];
+            }
+        }
+
+        k=n-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(nums[i]>pivot)
+            {
+                ans[k--]=nums[i];
+            }
+        }
+
+        return ans;
+    }
+};
+/*
     //METHOD-1
-    //2 Pass Sol
     vector<int> pivotArray(vector<int>& nums, int pivot) 
     {
         int cnt=0;
@@ -20,7 +47,7 @@ public:
             }
         }
 
-         while(cnt--)
+         while(cnt--) 
         {
             ans.push_back(pivot);
         }
@@ -35,4 +62,4 @@ public:
 
         return ans;
     }
-};
+*/
