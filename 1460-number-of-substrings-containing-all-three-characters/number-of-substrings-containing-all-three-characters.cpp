4a2b240f4740faf0ee7e984bbfe83deb7afc6df0
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-06-30 11:42:39 +0530

    Time: 15 ms (32.23%) | Memory: 11.1 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,51 +1,14 @@
 class Solution {
 public:
-    //METHOD-2
-    //SIMPLE APPROACH
-    int numberOfSubstrings(string s) 
-    {
-        int n= s.size();
-        //recent index..
-        int a=-1,b=-1,c=-1,ans=0;
- 
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]=='a')
-            {
-                a=i;
-            }
-            else if(s[i]=='b')
-            {
-                b=i;
-            }
-            else
-            {
-                c=i;
-            }
-            ans+= 1+min(a,min(b,c));
-        }   
-        return ans;
+    int numberOfSubstrings(string s) {
+        int res = 0;
+        int p[3] = {-1, -1, -1};
+
+        for (int i = 0; i < s.length(); i++) {
+            p[(s[i] & 31) - 1] = i;
+            res += min({p[0], p[1], p[2]}) + 1;
+        }
+
+        return res;
     }
 };
-/*
-    //METHOD-1
-    //NORMAL SLIDIING WINDOW
-    int numberOfSubstrings(string s) 
-    {
-        int n= s.size();
-        vector<int>mp(3,0);
-        int l=0,r=0,ans=0;
-        while(r<n)
-        {
-            mp[s[r]-'a']++;
-            while(mp[0]>0 && mp[1]>0 && mp[2]>0)
-            {
-                ans+= n-r;
-                mp[s[l]-'a']--;
-                l++;
-            }
-            r++;
-        }   
-        return ans;
-    }
-*/
