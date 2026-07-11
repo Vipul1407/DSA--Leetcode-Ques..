46d0895a40712921c0b73173732439912cd90f18
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-07-11 10:04:32 +0530

    Time: 42 ms (66.19%) | Memory: 127.9 MB (82.06%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,178 +1,39 @@
-
-//CONNECTED COMPONENT IS THAT WHICH CONTAINS EDGE B/W EVERY CONNECTED NODE IN GRAPH..
-//Means Total edges in graph is n(n-1)/2 
-//METHOD-3
-//DSU..(DISJOINT SET)
-//TC= O(E* Alpha(V))
-class DSU
-{
-    public:
-    vector<int>parent,size;
-    DSU(int n)
-    {
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++)
-        {
-            parent[i]=i;
-        }
-        size.resize(n+1,1);
-    }
-    int findupar(int node)
-    {
-        if(node==parent[node])
-        {
-            return node;
-        }
-        return parent[node]= findupar(parent[node]);
-    }
-    void unionbysize(int u,int v)
-    {
-        int up_u= findupar(u);
-        int up_v= findupar(v);
-        if(up_u==up_v)
-        {
-            return;
-        }
-        else if(parent[up_u]<parent[up_v])
-        {
-            parent[up_u]= up_v;
-            size[up_v]+= size[up_u];
-        }
-        else
-        {
-            parent[up_v]= up_u;
-            size[up_u]+= size[up_v];
-        }
-    }
-};
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) 
-    {
-        DSU ds(n);
-        //storing count of edges in mp..
-        unordered_map<int,int>mp;
-        for(auto &i:edges)
-        {
-            int u= i[0];
-            int v= i[1];
-            ds.unionbysize(u,v);
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> A(n);
+
+        for (auto& e : edges) {
+            int u = e[0], v = e[1];
+            A[u].push_back(v);
+            A[v].push_back(u);
         }
-        for(auto &i:edges)
-        {
-            int root= ds.findupar(i[0]);
-            mp[root]++;
-        }
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            if(ds.parent[i]==i)
-            {
-                //nodes will be the size..
-                int v= ds.size[i];
-                //edges will be stored in mp..
-                int e= mp[i];;
-                if(e== v*(v-1)/2)
-                {
-                    cnt++;
-                }
+
+        bitset<51> vis;
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            bool state = vis.test(i);
+
+            if (!state) {
+                int V = 0, E = 0;
+
+                auto dfs = [&](auto& self, int x) -> void {
+                    V++;
+                    E += A[x].size();
+                    vis.set(x);
+
+                    for (auto& state : A[x])
+                        if (!vis.test(state))
+                            self(self, state);
+                };
+
+                dfs(dfs, i);
+
+                res += E == V * (V - 1);
             }
         }
-        return cnt;
+
+        return res;
     }
 };
-/*
-//CONNECTED COMPONENT IS THAT WHICH CONTAINS EDGE B/W EVERY CONNECTED NODE IN GRAPH..
-//Means Total edges in graph is n(n-1)/2 
-    //METHOD-1
-    //DFS..
-    void dfs(int i,unordered_map<int,vector<int>>&adj, vector<int>&vis, int &v, int &e)
-    {
-        vis[i]=1;
-        v++;
-        e+= adj[i].size();
-        for(auto j:adj[i])
-        {
-            if(!vis[j])
-            {
-                dfs(j,adj,vis,v,e);
-            }
-        }
-    }
-    int countCompleteComponents(int n, vector<vector<int>>& edges) 
-    {
-        unordered_map<int,vector<int>>adj;
-        for(auto i:edges)
-        {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-        }
-        vector<int>vis(n,0);
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                int v=0;
-                int e=0;
-                dfs(i,adj,vis,v,e);
-                if(e== v*(v-1))
-                {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-
-    //CONNECTED COMPONENT IS THAT WHICH CONTAINS EDGE B/W EVERY CONNECTED NODE IN GRAPH..
-    //Means Total edges in graph is n(n-1)/2 
-    //METHOD-2
-    //BFS..
-    void bfs(int i,unordered_map<int,vector<int>>&adj,vector<int>&vis,int &v,int &e)
-    {
-        queue<int>q;
-        q.push(i);
-        vis[i]=1;
-        while(q.size())
-        {
-            int top= q.front();
-            q.pop();
-            v++;
-            e+= adj[top].size();
-            for(auto j:adj[top])
-            {
-                if(!vis[j])
-                {
-                    q.push(j);
-                    vis[j]=1;
-                }
-            }
-        }
-    }
-    int countCompleteComponents(int n, vector<vector<int>>& edges) 
-    {
-        unordered_map<int,vector<int>>adj;
-        for(auto i:edges)
-        {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-        }
-        vector<int>vis(n,0);
-        int cnt=0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                int v=0;
-                int e=0;
-                bfs(i,adj,vis,v,e);
-                if(e== v*(v-1))
-                {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-*/
