d2dfbd26c01e48ebe52b068e5e7e51db69f3e79c
Vipul <129517280+Vipul1407@users.noreply.github.com>
2026-08-12 09:14:32 +0530

    Time: 63 ms (63.26%) | Memory: 155.8 MB (5.11%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int maxSubarrayLength(vector<int>& nums, int k) 
-    {
-        int n= nums.size();
-        int l=0,r=0;
-        int ans=0;
-        unordered_map<int,int>mp;
-        while(r<n)
-        {
-            mp[nums[r]]++;
-            while(mp[nums[r]]>k)
-            {
-                mp[nums[l]]--;
-                l++;
-            }
-            ans= max(ans,r-l+1);
-            r++;
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        const int n=nums.size();
+        int cnt=0;
+        unordered_map<int, int> freq;//1 <= nums[i] <= 10^9
+        freq.reserve(n);
+        for (int l=0, r=0; r<n; r++){
+            int x=nums[r];
+            auto it=freq.find(x);
+            int& f=(it==freq.end())?freq[x]=1:++(it->second);
+            while (f>k)
+                freq[nums[l++]]--;
+        
+            cnt=max(cnt,r-l+1);// max len
         }
-        return ans;
+        return cnt;
     }
 };
